923d4f445d847abdf885173259e7190cbb0d02ff
Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>
2010-08-02 10:29:02 +0200

    ENH: Added gcc specific defines
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>

--- kernel/compiler_gcc.hpp
+++ kernel/compiler_gcc.hpp
@@ -8,10 +8,24 @@
 
 #if !defined(FEAST_COMPILER) && defined(__GNUC__)
 
+// calc linear sortable gcc version
+#  define _GCC_VER (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
+
 // define FEAST_COMPILER_GCC
-#  define FEAST_COMPILER_GCC 1
+#  define FEAST_COMPILER_MSC _GCC_VER
 
 #  define FEAST_COMPILER "GNU C++ compiler"
+
+// Now let's see what C++0x features the compiler offers
+#  if (_GCC_VER > 40600)
+#    define HAVE_CPP0X_NULLPTR 1
+#  endif
+#  if (_GCC_VER > 40300)
+#    define HAVE_CPP0X_STATIC_ASSERT 1
+#  endif
+#  if (_GCC_VER > 40100)
+#    define HAVE_CPP0X_EXTERN_TEMPLATE 1
+#  endif
 
 #endif // !defined(FEAST_COMPILER) && defined(__GNUC__)
 
