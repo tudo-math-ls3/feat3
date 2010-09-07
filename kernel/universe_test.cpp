5751bce15e121773ca5d73208d35f2ff7804c791
Hilmar Wobker <hilmar.wobker@math.tu-dortmund.de>
2010-09-07 16:32:10 +0200

    ENH: adaption to changes in universe.hpp, further minor changes
    Signed-off-by: Hilmar Wobker <hilmar.wobker@math.tu-dortmund.de>

--- kernel/universe_test.cpp
+++ kernel/universe_test.cpp
@@ -1,3 +1,6 @@
+#pragma once
+#ifndef KERNEL_UNIVERSE_TEST_CPP
+#define KERNEL_UNIVERSE_TEST_CPP 1
 // Test driver for universe
 
 // includes, system
@@ -6,6 +9,8 @@
 // includes, Feast
 #include <kernel/base_header.hpp>
 #include <kernel/universe.hpp>
+#include <kernel/util/string_utils.hpp>
+#include <kernel/util/mpi_utils.hpp>
 
 // main routine
 // @author Hilmar Wobker
@@ -22,33 +27,25 @@ int main(int argc, char* argv[])
   num_processes_in_group[0] = 2;
   num_processes_in_group[1] = 3;
 
-  // create universe
+  // create universe with one process group
+//  Universe* universe = Universe::create(argc, argv);
 
-  // simple constructor for creating one process group
-  // Universe universe(argc, argv);
-
-  // constructor for creating more than one process group
-  // @Hilmar: So lebt das ganze Universum auf dem Stack und nicht auf dem Heap. Absicht?
-  Universe universe(argc, argv, num_process_groups, num_processes_in_group);
+  // create universe with several process groups
+  Universe* universe = Universe::create(argc, argv, num_process_groups, num_processes_in_group);
 
   // Get process objects. Note that on each process only one of the following three exists (the other two are
   // null pointers.).
-  LoadBalancer* load_balancer = universe.get_load_balancer();
-  GroupProcess* group_process = universe.get_group_process();
-  Master* master = universe.get_master();
-
-  // rank of this process
-  int my_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  LoadBalancer* load_balancer = universe->get_load_balancer();
+  GroupProcess* group_process = universe->get_group_process();
+  Master* master = universe->get_master();
 
   if (load_balancer != nullptr)
   {
     int rw = load_balancer->get_rank_world();
     int gid = load_balancer->get_group_id();
     int rl = load_balancer->get_rank_local();
-    std::cout << "process with world rank " << rw << " is the load balancer with id "
-              << gid << " and local rank " << rl << std::endl;
-    if (load_balancer->get_group_id() == 0)
+    std::cout << "Process " << rw << " is the load balancer with local rank " << rl << " in group "<< gid << std::endl;
+    if (gid == 0)
     {
       load_balancer->read_mesh();
       load_balancer->create_work_groups();
@@ -63,17 +60,26 @@ int main(int argc, char* argv[])
   {
     // not sure yet if it makes sense to let the user control the group processes
     int rw = group_process->get_rank_world();
-    std::cout << "Process with world rank "<< rw << " is a group process!" << std::endl;
+    int gid = group_process->get_group_id();
+    int rl = group_process->get_rank_local();
+    std::cout << "Process " << rw << " is the group process with local rank " << rl << " in group "<< gid << std::endl;
   }
   else if (master != nullptr)
   {
     // not sure yet if it makes sense to let the user control the master process
-    int rw = group_process->get_rank_world();
-    std::cout << "Process with world rank "<< rw << " is the MASTER OF THE UNIVERSE!" << std::endl;
+    int rw = master->get_rank_world();
+    std::cout << "Process " << rw << " is the MASTER OF THE UNIVERSE!" << std::endl;
   }
   else
   {
-    // dom-debug
-    std::cout << "Process with rank " << my_rank << " has no particular role, this should not happen." << std::endl;
+    // rank of this process
+    int my_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPIUtils::abort("Process with rank " + StringUtils::stringify(my_rank)
+                    + " has no particular role, this should not happen.");
   }
+
+  Universe::destroy();
 }
+
+#endif // guard KERNEL_UNIVERSE_TEST_CPP
