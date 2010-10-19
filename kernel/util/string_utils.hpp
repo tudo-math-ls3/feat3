af9fc0927e9f47a24dc4ef434c2d849eb089d6ae
Dominik Goeddeke <dominik.goeddeke@math.tu-dortmund.de>
2010-10-19 13:45:00 +0200

    STYLE: Unified stringify(), one implementation is enough
    Signed-off-by: Dominik Goeddeke <dominik.goeddeke@math.tu-dortmund.de>

--- kernel/util/string_utils.hpp
+++ kernel/util/string_utils.hpp
@@ -5,21 +5,82 @@
 #include <sstream>
 #include <string>
 
-/// collection of various string utilities
+/**
+* \brief collection of various string utilities
+*
+* \author Dirk Ribbrock
+* \author Dominik Goeddeke
+*/
 class StringUtils
 {
- public:
+  public:
   /* *****************
   * member functions *
   *******************/
 
-  ///casts C++ data types in std::string
-  template<typename T>
-  static inline std::string stringify(const T& x)
+  /**
+   * Convert item to a string.
+   * \param[in] item
+   * The item to stringify
+   */
+  template <typename T_>
+  static inline std::string stringify(const T_ & item)
   {
-    std::ostringstream o;
-    o << x;
-    return o.str();
+    std::ostringstream s;
+    s << item;
+    return s.str();
   }
+
+  /**
+   * Convert item to a string (overload for std::string).
+   * \param[in] item
+   * The item to stringify
+   */
+  static inline std::string stringify(const std::string & item)
+  {
+    return item;
+  }
+
+  /**
+   * Convert item to a string (overload for char).
+   * \param[in] item
+   * The item to stringify
+   */
+  static inline std::string stringify(const char & item)
+  {
+    return std::string(1, item);
+  }
+
+  /**
+   * Convert item to a string (overload for unsigned char).
+   * \param[in] item
+   * The item to stringify
+   */
+  static inline std::string stringify(const unsigned char & item)
+  {
+    return std::string(1, item);
+  }
+
+  /**
+   * Convert item to a string (overload for bool).
+   * \param[in] item
+   * The item to stringify
+   */
+  static inline std::string stringify(const bool & item)
+  {
+    return item ? "true" : "false";
+  }
+
+  /**
+   * Convert item to a string (overload for char *, which isn't a
+   * screwup like other pointers).
+   * \param[in] item
+   * The item to stringify
+   */
+  static inline std::string stringify(const char * const item)
+  {
+    return std::string(item);
+  }
+
 };
 #endif //  #ifndef STRING_UTILS_HPP
