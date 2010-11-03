f9c4fa9752be89a43e8ba609d2f16185d51d52c0
Peter Zajac <pzajac@math.tu-dortmund.de>
2010-11-03 09:16:43 +0100

    ENH: renamed top-level namespace
    * renamed top-level namespace to 'FEAST'
    * fixed indentation

--- kernel/util/stringify.hpp
+++ kernel/util/stringify.hpp
@@ -13,71 +13,71 @@
  * \author Dirk Ribbrock
  */
 
-namespace Feast
+namespace FEAST
 {
-    /**
-     * Convert item to a string.
-     * \param[in] item
-     * The item to stringify
-     */
-    template <typename T_>
-    std::string stringify(const T_ & item)
-    {
-        std::ostringstream s;
-        s << item;
-        return s.str();
-    }
+  /**
+   * Convert item to a string.
+   * \param[in] item
+   * The item to stringify
+   */
+  template <typename T_>
+  std::string stringify(const T_ & item)
+  {
+    std::ostringstream s;
+    s << item;
+    return s.str();
+  }
 
-    /**
-     * Convert item to a string (overload for std::string).
-     * \param[in] item
-     * The item to stringify
-     */
-    inline std::string stringify(const std::string & item)
-    {
-        return item;
-    }
+  /**
+   * Convert item to a string (overload for std::string).
+   * \param[in] item
+   * The item to stringify
+   */
+  inline std::string stringify(const std::string & item)
+  {
+    return item;
+  }
 
-    /**
-     * Convert item to a string (overload for char).
-     * \param[in] item
-     * The item to stringify
-     */
-    inline std::string stringify(const char & item)
-    {
-        return std::string(1, item);
-    }
+  /**
+   * Convert item to a string (overload for char).
+   * \param[in] item
+   * The item to stringify
+   */
+  inline std::string stringify(const char & item)
+  {
+    return std::string(1, item);
+  }
 
-    /**
-     * Convert item to a string (overload for unsigned char).
-     * \param[in] item
-     * The item to stringify
-     */
-    inline std::string stringify(const unsigned char & item)
-    {
-        return std::string(1, item);
-    }
+  /**
+   * Convert item to a string (overload for unsigned char).
+   * \param[in] item
+   * The item to stringify
+   */
+  inline std::string stringify(const unsigned char & item)
+  {
+    return std::string(1, item);
+  }
 
-    /**
-     * Convert item to a string (overload for bool).
-     * \param[in] item
-     * The item to stringify
-     */
-    inline std::string stringify(const bool & item)
-    {
-        return item ? "true" : "false";
-    }
+  /**
+   * Convert item to a string (overload for bool).
+   * \param[in] item
+   * The item to stringify
+   */
+  inline std::string stringify(const bool & item)
+  {
+    return item ? "true" : "false";
+  }
 
-    /**
-     * Convert item to a string (overload for char *, which isn't a
-     * screwup like other pointers).
-     * \param[in] item
-     * The item to stringify
-     */
-    inline std::string stringify(const char * const item)
-    {
-        return std::string(item);
-    }
-}
+  /**
+   * Convert item to a string (overload for char *, which isn't a
+   * screwup like other pointers).
+   * \param[in] item
+   * The item to stringify
+   */
+  inline std::string stringify(const char * const item)
+  {
+    return std::string(item);
+  }
+} // namespace FEAST
 
-#endif //UTIL_STRINGIFY_HHP
+#endif // UTIL_STRINGIFY_HHP
