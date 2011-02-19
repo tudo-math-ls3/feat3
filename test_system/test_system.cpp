a81a8954d5d6e57c96932b588b396b2b0696a621
Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>
2011-02-19 20:24:51 +0100

    ENH: Choose backend and precision of tests via command line
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>

--- test_system/test_system.cpp
+++ test_system/test_system.cpp
@@ -4,46 +4,60 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <algorithm>
 
 using namespace FEAST;
 using namespace TestSystem;
 
-int main(int /*argc*/, char** /*argv*/)
+int main(int argc, char** argv)
 {
     int result(EXIT_SUCCESS);
-    size_t list_size(0);
 
-    /*for (TestList::Iterator i(TestList::instance()->begin_tests()), i_end(TestList::instance()->end_tests()) ;
-            i != i_end ; )
+    if (argc > 1)
     {
-            ++i;
-    }*/
+      std::list<std::string> labels;
+      for (int i(1) ; i < argc ; ++i)
+      {
+        labels.push_back(argv[i]);
+      }
+      for (TestList::Iterator i(TestList::instance()->begin_tests()), i_end(TestList::instance()->end_tests()) ;
+          i != i_end ; )
+      {
+        if ((find(labels.begin(), labels.end(), (*i)->get_tag_name()) == labels.end()) &&
+            (find(labels.begin(), labels.end(), (*i)->get_prec_name()) == labels.end()))
+        {
+          i = TestList::instance()->erase(i);
+          continue;
+        }
+        ++i;
+      }
+    }
 
-    list_size = TestList::instance()->size();
+    size_t list_size(TestList::instance()->size());
     unsigned long iterator_index(1);
     for (TestList::Iterator i(TestList::instance()->begin_tests()), i_end(TestList::instance()->end_tests()) ;
-            i != i_end ; )
+        i != i_end ; )
     {
-        CONTEXT("When running test case '" + (*i)->id() + "':");
-        try
-        {
-            std::cout << "(" << iterator_index << "/" << list_size << ") " << (*i)->id() + " [Backend: "
-                << (*i)->get_tag_name() << "]" << " [Precision: "<< (*i)->get_prec_name() << "]" << std::endl;
-            (*i)->run();
-            std::cout << "PASSED" << std::endl;
-        }
-        catch (TestFailedException & e)
-        {
-            std::cout << "FAILED: " << (*i)->id() << std::endl << stringify(e.what()) << std::endl;
-            result = EXIT_FAILURE;
-        }
-        catch (InternalError & e)
-        {
-            std::cout << "FAILED: " << (*i)->id() << std::endl << stringify(e.what()) << std::endl << stringify(e.message()) << std::endl;
-            result = EXIT_FAILURE;
-        }
-        i = TestList::instance()->erase(i);
-        iterator_index++;
+      CONTEXT("When running test case '" + (*i)->id() + "':");
+      try
+      {
+        std::cout << "(" << iterator_index << "/" << list_size << ") " << (*i)->id() + " [Backend: "
+          << (*i)->get_tag_name() << "]" << " [Precision: "<< (*i)->get_prec_name() << "]" << std::endl;
+        (*i)->run();
+        std::cout << "PASSED" << std::endl;
+      }
+      catch (TestFailedException & e)
+      {
+        std::cout << "FAILED: " << (*i)->id() << std::endl << stringify(e.what()) << std::endl;
+        result = EXIT_FAILURE;
+      }
+      catch (InternalError & e)
+      {
+        std::cout << "FAILED: " << (*i)->id() << std::endl << stringify(e.what()) << std::endl << stringify(e.message()) << std::endl;
+        result = EXIT_FAILURE;
+      }
+      i = TestList::instance()->erase(i);
+      iterator_index++;
     }
 
     return result;
