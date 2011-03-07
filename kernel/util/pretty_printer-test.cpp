2ccf141f759e46e1eb9a8e73e82cb6025b6e35b1
Hilmar Wobker <hilmar.wobker@math.tu-dortmund.de>
2011-03-07 15:46:36 +0100

    ENH: test for PrettyPrinter class
    Signed-off-by: Hilmar Wobker <hilmar.wobker@math.tu-dortmund.de>

---
+++ kernel/util/pretty_printer-test.cpp
@@ -0,0 +1,78 @@
+// includes, system
+#include <iostream>
+#include <string>
+
+// includes, Feast
+#include <kernel/base_header.hpp>
+#include <kernel/util/string_utils.hpp>
+#include <kernel/util/pretty_printer.hpp>
+#include <kernel/util/assertion.hpp>
+#include <test_system/test_system.hpp>
+
+using namespace TestSystem;
+using namespace FEAST;
+
+/**
+* \brief testing the pretty printer
+*
+* \test
+* This tests the functionality of the pretty printer.
+*
+* \author Hilmar Wobker
+*/
+template <typename Tag_, typename DT_>
+class PrettyPrinterTest
+  : public TaggedTest<Tag_, DT_>
+{
+
+public:
+
+  /// CTOR
+  PrettyPrinterTest()
+    : TaggedTest<Tag_, DT_>("pretty_printer_test")
+  {
+  }
+
+
+  /// main routine
+  void run() const
+  {
+    CONTEXT("PrettyPrinterTest::run()");
+
+    // test the PrettyPrinter
+    std::string prefix(std::string("Proc 42"));
+    PrettyPrinter pp(40, '#', prefix + " ");
+    pp.add_line_sep();
+    pp.add_line_centered("Testing the pretty printer");
+    pp.add_line_sep();
+    pp.add_line("left bla blub");
+    pp.add_line("too long too long too long too long too long too long");
+    pp.add_line_no_right_delim("also too long too long too long too long too long too long");
+    pp.add_line("left bla blub");
+    pp.add_line_sep();
+
+    // print it like this
+    std::cout << pp.block();
+
+    // modify the pretty printer a little bit:
+    pp.reset_block();
+    pp.set_width(60);
+    pp.set_prefix("WUPP ");
+    pp.set_delim('*');
+    pp.add_line_sep();
+    pp.add_line_centered("Testing the pretty printer once again");
+    pp.add_line_sep();
+    pp.add_line_centered("bla");
+    pp.add_line_centered("bla bla");
+    pp.add_line_centered("bla bla bla");
+    pp.add_line_centered("bla bla");
+    pp.add_line_centered("bla");
+    pp.add_line_sep();
+
+    // or print it like this
+    pp.print(std::cout);
+  } // run()
+}; // PrettyPrinterTest
+
+// create test instance
+PrettyPrinterTest<Nil, Nil> pretty_printer_test;
