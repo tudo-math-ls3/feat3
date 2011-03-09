2a99f1d2a51cd961718b810c4efa19301a0ec535
Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>
2011-03-09 17:16:53 +0100

    ENH: Example on regex filtering in buildid
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>

--- cmake_modules/buildid.cmake
+++ cmake_modules/buildid.cmake
@@ -1,6 +1,7 @@
 #add build_id logic to the build system
 
 set (BUILD_ID "" CACHE STRING "Use specific build id or NONE or empty for autodetection")
+
 if (BUILD_ID STREQUAL "NONE")
   #do nothing, use env variable or whatever the user provides
 elseif (BUILD_ID STREQUAL "")
@@ -15,5 +16,23 @@ elseif (BUILD_ID STREQUAL "")
   #set (DEBUG_BUILD ON)
 else ()
   #use specific id
-  message ("Build ID not known!")
+  set (CMAKE_CXX_COMPILER "")
+  set (CMAKE_CXX_COMPILER_ARG1 "")
+  set (CMAKE_CXX_FLAGS "")
+  set (CMAKE_SHARED_LINKER_FLAGS "")
+  set (CMAKE_EXE_LINKER_FLAGS "")
+  set (CMAKE_MODULE_LINKER_FLAGS "")
+
+  ###########################
+  #   MPI Environments      #
+  ###########################
+
+  # TODO check if --version returns the expected environment
+  if (BUILD_ID MATCHES ".*ompi.*")
+    set (CMAKE_CXX_COMPILER "mpic++")
+  elseif (BUILD_ID MATCHES ".*mpich.*")
+    set (CMAKE_CXX_COMPILER "mpic++")
+  else ()
+    message (FATAL_ERROR "ERROR: Build ID '${BUILD_ID}' does not contain a supported MPI environment!")
+  endif ()
 endif ()
