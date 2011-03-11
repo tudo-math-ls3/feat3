df308d2f2322372311e3fadfe0282ad9174e7e04
Dominik Goeddeke <dominik.goeddeke@math.tu-dortmund.de>
2011-03-11 17:24:50 +0100

    ENH: Added compiler (version) detection for Intel
    Signed-off-by: Dominik Goeddeke <dominik.goeddeke@math.tu-dortmund.de>

--- kernel/util/compiler_intel.hpp
+++ kernel/util/compiler_intel.hpp
@@ -3,16 +3,35 @@
 /// Header guard
 #define UTIL_COMPILER_INTEL_HPP
 
-// Compiler detection header for Intel C++ compiler.
-
-/// \todo implement compiler detection for Intel Compiler Suite
+/**
+ * \file compiler_gnu.hpp
+ *
+ * \brief Compiler detection header for Intel C++ compiler.
+ *
+ * \author Dominik Goeddeke
+ */
 
 #if !defined(FEAST_COMPILER) && defined(__INTEL_COMPILER)
 
 // define FEAST_COMPILER_INTEL macro
-#  define FEAST_COMPILER_INTEL 1
+// Note that __ICC is already linear sortable
+#  define FEAST_COMPILER_INTEL __ICC
 
-#  define FEAST_COMPILER "Intel C/C++ compiler"
+// map version to human-readable string
+#  if (__ICC >= 1200)
+#    define FEAST_COMPILER "Intel C/C++ compiler 12.0"
+#  elif (__ICC >= 1110)
+#    define FEAST_COMPILER "Intel C/C++ compiler 11.1"
+#  elif (__ICC >= 1100)
+#    define FEAST_COMPILER "Intel C/C++ compiler 11.0"
+#  elif (__ICC >= 1010)
+#    define FEAST_COMPILER "Intel C/C++ compiler 10.1"
+#  elif (__ICC >= 1000)
+#    define FEAST_COMPILER "Intel C/C++ compiler 10.0"
+#  else
+// too old to have a chance to support FEAST anyway
+#    define FEAST_COMPILER "Intel C/C++ compiler"
+#  endif
 
 #endif // !defined(FEAST_COMPILER) && defined(__INTEL_COMPILER)
 
