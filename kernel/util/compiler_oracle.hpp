50473a1f62e569817b98c8ec27f9ebe92f780231
Dominik Goeddeke <dominik.goeddeke@math.tu-dortmund.de>
2011-03-11 19:59:02 +0100

    ENH: Added compiler detection for Sun/OracleStudio
    Signed-off-by: Dominik Goeddeke <dominik.goeddeke@math.tu-dortmund.de>

---
+++ kernel/util/compiler_oracle.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef UTIL_COMPILER_ORACLE_HPP
+/// Header guard
+#define UTIL_COMPILER_ORACLE_HPP
+
+/**
+ * \file compiler_oracle.hpp
+ *
+ * \brief Compiler detection header for SunStudio/OracleStudio compilers.
+ *
+ * \author Dominik Goeddeke
+ */
+
+#if !defined(FEAST_COMPILER) && defined(__SUNPRO_CC)
+
+// define FEAST_COMPILER_ORACLE macro
+#  define FEAST_COMPILER_ORACLE __SUNPRO_CC
+
+// map version to human-readable string and add a few
+// C++0x-specific details.
+#  if (__SUNPRO_CC == 0x5110)
+//   SunStudio does not support nullptr, do not define HAVE_CPP0X_NULLPTR 1
+#    define FEAST_COMPILER "SunStudio/OracleStudio C/C++ compiler 12.2"
+#  else
+#    define FEAST_COMPILER "SunStudio/OracleStudio C/C++ compiler (unknownn version)"
+#  endif
+
+#endif // !defined(FEAST_COMPILER) && defined(__SUNPRO_CC)
+
+#endif // UTIL_COMPILER_ORACLE_HPP
