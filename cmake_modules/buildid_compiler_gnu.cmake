fdb7e5958388279f2c3e79d8551fd7f25507da75
Dominik Goeddeke <dominik.goeddeke@math.tu-dortmund.de>
2011-04-28 21:59:17 +0200

    ENH: GCC on Linux creates 64-bit code
    Signed-off-by: Dominik Goeddeke <dominik.goeddeke@math.tu-dortmund.de>

--- cmake_modules/buildid_compiler_gnu.cmake
+++ cmake_modules/buildid_compiler_gnu.cmake
@@ -109,6 +109,7 @@ if (FEAST_CXX_FLAGS_INTERNAL STREQUAL "")
 
   if (FEAST_DEBUG_MODE)
     # unoptimised settings for all archs
+    # the following flag might be useful: -m128bit-long-double
     set (FEAST_CXX_FLAGS_INTERNAL "${FEAST_CXX_FLAGS_INTERNAL} -O0 -std=c++0x -pedantic -Wall -Wextra -Wundef -g")
 
   else ()
@@ -117,6 +118,7 @@ if (FEAST_CXX_FLAGS_INTERNAL STREQUAL "")
     # -ffast-math turns on additional non-IEEE-compliant optimisations
     # -mfpmath=sse forces SSE math rather than FPU (i387) floating point math
     # the other default flags are self-explanatory and are not included in -O3
+    # the following flag might be useful: -m128bit-long-double
     set (FEAST_CXX_FLAGS_INTERNAL "-O3 -ffast-math -foptimize-register-move -fprefetch-loop-arrays -funroll-loops -mfpmath=sse")
 
     # please try to maintain the same order as in the buildid_arch module
