ec35dcedfcfef0acea427815e0df8d02d8bb987b
Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>
2011-10-05 15:16:02 +0200

    ENH: Introduced cpp files in CMakeLists files
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>

--- kernel/util/exception.hpp
+++ kernel/util/exception.hpp
@@ -16,39 +16,8 @@
 namespace FEAST
 {
 #ifndef FEAST_NO_CONTEXT
-  /// The global context stack.
-  /// \todo Ist der stack global oder compile-unit lokal?
-  std::list<std::string> * context_stack = 0;
 
-  /**
-  * \brief This structs holds the actual context history
-  *
-  * \author Dirk Ribbrock
-  */
-  struct ContextData
-  {
-    /// The local context stack
-    std::list<std::string> local_context_stack;
-
-    /// CTOR
-    ContextData()
-    {
-      if (context_stack)
-      {
-        local_context_stack.assign(context_stack->begin(), context_stack->end());
-      }
-    }
-
-    /// returns a full context stack aka backtrace
-    std::string backtrace(const std::string& delimiter) const
-    {
-      if (context_stack)
-      {
-        return join_strings(local_context_stack.begin(), local_context_stack.end(), delimiter);
-      }
-      else return "";
-    }
-  };
+  struct ContextData;
 #endif // FEAST_NO_CONTEXT
 
   /**
@@ -78,72 +47,28 @@ namespace FEAST
     * \param message
     * the exception's message
     */
-    Exception(const std::string & message) :
-#ifndef FEAST_NO_CONTEXT
-      _context_data(new ContextData),
-#endif // FEAST_NO_CONTEXT
-      _message(message)
-    {
-    }
+    Exception(const std::string & message);
 
     /// copy CTOR
-    Exception(const Exception & other) :
-      std::exception(other),
-#ifndef FEAST_NO_CONTEXT
-      _context_data(new ContextData(*other._context_data)),
-#endif // FEAST_NO_CONTEXT
-      _message(other._message)
-    {
-    }
+    Exception(const Exception & other);
 
   public:
     /// DTOR
-    virtual ~Exception() throw()
-    {
-#ifndef FEAST_NO_CONTEXT
-      delete _context_data;
-#endif // FEAST_NO_CONTEXT
-    }
+    virtual ~Exception() throw();
 
     /// returns error message
-    const std::string message() const
-    {
-      return _message;
-    }
+    const std::string message() const;
 
 #ifndef FEAST_NO_CONTEXT
     /// returns backtrace
-    std::string backtrace(const std::string & delimiter) const
-    {
-      return _context_data->backtrace(delimiter);
-    }
+    std::string backtrace(const std::string & delimiter) const;
 #endif // FEAST_NO_CONTEXT
 
     /// returns true if the backtrace is empty
     bool empty() const;
 
     /// return descriptive exception name
-    const char * what() const throw()
-    {
-      /// \todo Add a working win32 alternative (see http://www.int0x80.gr/papers/name_mangling.pdf)
-      /*if (_what_str.empty())
-      {
-        int status(0);
-        char * const name(abi::__cxa_demangle(("_Z" + stringify(std::exception::what())).c_str(), 0, 0, &status));
-        if (0 == status)
-        {
-          _what_str = name;
-          _what_str += " (" + message() + ")";
-          std::free(name);
-        }
-      }*/
-      if (_what_str.empty())
-      {
-        _what_str = stringify(std::exception::what());
-        _what_str += " (" + message() + ")";
-      }
-      return _what_str.c_str();
-    }
+    const char * what() const throw();
   };
 
 
@@ -191,30 +116,10 @@ namespace FEAST
     * \param context
     * description of the context
     */
-    Context(const char * const file, const long line, const std::string & context)
-    {
-      if (! context_stack)
-      {
-        context_stack = new std::list<std::string>;
-      }
-
-      context_stack->push_back(context + " (" + stringify(file) + ":" + stringify(line) +")");
-    }
+    Context(const char * const file, const long line, const std::string & context);
 
     /// DTOR
-    ~Context()
-    {
-      if (! context_stack)
-        throw InternalError("no context!");
-
-      context_stack->pop_back();
-
-      if (context_stack->empty())
-      {
-        delete context_stack;
-        context_stack = 0;
-      }
-    }
+    ~Context();
 
     /**
     * \brief Current context
@@ -222,13 +127,7 @@ namespace FEAST
     * \param[in] delimiter
     * A delimiter added between to context strings
     */
-    static std::string backtrace(const std::string & delimiter)
-    {
-      if (! context_stack)
-        return "";
-
-      return join_strings(context_stack->begin(), context_stack->end(), delimiter);
-    }
+    static std::string backtrace(const std::string & delimiter);
   };
 #endif // FEAST_NO_CONTEXT
 
