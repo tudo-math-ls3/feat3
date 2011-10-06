tudo-math-ls3/feat3

commit cc69197bcc54a779c87466ffd7f501d4a2a42f53

FIX: replaced unsafe bool type-cast
Signed-off-by: Peter Zajac <pzajac@math.tu-dortmund.de>

kernel/manager_comp.hpp
@@ -240,7 +240,7 @@ namespace FEAST
 
         if(belongs_to_work_group()[igroup])
         {
-          _work_groups[igroup] = new WorkGroupExt(comm, igroup, (bool)_group_contains_extra_coord[igroup],
+          _work_groups[igroup] = new WorkGroupExt(comm, igroup, _group_contains_extra_coord[igroup] == 0,
                                                   _process_group->is_coordinator());
         }
         // free the group again

kernel/manager_comp_non_coord.hpp
@@ -306,7 +306,7 @@ namespace FEAST
             + ManagerComp<space_dim_, world_dim_>::_interlevel_groups[igroup]->test_communication();
         }
       }
-      return work_group_test_ok + interlevel_group_test_ok;
+      return (work_group_test_ok + interlevel_group_test_ok) == 0;
     } // _test_communication
 
 

commit e5c20a95ca25073005afb02384db9502abe946ec

FIX: serious bug
This bug was introduced in commit cc69197bcc54a779c87466ffd7f501d4a2a42f53. Thanks to Dirk for his help finding this one.

Signed-off-by: Peter Zajac <pzajac@math.tu-dortmund.de>

kernel/manager_comp.hpp
@@ -240,7 +240,7 @@ namespace FEAST
 
         if(belongs_to_work_group()[igroup])
         {
-          _work_groups[igroup] = new WorkGroupExt(comm, igroup, _group_contains_extra_coord[igroup] == 0,
+          _work_groups[igroup] = new WorkGroupExt(comm, igroup, _group_contains_extra_coord[igroup] != 0,
                                                   _process_group->is_coordinator());
         }
         // free the group again