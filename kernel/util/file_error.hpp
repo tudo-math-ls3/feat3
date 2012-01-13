650a4f1014c948809d1b7d10678a8b656f707f03
Peter Zajac <pzajac@math.tu-dortmund.de>
2012-01-13 11:48:44 +0100

    ENH: added classes for file-related exceptions
    Signed-off-by: Peter Zajac <pzajac@math.tu-dortmund.de>

---
+++ kernel/util/file_error.hpp
@@ -0,0 +1,52 @@
+#pragma once
+#ifndef KERNEL_UTIL_FILE_ERROR_HPP
+#define KERNEL_UTIL_FILE_ERROR_HPP 1
+
+// includes, FEAST
+#include <kernel/util/exception.hpp>
+
+namespace FEAST
+{
+  /**
+   * \brief Base class for file related errors
+   * \author Peter Zajac
+   */
+  class FileError :
+    public Exception
+  {
+  public:
+    /**
+     * \brief Constructor
+     * \param[in] message
+     * The error message.
+     */
+    explicit FileError(const String& message) :
+      Exception(message)
+    {
+    }
+  }; // class FileError
+
+  /**
+   * \brief File-Not-Found exception
+   *
+   * This exception is thrown when a file was not found.
+   *
+   * \author Peter Zajac
+   */
+  class FileNotFound :
+    public FileError
+  {
+  public:
+    /**
+     * \brief Constructor
+     * \param[in] filename
+     * The name (and path) of the file that was not found.
+     */
+    explicit FileNotFound(const String& filename) :
+      FileError("File not found: '" + filename + "'")
+    {
+    }
+  }; // class FileNotFound
+} // namespace FEAST
+
+#endif // KERNEL_UTIL_FILE_ERROR_HPP
