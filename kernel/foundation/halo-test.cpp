02c6502343f3f531ec3ac3580ec721958c7f32d6
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2012-05-18 18:29:18 +0200

    ENH: (New Foundation:) Added halo class and test
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

---
+++ kernel/foundation/halo-test.cpp
@@ -0,0 +1,118 @@
+#include <kernel/base_header.hpp>
+#include <test_system/test_system.hpp>
+
+#include <kernel/foundation/mesh.hpp>
+#include <kernel/foundation/halo.hpp>
+#include<deque>
+
+using namespace FEAST;
+using namespace FEAST::TestSystem;
+
+template<typename Tag_, typename IndexType_, template<typename, typename> class OT_, typename IT_>
+class HaloTest:
+  public TaggedTest<Tag_, IndexType_>
+{
+  public:
+    HaloTest(const std::string & tag) :
+      TaggedTest<Tag_, IndexType_>("HaloTest<" + tag + ">")
+    {
+    }
+
+    void run() const
+    {
+
+      //##################################################################
+      //     0  1
+      //   0--1--2     *--*--*
+      // 2 | 3|  |4    | 0| 1|
+      //   3--4--5     *--*--*
+      //    5  6
+
+      Foundation::Mesh<Foundation::rnt_2D, Foundation::Topology<IndexType_, OT_, IT_> > m3(0);
+
+      //configure attribute
+      unsigned my_attribute_index(Foundation::MeshAttributeRegistration<Foundation::Mesh<Foundation::rnt_2D, Foundation::Topology<IndexType_, OT_, IT_> >, double>::execute(m3, Foundation::pl_vertex));
+
+      //add vertices
+      m3.add_polytope(Foundation::pl_vertex);
+      m3.add_polytope(Foundation::pl_vertex);
+      m3.add_polytope(Foundation::pl_vertex);
+      m3.add_polytope(Foundation::pl_vertex);
+      m3.add_polytope(Foundation::pl_vertex);
+      m3.add_polytope(Foundation::pl_vertex);
+      m3.add_attribute_value(my_attribute_index, double(0));
+      m3.add_attribute_value(my_attribute_index, double(0.5));
+      m3.add_attribute_value(my_attribute_index, double(1));
+      m3.add_attribute_value(my_attribute_index, double(0));
+      m3.add_attribute_value(my_attribute_index, double(0.5));
+      m3.add_attribute_value(my_attribute_index, double(1));
+
+      //add edges
+      m3.add_polytope(Foundation::pl_edge);
+      m3.add_polytope(Foundation::pl_edge);
+      m3.add_polytope(Foundation::pl_edge);
+      m3.add_polytope(Foundation::pl_edge);
+      m3.add_polytope(Foundation::pl_edge);
+      m3.add_polytope(Foundation::pl_edge);
+      m3.add_polytope(Foundation::pl_edge);
+
+      //add faces
+      m3.add_polytope(Foundation::pl_face);
+      m3.add_polytope(Foundation::pl_face);
+
+      m3.add_adjacency(Foundation::pl_edge, Foundation::pl_vertex, 0, 0); //v->e is set automagically
+      m3.add_adjacency(Foundation::pl_edge, Foundation::pl_vertex, 0, 1);
+      m3.add_adjacency(Foundation::pl_edge, Foundation::pl_vertex, 1, 1);
+      m3.add_adjacency(Foundation::pl_edge, Foundation::pl_vertex, 1, 2);
+      m3.add_adjacency(Foundation::pl_edge, Foundation::pl_vertex, 2, 0);
+      m3.add_adjacency(Foundation::pl_edge, Foundation::pl_vertex, 2, 3);
+      m3.add_adjacency(Foundation::pl_edge, Foundation::pl_vertex, 3, 1);
+      m3.add_adjacency(Foundation::pl_edge, Foundation::pl_vertex, 3, 4);
+      m3.add_adjacency(Foundation::pl_edge, Foundation::pl_vertex, 4, 2);
+      m3.add_adjacency(Foundation::pl_edge, Foundation::pl_vertex, 4, 5);
+      m3.add_adjacency(Foundation::pl_edge, Foundation::pl_vertex, 5, 3);
+      m3.add_adjacency(Foundation::pl_edge, Foundation::pl_vertex, 5, 4);
+      m3.add_adjacency(Foundation::pl_edge, Foundation::pl_vertex, 6, 4);
+      m3.add_adjacency(Foundation::pl_edge, Foundation::pl_vertex, 6, 5);
+
+      m3.add_adjacency(Foundation::pl_face, Foundation::pl_edge, 0, 0);
+      m3.add_adjacency(Foundation::pl_face, Foundation::pl_edge, 0, 2);
+      m3.add_adjacency(Foundation::pl_face, Foundation::pl_edge, 0, 3);
+      m3.add_adjacency(Foundation::pl_face, Foundation::pl_edge, 0, 5);
+      m3.add_adjacency(Foundation::pl_face, Foundation::pl_edge, 1, 1);
+      m3.add_adjacency(Foundation::pl_face, Foundation::pl_edge, 1, 3);
+      m3.add_adjacency(Foundation::pl_face, Foundation::pl_edge, 1, 4);
+      m3.add_adjacency(Foundation::pl_face, Foundation::pl_edge, 1, 6);
+
+      //clone mesh
+      Foundation::Mesh<Foundation::rnt_2D, Foundation::Topology<IndexType_, OT_, IT_> > m4(1, m3);
+
+      //init simple halo
+      Foundation::Halo<0, Foundation::Mesh<Foundation::rnt_2D, Foundation::Topology<IndexType_, OT_, IT_> > > h(m3, 1);
+
+      //add connections
+      //
+      // *--*--*
+      // |0 | 1| m3
+      // *--*--*
+      //  5   6
+      //  |   |
+      //  0   1
+      // *--*--*
+      // |0 | 1| m4
+      // *--*--*
+
+      h.add_halo_element_pair(5u, 0u);
+      h.add_halo_element_pair(6u, 1u);
+
+      TEST_CHECK_EQUAL(h.size(), 2u);
+      TEST_CHECK_EQUAL(h.get_element(0u), 5u);
+      TEST_CHECK_EQUAL(h.get_element(1u), 6u);
+      TEST_CHECK_EQUAL(h.get_element_counterpart(0u), 0u);
+      TEST_CHECK_EQUAL(h.get_element_counterpart(1u), 1u);
+    }
+};
+HaloTest<Nil, Index, std::vector, std::vector<Index> > halo_test_cpu_v_v("std::vector, std::vector");
+HaloTest<Nil, Index, std::deque, std::vector<Index> > halo_test_cpu_d_v("std::deque, std::vector");
+HaloTest<Nil, Index, std::vector, std::deque<Index> > halo_test_cpu_v_d("std::vector, std::deque");
+HaloTest<Nil, Index, std::deque, std::deque<Index> > halo_test_cpu_d_d("std::deque, std::deque");
---
+++ kernel/foundation/halo.hpp
@@ -0,0 +1,86 @@
+#pragma once
+#ifndef KERNEL_FOUNDATION_HALO_HH
+#define KERNEL_FOUNDATION_HALO_HH 1
+
+#include<vector>
+
+namespace FEAST
+{
+  namespace Foundation
+  {
+    template<unsigned delta_, typename MeshType_, template<typename, typename> class StorageType_ = std::vector, typename IndexType_ = unsigned long>
+    class Halo
+    {
+      public:
+        typedef IndexType_ index_type_;
+        typedef MeshType_ mesh_type_;
+        typedef typename MeshType_::attr_type_1_ attr_type_1_;
+        typedef typename MeshType_::attr_type_2_ attr_type_2_;
+        typedef typename MeshType_::attr_type_3_ attr_type_3_;
+
+        Halo(MeshType_ & mesh, IndexType_ other) :
+          _halo_elements(StorageType_<IndexType_, std::allocator<IndexType_> >()),
+          _halo_element_counterparts(StorageType_<IndexType_, std::allocator<IndexType_> >()),
+          _mesh(mesh),
+          _other(other),
+          _overlap(delta_)
+      {
+      }
+
+        ~Halo()
+        {
+        }
+
+        ///Add correspondence of i
+        void add_halo_element_pair(IndexType_ i, IndexType_ j)
+        {
+          _halo_elements.push_back(i);
+          _halo_element_counterparts.push_back(j);
+        }
+
+        IndexType_ get_element_counterpart(IndexType_ index)
+        {
+          return _halo_element_counterparts.at(index);
+        }
+
+        IndexType_ get_element(IndexType_ index)
+        {
+          return _halo_elements.at(index);
+        }
+
+        IndexType_ size()
+        {
+          return _halo_elements.size();
+        }
+
+        MeshType_ & get_mesh()
+        {
+          return _mesh;
+        }
+
+        IndexType_ get_other()
+        {
+          return _other;
+        }
+
+        unsigned get_overlap()
+        {
+          return _overlap;
+        }
+
+      private:
+        StorageType_<IndexType_, std::allocator<IndexType_> > _halo_elements;
+        StorageType_<IndexType_, std::allocator<IndexType_> > _halo_element_counterparts;
+
+        MeshType_ & _mesh;
+        IndexType_ _other;
+
+        unsigned _overlap;
+    };
+
+  }
+
+}
+
+
+#endif
