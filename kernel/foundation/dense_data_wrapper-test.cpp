5b9feb0e2df1910d184ae08c686660a1b2251a0a
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2012-05-18 13:48:40 +0200

    ENH: (New Foundation:) Added dense data wrapper and interim test
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

---
+++ kernel/foundation/dense_data_wrapper-test.cpp
@@ -0,0 +1,57 @@
+#include <kernel/base_header.hpp>
+#include <test_system/test_system.hpp>
+
+#include <kernel/foundation/dense_data_wrapper.hpp>
+#include<deque>
+
+using namespace FEAST;
+using namespace FEAST::TestSystem;
+
+//Test container
+template<typename DT_>
+class TestArrayClass
+{
+  public:
+    TestArrayClass(Index size) :
+      _size(size),
+      _data(new DT_[size])
+    {
+    }
+
+    ~TestArrayClass()
+    {
+      delete[] _data;
+    }
+
+    DT_ & operator[] (Index i)
+    {
+      return _data[i];
+    }
+
+    Index size()
+    {
+      return _size;
+    }
+
+  private:
+    Index _size;
+    DT_ * _data;
+
+};
+
+template<typename Tag_, typename DataType_, template<typename> class ContType_ = TestArrayClass>
+class DenseDataWrapperTest:
+  public TaggedTest<Tag_, DataType_>
+{
+  public:
+    DenseDataWrapperTest(const std::string & tag) :
+      TaggedTest<Tag_, DataType_>("DenseDataWrapperTest<" + tag + ">")
+    {
+    }
+
+    void run() const
+    {
+      Foundation::DenseDataWrapper<15u, DataType_, ContType_> test;
+    }
+};
+DenseDataWrapperTest<Nil, double> ddw_test("Array<double>");
---
+++ kernel/foundation/dense_data_wrapper.hpp
@@ -0,0 +1,62 @@
+#pragma once
+#ifndef KERNEL_FOUNDATION_DENSE_DATA_WRAPPER_HH
+#define KERNEL_FOUNDATION_DENSE_DATA_WRAPPER_HH 1
+
+namespace FEAST
+{
+  namespace Foundation
+  {
+    template<unsigned long _i,
+             typename DT_,
+             template<typename> class ContType_>
+      class DenseDataWrapper
+      {
+        public:
+          DenseDataWrapper() :
+            _size(_i),
+            _num_non_zeros(0),
+            _data(ContType_<DT_>(_i))
+        {
+        }
+
+          ~DenseDataWrapper()
+          {
+          }
+
+          unsigned long size()
+          {
+            return _num_non_zeros;
+          }
+
+          unsigned long capacity()
+          {
+            return _size - _num_non_zeros;
+          }
+
+          void push_back(DT_ value)
+          {
+            //todo capacity check
+            _data[_num_non_zeros] = value;
+            ++_num_non_zeros;
+          }
+
+          DT_ & at(unsigned long i)
+          {
+            //todo in non-zero range check
+            return _data[i];
+          }
+
+          DT_ & operator[](unsigned long i)
+          {
+            //todo in non-zero range check
+            return _data[i];
+          }
+
+        private:
+          unsigned long _size;
+          unsigned long _num_non_zeros;
+          ContType_<DT_> _data;
+      };
+  }
+}
+#endif
