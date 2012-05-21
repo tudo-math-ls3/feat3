3fc8bade0fdd3e3bc82570e25a4d46d6c7c56b3d
Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>
2012-05-21 12:44:23 +0200

    ENH: Added DenseMatrix container class
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>

---
+++ kernel/hornet/dense_matrix-test.cpp
@@ -0,0 +1,53 @@
+// This test needs DEBUG defined.
+#ifndef DEBUG
+#define DEBUG 1
+#endif
+
+#include <kernel/base_header.hpp>
+#include <test_system/test_system.hpp>
+#include <kernel/hornet/dense_matrix.hpp>
+
+using namespace FEAST;
+using namespace FEAST::TestSystem;
+
+/**
+* \brief Test class for the assertion class.
+*
+* \test test description missing
+*
+* \tparam Tag_
+* description missing
+*
+* \tparam DT_
+* description missing
+*
+* \author Dirk Ribbrock
+*/
+template<
+  typename Tag_,
+  typename DT_>
+class DenseMatrixTest
+  : public TaggedTest<Tag_, DT_>
+{
+
+public:
+
+  DenseMatrixTest()
+    : TaggedTest<Tag_, DT_>("dense_vector_test")
+  {
+  }
+
+  virtual void run() const
+  {
+    DenseMatrix<Tag_, DT_> a(10, 10);
+    DenseMatrix<Tag_, DT_> b(10, 10, 5.);
+    DenseMatrix<Tag_, DT_> c(b);
+    DenseMatrix<int, DT_> d(b);
+    TEST_CHECK_EQUAL(c.size(), b.size());
+    TEST_CHECK_EQUAL(c.size(), b.size());
+    DenseMatrix<Tag_, DT_> e(11, 12, 5.);
+    TEST_CHECK_EQUAL(e.rows(), 11);
+    TEST_CHECK_EQUAL(e.columns(), 12);
+  }
+};
+DenseMatrixTest<Nil, double> dense_matrix_test_double;
---
+++ kernel/hornet/dense_matrix.hpp
@@ -0,0 +1,100 @@
+#pragma once
+#ifndef KERNEL_HORNET_DENSE_MATRIX_HPP
+#define KERNEL_HORNET_DENSE_MATRIX_HPP 1
+
+// includes, FEAST
+#include <kernel/base_header.hpp>
+#include <kernel/hornet/container.hpp>
+
+
+namespace FEAST
+{
+  template <typename Arch_, typename DT_>
+  class DenseMatrix : public Container<Arch_, DT_>
+  {
+    private:
+      DT_ * _pelements;
+      Index _rows;
+      Index _columns;
+
+    public:
+      typedef DT_ data_type;
+
+      DenseMatrix(Index rows, Index columns) :
+        Container<Arch_, DT_>(rows * columns)
+      {
+        this->_size = rows * columns;
+        this->_rows = rows;
+        this->_columns = columns;
+
+        //TODO add arch
+        this->_elements.push_back((DT_*)MemoryArbiter::instance()->allocate_memory(this->_size * sizeof(DT_)));
+        _pelements = this->_elements.at(0);
+      }
+
+      DenseMatrix(Index rows, Index columns, DT_ value) :
+        Container<Arch_, DT_>(rows * columns)
+      {
+        this->_size = rows * columns;
+        this->_rows = rows;
+        this->_columns = columns;
+        //TODO add arch
+        this->_elements.push_back((DT_*)MemoryArbiter::instance()->allocate_memory(this->_size * sizeof(DT_)));
+        _pelements = this->_elements.at(0);
+
+        //TODO add arch, use memory arbiter set memory function
+        for (Index i(0) ; i < this->_size ; ++i)
+        {
+          _pelements[i] = value;
+        }
+      }
+
+      DenseMatrix(const DenseMatrix<Arch_, DT_> & other) :
+        Container<Arch_, DT_>(other)
+      {
+      }
+
+      template <typename Arch2_, typename DT2_>
+      DenseMatrix(const DenseMatrix<Arch2_, DT2_> & other) :
+        Container<Arch_, DT_>(other)
+      {
+      }
+
+      void operator()(Index index, DT_ val)
+      {
+        _pelements[index] = val;
+      }
+
+      void operator()(Index row, Index col, DT_ val)
+      {
+        _pelements[row * this->rows + col] = val;
+      }
+
+      const DT_ & operator[](Index index) const
+      {
+        return _pelements[index];
+      }
+
+      /*const DT_ & operator[](Index row, Index col) const
+      {
+        return _pelements[row * this->rows + col];
+      }*/
+
+      virtual const Index & size() const
+      {
+        return this->_size;
+      }
+
+      virtual const Index & rows() const
+      {
+        return this->_rows;
+      }
+
+      virtual const Index & columns() const
+      {
+        return this->_columns;
+      }
+  };
+} // namespace FEAST
+
+#endif // KERNEL_HORNET_DENSE_VECTOR_HPP
