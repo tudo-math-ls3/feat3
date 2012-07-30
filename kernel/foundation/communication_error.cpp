8e608b0b4a18cb24a0bad21a6b62bad24afba4d3
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2012-07-30 14:13:17 +0200

    FIX: Added missing file
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

---
+++ kernel/foundation/communication_error.cpp
@@ -0,0 +1,15 @@
+#include <kernel/foundation/communication_error.hpp>
+#include <string>
+
+using namespace FEAST;
+
+CommunicationError::CommunicationError(const std::string & message) throw () :
+    Exception(message)
+{
+}
+
+CommunicationHaloOverlapMismatch::CommunicationHaloOverlapMismatch(Index comm_overlap, Index halo_overlap) throw () :
+    CommunicationError("Comm overlap '" + stringify(comm_overlap) + "' does not match halo overlap '"
+            + stringify(halo_overlap) + "'")
+{
+}
