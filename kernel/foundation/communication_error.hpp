63ce47b4e191532231f841a85a3d9d4b2f1d1b48
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2012-07-30 14:14:37 +0200

    FIX: Added missing file
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

---
+++ kernel/foundation/communication_error.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef FEM_GUARD_COMMUNICATION_ERROR_HH
+#define FEM_GUARD_COMMUNICATION_ERROR_HH 1
+
+#include <kernel/util/exception.hpp>
+
+#include <string>
+
+namespace FEAST
+{
+    class CommunicationError :
+        public Exception
+    {
+        public:
+            CommunicationError(const std::string & message) throw ();
+    };
+
+    class CommunicationHaloOverlapMismatch :
+        public CommunicationError
+    {
+        public:
+            CommunicationHaloOverlapMismatch(Index comm_overlap, Index halo_overlap) throw ();
+    };
+}
+
+#endif
