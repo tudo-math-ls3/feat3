c8fb13bcd50f6e3dc28984255d8acb2b2c1f3b07
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2012-08-30 10:40:17 +0200

    ENH: added transfer of vertex-coords into vertex_set
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

--- kernel/foundation/mesh-test.cpp
+++ kernel/foundation/mesh-test.cpp
@@ -664,6 +664,31 @@ class MeshTestGeometryInterface:
 
     void run() const
     {
+      /*(0,1) (1,1)
+       *  *----*
+       *  |    |
+       *  |    |
+       *  *----*
+       *(0,0) (1,0)
+       */
+
+      //create attributes for vertex coords
+      std::vector<Foundation::SmartPointer<Foundation::AttributeBase<OT_> > > attrs;
+      attrs.push_back(Foundation::SmartPointer<Foundation::AttributeBase<OT_> >(new Foundation::Attribute<double, OT_>)); //vertex x-coords
+      attrs.push_back(Foundation::SmartPointer<Foundation::AttributeBase<OT_> >(new Foundation::Attribute<double, OT_>)); //vertex y-coords
+
+      ((Foundation::Attribute<double, OT_>*)(attrs.at(0).get()))->get_data().push_back(double(double(0)));
+      ((Foundation::Attribute<double, OT_>*)(attrs.at(1).get()))->get_data().push_back(double(double(0)));
+
+      ((Foundation::Attribute<double, OT_>*)(attrs.at(0).get()))->get_data().push_back(double(double(1)));
+      ((Foundation::Attribute<double, OT_>*)(attrs.at(1).get()))->get_data().push_back(double(double(0)));
+
+      ((Foundation::Attribute<double, OT_>*)(attrs.at(0).get()))->get_data().push_back(double(double(0)));
+      ((Foundation::Attribute<double, OT_>*)(attrs.at(1).get()))->get_data().push_back(double(double(1)));
+
+      ((Foundation::Attribute<double, OT_>*)(attrs.at(0).get()))->get_data().push_back(double(double(1)));
+      ((Foundation::Attribute<double, OT_>*)(attrs.at(1).get()))->get_data().push_back(double(double(1)));
+
       /*  2    3
        *  *-1--*
        *  2    |
@@ -673,7 +698,10 @@ class MeshTestGeometryInterface:
        */
 
       //creating foundation mesh
-      Foundation::Mesh<Foundation::rnt_2D, Foundation::Topology<IndexType_, OT_, IT_> > m(0);
+      Foundation::Mesh<Foundation::rnt_2D, Foundation::Topology<IndexType_, OT_, IT_> > m(0, &attrs);
+      Foundation::MeshAttributeRegistration::execute(m, Foundation::pl_vertex);
+      Foundation::MeshAttributeRegistration::execute(m, Foundation::pl_vertex);
+
       m.add_polytope(Foundation::pl_vertex);
       m.add_polytope(Foundation::pl_vertex);
       m.add_polytope(Foundation::pl_vertex);
@@ -701,6 +729,7 @@ class MeshTestGeometryInterface:
       m.add_adjacency(Foundation::pl_vertex, Foundation::pl_edge, 3, 1);
       m.add_adjacency(Foundation::pl_vertex, Foundation::pl_edge, 3, 3);
       m.add_adjacency(Foundation::pl_vertex, Foundation::pl_face, 3, 0);
+
 
       //creating conformal geometry mesh
       IndexType_ index_set[] = {4, 4, 1};
@@ -746,9 +775,24 @@ class MeshTestGeometryInterface:
       TEST_CHECK_EQUAL(geo_vertex_at_face[0][2], 2);
       TEST_CHECK_EQUAL(geo_vertex_at_face[0][3], 3);
 
-      //typename confmeshtype_::VertexSetType& bla(geo_m.get_vertex_set());
-      //bla[0][0] = double(0.5);//xcoord of first node
+      typename confmeshtype_::VertexSetType& vertex_coord_tuples(geo_m.get_vertex_set());
+      vertex_coord_tuples[0][0] = ((Foundation::Attribute<double, OT_>*)(m.get_attributes()->at(0).get()))->get_data().at(0); //xcoord of first node
+      vertex_coord_tuples[0][1] = ((Foundation::Attribute<double, OT_>*)(m.get_attributes()->at(1).get()))->get_data().at(0); //ycoord of first node
+      vertex_coord_tuples[1][0] = ((Foundation::Attribute<double, OT_>*)(m.get_attributes()->at(0).get()))->get_data().at(1);
+      vertex_coord_tuples[1][1] = ((Foundation::Attribute<double, OT_>*)(m.get_attributes()->at(1).get()))->get_data().at(1);
+      vertex_coord_tuples[2][0] = ((Foundation::Attribute<double, OT_>*)(m.get_attributes()->at(0).get()))->get_data().at(2);
+      vertex_coord_tuples[2][1] = ((Foundation::Attribute<double, OT_>*)(m.get_attributes()->at(1).get()))->get_data().at(2);
+      vertex_coord_tuples[3][0] = ((Foundation::Attribute<double, OT_>*)(m.get_attributes()->at(0).get()))->get_data().at(3);
+      vertex_coord_tuples[3][1] = ((Foundation::Attribute<double, OT_>*)(m.get_attributes()->at(1).get()))->get_data().at(3);
 
+      TEST_CHECK_EQUAL(vertex_coord_tuples[0][0], 0);
+      TEST_CHECK_EQUAL(vertex_coord_tuples[0][1], 0);
+      TEST_CHECK_EQUAL(vertex_coord_tuples[1][0], 1);
+      TEST_CHECK_EQUAL(vertex_coord_tuples[1][1], 0);
+      TEST_CHECK_EQUAL(vertex_coord_tuples[2][0], 0);
+      TEST_CHECK_EQUAL(vertex_coord_tuples[2][1], 1);
+      TEST_CHECK_EQUAL(vertex_coord_tuples[3][0], 1);
+      TEST_CHECK_EQUAL(vertex_coord_tuples[3][1], 1);
     }
 };
 MeshTestGeometryInterface<Archs::None, unsigned long, std::vector, std::vector<unsigned long> > mesh_test_fginter_cpu_v_v("std::vector, std::vector");
