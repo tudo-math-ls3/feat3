320fef6f081e67af531d7c9fa5f3a4a9ba151243
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2012-10-05 12:22:04 +0200

    ENH: post comm only once for multi-threaded mode
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

--- kernel/foundation/communication.hpp
+++ kernel/foundation/communication.hpp
@@ -45,15 +45,18 @@ namespace FEAST
     {
       public:
         template<typename DataType_>
-        static inline void send_recv(DataType_ * sendbuf, Index /*dest_rank*/, Index num_elements, DataType_* recvbuf, Index /*source_rank*/)
+        static inline void send_recv(DataType_ * sendbuf, Index dest_rank, Index num_elements, DataType_* recvbuf, Index source_rank)
           {
-            DataType_ buf;
-            const Index i_end(num_elements);
-            for(Index i(0) ; i < i_end ; ++i)
+            if(source_rank <= dest_rank)
             {
-              buf = sendbuf[i];
-              sendbuf[i] = recvbuf[i];
-              recvbuf[i] = buf;
+              DataType_ buf;
+              const Index i_end(num_elements);
+              for(Index i(0) ; i < i_end ; ++i)
+              {
+                buf = sendbuf[i];
+                sendbuf[i] = recvbuf[i];
+                recvbuf[i] = buf;
+              }
             }
           }
 
@@ -80,49 +83,49 @@ namespace FEAST
      * \author Markus Geveler
      */
     template<unsigned i_ = 1, CommModes j_ = com_send_receive, typename AttributeType_ = double, typename Tag_ = Nil> //overlap is a compile-time decision now, if not feasible, move to inner function template
-    class Communication
-    {
-      public:
-        //example: Halo-based
-        template<typename HaloType_, typename MeshType_>
-          static void execute(
-              HaloType_ & halo,
-              unsigned attr_index,
-              MeshType_ & other_mesh,
-              Index other_rank) //TODO other_mesh resolved by process mesh list (mesh by id), needs to be a local thing
-          {
-#ifdef FOUNDATION_DEBUG
-            if(i_ != halo.get_overlap())
-              throw CommunicationHaloOverlapMismatch(i_, halo.get_overlap());
-#endif
-            //temp example
-            switch(j_)
+      class Communication
+      {
+        public:
+          //example: Halo-based
+          template<typename HaloType_, typename MeshType_>
+            static void execute(
+                HaloType_ & halo,
+                unsigned attr_index,
+                MeshType_ & other_mesh,
+                Index other_rank) //TODO other_mesh resolved by process mesh list (mesh by id), needs to be a local thing
             {
-              case com_send_receive:
-                {
-                  //temp until some sort of patch-internal buffer or master bufferpool available. Emulates Pack(). if rank=.. states here
-                  AttributeType_* sendbuf(new AttributeType_[halo.size()]);
-                  AttributeType_* recvbuf(new AttributeType_[halo.size()]);
-                  for(Index i(0) ; i < halo.size() ; ++i)
+#ifdef FOUNDATION_DEBUG
+              if(i_ != halo.get_overlap())
+                throw CommunicationHaloOverlapMismatch(i_, halo.get_overlap());
+#endif
+              //temp example
+              switch(j_)
+              {
+                case com_send_receive:
                   {
-                    sendbuf[i] = ((Attribute<AttributeType_>*)(halo.get_mesh().get_attributes()->at(attr_index).get()))->get_data().at(halo.get_element(i));
-                    recvbuf[i] = ((Attribute<AttributeType_>*)(other_mesh.get_attributes()->at(attr_index).get()))->get_data().at(halo.get_element_counterpart(i));
-                  }
-                  //'post'
-                  Foundation::Comm<Tag_>::send_recv(sendbuf, other_rank, halo.size(), recvbuf, halo.get_mesh().get_pp_rank());
-                  for(Index i(0) ; i < halo.size() ; ++i)
-                  {
-                    ((Attribute<AttributeType_>*)(halo.get_mesh().get_attributes()->at(attr_index).get()))->get_data().at(halo.get_element(i)) = sendbuf[i];
-                    ((Attribute<AttributeType_>*)(other_mesh.get_attributes()->at(attr_index).get()))->get_data().at(halo.get_element_counterpart(i)) = recvbuf[i];
-                  }
+                    //temp until some sort of patch-internal buffer or master bufferpool available. Emulates Pack(). if rank=.. states here
+                    AttributeType_* sendbuf(new AttributeType_[halo.size()]);
+                    AttributeType_* recvbuf(new AttributeType_[halo.size()]);
+                    for(Index i(0) ; i < halo.size() ; ++i)
+                    {
+                      sendbuf[i] = ((Attribute<AttributeType_>*)(halo.get_mesh().get_attributes()->at(attr_index).get()))->get_data().at(halo.get_element(i));
+                      recvbuf[i] = ((Attribute<AttributeType_>*)(other_mesh.get_attributes()->at(attr_index).get()))->get_data().at(halo.get_element_counterpart(i));
+                    }
+                    //'post'
+                    Foundation::Comm<Tag_>::send_recv(sendbuf, other_rank, halo.size(), recvbuf, halo.get_mesh().get_pp_rank());
+                    for(Index i(0) ; i < halo.size() ; ++i)
+                    {
+                      ((Attribute<AttributeType_>*)(halo.get_mesh().get_attributes()->at(attr_index).get()))->get_data().at(halo.get_element(i)) = sendbuf[i];
+                      ((Attribute<AttributeType_>*)(other_mesh.get_attributes()->at(attr_index).get()))->get_data().at(halo.get_element_counterpart(i)) = recvbuf[i];
+                    }
 
-                  delete[] sendbuf;
-                  delete[] recvbuf;
-                }
+                    delete[] sendbuf;
+                    delete[] recvbuf;
+                  }
+              }
             }
-          }
-        //TODO
-    };
+          //TODO
+      };
   }
 }
 #endif
