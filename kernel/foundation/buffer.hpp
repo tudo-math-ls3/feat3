1740e5bda8acf06586ca22d74fc2fd8bdc2a9ab9
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2012-10-10 14:40:03 +0200

    ENH: added buffer classes draft
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

---
+++ kernel/foundation/buffer.hpp
@@ -0,0 +1,76 @@
+#pragma once
+#ifndef KERNEL_FOUNDATION_BUFFER_HH
+#define KERNEL_FOUNDATION_BUFFER_HH 1
+
+#include<kernel/base_header.hpp>
+#include<kernel/util/cpp11_smart_pointer.hpp>
+#include<vector>
+
+namespace FEAST
+{
+  namespace Foundation
+  {
+    class SharedArrayBase
+    {
+      public:
+        virtual Index size() const = 0;
+        virtual ~SharedArrayBase()
+        {
+        }
+    };
+
+    ///does not need to be copy-assignable, since only shared_ptrs are going to be copied externally
+    template<typename T_>
+    class BufferedSharedArray : public SharedArrayBase
+    {
+      public:
+        static std::shared_ptr<SharedArrayBase> create(Index i)
+        {
+          return std::shared_ptr<SharedArrayBase>(new BufferedSharedArray<T_>(i));
+        }
+
+        T_& operator[](Index i) const
+        {
+          return _data[i];
+        }
+
+        T_& operator[](Index i)
+        {
+          return _data[i];
+        }
+
+        Index size() const
+        {
+          return _size;
+        }
+
+        ~BufferedSharedArray()
+        {
+          delete[] _data;
+        }
+
+      private:
+        BufferedSharedArray(Index i) :
+          _data(new T_[i]),
+          _size(i)
+        {
+        }
+
+        T_* _data;
+        Index _size;
+    };
+
+    template<typename T_>
+    struct Bufferable
+    {
+      virtual T_ buffer() = 0;
+      virtual void to_buffer(T_& buffer) = 0;
+      virtual void from_buffer(const T_& buffer) = 0;
+    };
+
+    class Buffer
+    {
+    };
+  }
+}
+#endif
