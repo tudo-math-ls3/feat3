6cf76f432380547e12dbe5500a031804d2bab798
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2012-10-15 10:40:44 +0200

    ENH: added serial attribute commtest
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

--- kernel/foundation/attribute-test.cpp
+++ kernel/foundation/attribute-test.cpp
@@ -1,3 +1,5 @@
+#define FEAST_SERIAL_MODE
+
 #include <kernel/base_header.hpp>
 #include <test_system/test_system.hpp>
 
@@ -45,3 +47,43 @@ AttributeTest<Archs::None, double, int, std::vector> attribute_test_cpu_v_double
 AttributeTest<Archs::None, unsigned long, float, std::deque> attribute_test_cpu_d_ulong_float("StorageType: std::deque, DataTypes: ulong, float");
 AttributeTest<Archs::None, double, float, std::deque> attribute_test_cpu_d_double_float("StorageType: std::deque, DataTypes: double, float");
 AttributeTest<Archs::None, double, int, std::deque> attribute_test_cpu_d_double_int("StorageType: std::deque, DataTypes: double, int");
+
+template<typename Tag_, typename DataType1_, typename DataType2_, template<typename, typename> class ST_>
+class AttributeCommTest:
+  public TaggedTest<Tag_, DataType1_>
+{
+  public:
+    AttributeCommTest(const std::string & tag) :
+      TaggedTest<Tag_, DataType1_>("AttributeCommTest<" + tag + ">")
+    {
+    }
+
+    virtual void run() const
+    {
+      Foundation::Attribute<DataType1_> attr;
+      attr.push_back(DataType1_(0));
+      attr.push_back(DataType1_(42));
+
+      typename Foundation::Attribute<DataType1_>::buffer_type_ sendbuf(attr.buffer());
+      typename Foundation::Attribute<DataType1_>::buffer_type_ recvbuf(attr.buffer());
+
+      attr.to_buffer(sendbuf);
+
+      attr.send_recv(
+          sendbuf,
+          0,
+          recvbuf,
+          0);
+
+      attr.from_buffer(recvbuf);
+
+      TEST_CHECK_EQUAL(attr.at(0), DataType1_(0));
+      TEST_CHECK_EQUAL(attr.at(1), DataType1_(42));
+    }
+};
+AttributeCommTest<Archs::None, unsigned long, float, std::vector> attribute_commtest_cpu_v_ulong_float("StorageType: std::vector, DataTypes: ulong, float");
+AttributeCommTest<Archs::None, double, float, std::vector> attribute_commtest_cpu_v_double_float("StorageType: std::vector, DataTypes: double, float");
+AttributeCommTest<Archs::None, double, int, std::vector> attribute_commtest_cpu_v_double_int("StorageType: std::vector, DataTypes: double, int");
+AttributeCommTest<Archs::None, unsigned long, float, std::deque> attribute_commtest_cpu_d_ulong_float("StorageType: std::deque, DataTypes: ulong, float");
+AttributeCommTest<Archs::None, double, float, std::deque> attribute_commtest_cpu_d_double_float("StorageType: std::deque, DataTypes: double, float");
+AttributeCommTest<Archs::None, double, int, std::deque> attribute_commtest_cpu_d_double_int("StorageType: std::deque, DataTypes: double, int");
