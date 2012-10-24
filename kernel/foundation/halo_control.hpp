e09e8a7d5beb32aae8439cafc641440c4fc69791
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2012-10-24 15:20:03 +0200

    FIX: halo control: fixed multiple occurances of indices
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

--- kernel/foundation/halo_control.hpp
+++ kernel/foundation/halo_control.hpp
@@ -53,6 +53,8 @@ namespace FEAST
           class HaloType_>
         static void fill_sizes(const HaloType_<a_, pl_edge, b_, c_, d_>& halo, typename HaloType_<a_, pl_edge, b_, c_, d_>::index_type_* target)
         {
+          ASSERT(a_ != 0, "Error: Halos with 0-overlap may not contain edges in 1D!");
+
           typedef typename HaloType_<a_, pl_edge, b_, c_, d_>::index_type_ IndexType;
           target[0] = IndexType(halo.size() + 1);
           target[1] = IndexType(halo.size());
@@ -124,11 +126,52 @@ namespace FEAST
 
           IndexType num_edges(0);
           IndexType num_vertices(0);
+          typename HaloType_<a_, pl_face, b_, c_, d_>::mesh_type_::topology_type_::storage_type_ all_edges;
+          typename HaloType_<a_, pl_face, b_, c_, d_>::mesh_type_::topology_type_::storage_type_ all_vertices;
           for(IndexType i(0) ; i < halo.size() ; ++i)
           {
             ///for any face count edges
-            num_edges += halo.get_mesh().get_adjacent_polytopes(pl_face, pl_edge, halo.get_element(i)).size();
-            num_vertices += halo.get_mesh().get_adjacent_polytopes(pl_face, pl_vertex, halo.get_element(i)).size();
+            typename HaloType_<a_, pl_face, b_, c_, d_>::mesh_type_::topology_type_::storage_type_ edges(halo.get_mesh().get_adjacent_polytopes(pl_face, pl_edge, halo.get_element(i)));
+            IndexType count(0);
+            for(IndexType j(0) ; j < edges.size() ; ++j)
+            {
+              bool already_in(false);
+              for(IndexType k(0) ; k < all_edges.size() ; ++k)
+              {
+                if(all_edges.at(k) == edges.at(j))
+                  already_in = true;
+              }
+              if(!already_in)
+              {
+                all_edges.push_back(edges.at(j));
+                ++count;
+              }
+            }
+            num_edges += count;
+
+          }
+
+          ///for any edge computed before, check vertex and add if not yet added
+          for(IndexType i(0) ; i < all_edges.size() ; ++i)
+          {
+            ///for any face count edges
+            typename HaloType_<a_, pl_face, b_, c_, d_>::mesh_type_::topology_type_::storage_type_ vertices(halo.get_mesh().get_adjacent_polytopes(pl_edge, pl_vertex, all_edges.at(i)));
+            IndexType count(0);
+            for(IndexType j(0) ; j < vertices.size() ; ++j)
+            {
+              bool already_in(false);
+              for(IndexType k(0) ; k < all_vertices.size() ; ++k)
+              {
+                if(all_vertices.at(k) == vertices.at(j))
+                  already_in = true;
+              }
+              if(!already_in)
+              {
+                all_vertices.push_back(vertices.at(j));
+                ++count;
+              }
+            }
+            num_vertices += count;
           }
 
           target[0] = IndexType(num_vertices);
@@ -140,7 +183,27 @@ namespace FEAST
     template<>
     struct HaloControl<dim_3D>
     {
-      ///TODO
+        ///delta = 0 case: in 3D, zero-overlap halos can only be given disambigously by faces
+        ///0, pl_vertex case
+        /*template<
+          typename b_,
+          template<typename, typename> class c_,
+          typename d_,
+          template<unsigned,
+            PolytopeLevels,
+            typename,
+            template<typename, typename> class,
+            typename>
+          class HaloType_>
+        static void fill_sizes(const HaloType_<0, pl_vertex, b_, c_, d_>& halo, typename HaloType_<0, pl_vertex, b_, c_, d_>::index_type_* target)
+        {
+          typedef typename HaloType_<0, pl_vertex, b_, c_, d_>::index_type_ IndexType;
+
+          target[0] = IndexType();
+          target[1] = IndexType();
+          target[2] = IndexType(halo.size());
+          target[3] = IndexType();
+        }*/
     };
 
   }
