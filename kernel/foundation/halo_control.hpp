b95fbe1f304c2a5780353d75266ee2269ded06f1
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2012-10-24 13:10:04 +0200

    ENH: halo control: added 1D sizes case
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

--- kernel/foundation/halo_control.hpp
+++ kernel/foundation/halo_control.hpp
@@ -19,6 +19,44 @@ namespace FEAST
     template<>
     struct HaloControl<dim_1D>
     {
+        ///delta = 0 case: in 1D, zero-overlap halos can only be given in terms of vertices
+        ///0, pl_vertex case
+        template<
+          typename b_,
+          template<typename, typename> class c_,
+          typename d_,
+          template<unsigned,
+            PolytopeLevels,
+            typename,
+            template<typename, typename> class,
+            typename>
+          class HaloType_>
+        static void fill_sizes(const HaloType_<0, pl_vertex, b_, c_, d_>& halo, typename HaloType_<0, pl_vertex, b_, c_, d_>::index_type_* target)
+        {
+          typedef typename HaloType_<0, pl_vertex, b_, c_, d_>::index_type_ IndexType;
+          target[0] = IndexType(halo.size());
+          target[1] = IndexType(halo.size() - 1);
+        }
+
+        ///delta = i case: in 1D, overlapping meshes have halos given in terms of edges
+        ///i, pl_edge case
+        template<
+          unsigned a_,
+          typename b_,
+          template<typename, typename> class c_,
+          typename d_,
+          template<unsigned,
+            PolytopeLevels,
+            typename,
+            template<typename, typename> class,
+            typename>
+          class HaloType_>
+        static void fill_sizes(const HaloType_<a_, pl_edge, b_, c_, d_>& halo, typename HaloType_<a_, pl_edge, b_, c_, d_>::index_type_* target)
+        {
+          typedef typename HaloType_<a_, pl_edge, b_, c_, d_>::index_type_ IndexType;
+          target[0] = IndexType(halo.size() + 1);
+          target[1] = IndexType(halo.size());
+        }
     };
 
     template<>
@@ -80,7 +118,7 @@ namespace FEAST
           class HaloType_>
         static void fill_sizes(const HaloType_<a_, pl_face, b_, c_, d_>& halo, typename HaloType_<a_, pl_face, b_, c_, d_>::index_type_* target)
         {
-          ASSERT(a_ != 0, "Error: Faces can not be 0-overlapped in 2D!");
+          ASSERT(a_ != 0, "Error: Halos with 0-overlap may not contain faces in 2D!");
 
           typedef typename HaloType_<a_, pl_face, b_, c_, d_>::index_type_ IndexType;
 
@@ -102,6 +140,7 @@ namespace FEAST
     template<>
     struct HaloControl<dim_3D>
     {
+      ///TODO
     };
 
   }
