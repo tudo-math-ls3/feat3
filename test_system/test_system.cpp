2c2def445c58750410526eca86d7dee419573286
Peter Zajac <pzajac@math.tu-dortmund.de>
2012-11-19 15:03:36 +0100

    ENH: added more output to test system driver
    Signed-off-by: Peter Zajac <pzajac@math.tu-dortmund.de>

--- test_system/test_system.cpp
+++ test_system/test_system.cpp
@@ -28,6 +28,8 @@ int main(int argc, char** argv)
   }
 
   size_t list_size(TestList::instance()->size());
+  size_t tests_passed(0u);
+  size_t tests_failed(0u);
   unsigned long iterator_index(1);
   for(TestList::Iterator i(TestList::instance()->begin_tests()), i_end(TestList::instance()->end_tests()) ;
       i != i_end ; )
@@ -39,23 +41,27 @@ int main(int argc, char** argv)
         << (*i)->get_tag_name() << "]" << " [Precision: "<< (*i)->get_prec_name() << "]" << std::endl;
       (*i)->run();
       std::cout << "PASSED" << std::endl;
+      ++tests_passed;
     }
     catch (TestFailedException & e)
     {
       std::cout << "FAILED: " << (*i)->id() << std::endl << stringify(e.what()) << std::endl;
       result = EXIT_FAILURE;
+      ++tests_failed;
     }
     catch (InternalError & e)
     {
       std::cout << "FAILED with InternalError: " << (*i)->id() << std::endl << stringify(e.what()) << std::endl
         << stringify(e.message()) << std::endl;
       result = EXIT_FAILURE;
+      ++tests_failed;
     }
     catch (std::exception & e)
     {
       std::cout << "FAILED with unknown Exception: " << (*i)->id() << std::endl << stringify(e.what()) << std::endl
         << std::endl;
       result = EXIT_FAILURE;
+      ++tests_failed;
     }
     i = TestList::instance()->erase(i);
     iterator_index++;
@@ -67,7 +73,15 @@ int main(int argc, char** argv)
     i = TestList::instance()->erase(i);
   }
 
-  if (result == EXIT_SUCCESS)
-    std::cout<<"All " << list_size << " tests PASSED!" << std::endl;
+  if(result == EXIT_SUCCESS)
+  {
+    std::cout << "All " << list_size << " tests PASSED!" << std::endl;
+  }
+  else
+  {
+    std::cout << tests_passed << " of " << list_size << " tests PASSED, "
+      << tests_failed << " tests FAILED!" << std::endl;
+  }
+
   return result;
 }
