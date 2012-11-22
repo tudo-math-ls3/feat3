022a34ecf751464a3a74d0b691a000c6b73af3c0
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2012-11-22 12:38:59 +0100

    ENH: added substitution mechanism, product functor, precon proxy
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

--- kernel/scarc/solver_functor-test.cpp
+++ kernel/scarc/solver_functor-test.cpp
@@ -38,11 +38,14 @@ class SolverFunctorTest:
       DenseVector<Tag_, DataType_> c(1000);
       Defect<Algo_>::value(c, b, A, x);
       Sum<Algo_>::value(c, c, c);
+      Product<Algo_>::value(c, A, c);
 
       DefectFunctor<Algo_, DenseVector<Tag_, DataType_>, SparseMatrixCSR<Tag_, DataType_> > f(d, b, A, x);
       SumFunctor<Algo_, DenseVector<Tag_, DataType_> > f1(d, d, d);
+      ProductFunctor<Algo_, DenseVector<Tag_, DataType_>, SparseMatrixCSR<Tag_, DataType_> > f2(d, A, d);
       f.execute();
       f1.execute();
+      f2.execute();
 
       TEST_CHECK_EQUAL(d, c);
 
@@ -55,15 +58,19 @@ class SolverFunctorTest:
       cf.add_functor(new DefectFunctor<Algo_, DenseVector<Tag_, DataType_>, SparseMatrixCSR<Tag_, DataType_> >(dn, b, A, x));
       cf.add_functor(new SumFunctor<Algo_, DenseVector<Tag_, DataType_> >(dn, dn, dn));
       cf.execute();
-      TEST_CHECK_EQUAL(d, c);
 
       cf.add_functor(new ProxyPreconApplyFunctor<DenseVector<Tag_, DataType_> >(dn));
 
-      ///execution may not work before subsitution
+      ///execution may not work before substitution
       TEST_CHECK_THROWS(cf.execute(), ScaRCError);
 
-      ///bring up a 'preconditioner'
-
+      ///bring up precon
+      DenseVector<Tag_, DataType_> dummy;
+      ProductFunctor<Algo_, DenseVector<Tag_, DataType_>, SparseMatrixCSR<Tag_, DataType_> > precon(dn, A, dummy);
+      ///substitute
+      cf.substitute_precon(precon);
+      cf.execute();
+      TEST_CHECK_EQUAL(dn, c);
     }
 };
 SolverFunctorTest<Mem::Main, Algo::Generic, double> sf_cpu_double("StorageType: std::vector, DataType: double");
--- kernel/scarc/solver_functor.hpp
+++ kernel/scarc/solver_functor.hpp
@@ -7,6 +7,7 @@
 #include<kernel/foundation/functor.hpp>
 #include<kernel/lafem/defect.hpp>
 #include<kernel/lafem/sum.hpp>
+#include<kernel/lafem/product.hpp>
 
 using namespace FEAST::Foundation;
 using namespace FEAST;
@@ -15,38 +16,6 @@ namespace FEAST
 {
   namespace ScaRC
   {
-
-    template<template<typename, typename> class StorageType_ = std::vector>
-    class CompoundSolverFunctor : public CompoundFunctor<StorageType_>
-    {
-      public:
-        ///overwrite execute and undo functions
-        virtual void execute()
-        {
-          for(Index i(0) ; i < (this->_functors).size() ; ++i)
-          {
-            (this->_functors).at(i)->execute();
-          }
-        }
-
-        virtual void undo()
-        {
-          throw ScaRCError("Error: Numerical functor lists can not be undone!");
-        }
-
-        ///substitute first ProxyPreconApplyFunctor by new_precon
-        template<template<typename, typename> class StorageTypeOther_>
-        void substitute_precon(std::shared_ptr<CompoundSolverFunctor<StorageTypeOther_> >& new_precon)
-        {
-          for(Index i(0) ; i < (this->_functors).size() ; ++i)
-          {
-            if( (this->_functors).at(i)->type_name() == "ProxyPreconApplyFunctor")
-              (this->_functors).at(i) = new_precon;
-          }
-
-          throw ScaRCError("Error: No ProxyPreconApplyFunctor in functor list!");
-        }
-    };
 
     template<typename VT_>
     class ProxyPreconApplyFunctor : public FunctorBase
@@ -86,9 +55,48 @@ namespace FEAST
         {
         }
 
+        VT_& get_argument()
+        {
+          return _x;
+        }
+
       private:
         ///apply preconditioner to what?
         VT_& _x;
+    };
+
+    template<template<typename, typename> class StorageType_ = std::vector>
+    class CompoundSolverFunctor : public CompoundFunctor<StorageType_>
+    {
+      public:
+        ///overwrite execute and undo functions
+        virtual void execute()
+        {
+          for(Index i(0) ; i < (this->_functors).size() ; ++i)
+          {
+            (this->_functors).at(i)->execute();
+          }
+        }
+
+        virtual void undo()
+        {
+          throw ScaRCError("Error: Numerical functor lists can not be undone!");
+        }
+
+        ///substitute first ProxyPreconApplyFunctor by new_precon
+        template<typename T_>
+        void substitute_precon(T_& new_precon)
+        {
+          for(Index i(0) ; i < (this->_functors).size() ; ++i)
+          {
+            if( (this->_functors).at(i)->type_name() == "ProxyPreconApplyFunctor")
+            {
+              (this->_functors).at(i) = std::shared_ptr<FunctorBase>(new T_(new_precon.substitute(((ProxyPreconApplyFunctor<typename T_::vector_type_>*)(this->_functors.at(i).get()))->get_argument())));
+              return;
+            }
+          }
+          throw ScaRCError("Error: No ProxyPreconApplyFunctor in functor list!");
+        }
     };
 
     template<typename Algo_, typename VT_, typename MT_>
@@ -194,6 +202,74 @@ namespace FEAST
         VT_& _y;
         const VT_& _r;
         const VT_& _l;
+    };
+
+    ///PreconApplicable interface
+    template<typename MyFunctorType_, typename VT_>
+    class PreconApplicable
+    {
+      public:
+        typedef VT_ vector_type_;
+
+        ///named copy-CTOR
+        virtual MyFunctorType_ substitute(VT_& arg) = 0;
+    };
+
+
+    template<typename Algo_, typename VT_, typename MT_>
+    class ProductFunctor : public FunctorBase, public PreconApplicable<ProductFunctor<Algo_, VT_, MT_>, VT_>
+    {
+      public:
+        ProductFunctor(VT_& y, const MT_& A, const VT_& x) :
+          _y(y),
+          _A(A),
+          _x(x)
+        {
+        }
+
+        virtual const std::string type_name()
+        {
+          return "ProductFunctor";
+        }
+
+        virtual void execute()
+        {
+          LAFEM::Product<Algo_>::value(_y, _A, _x);
+        }
+
+        virtual void undo()
+        {
+          throw ScaRCError("Error: Numerical functors can not be undone!");
+        }
+
+        ProductFunctor& operator=(const ProductFunctor& rhs)
+        {
+          if(this == &rhs)
+            return *this;
+
+          this->_y = rhs._y;
+          this->_A = rhs._A;
+          this->_x = rhs._x;
+          return *this;
+        }
+
+        ProductFunctor(const ProductFunctor& other) :
+          _y(other._y),
+          _A(other._A),
+          _x(other._x)
+        {
+        }
+
+        ///implementation of PreconApplicable interface
+        virtual ProductFunctor<Algo_, VT_, MT_> substitute(VT_& arg)
+        {
+          return ProductFunctor<Algo_, VT_, MT_>(_y, _A, arg);
+        }
+
+      private:
+        VT_& _y;
+        const MT_& _A;
+        const VT_& _x;
     };
   }
 }
