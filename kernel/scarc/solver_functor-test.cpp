20263f067239175a76fc4b9c4ba82ab1fd163468
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2012-11-26 14:54:52 +0100

    ENH: restructured solver functors for inline substitution
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

--- kernel/scarc/solver_functor-test.cpp
+++ kernel/scarc/solver_functor-test.cpp
@@ -24,7 +24,90 @@ class SolverFunctorTest:
 
     virtual void run() const
     {
+      DenseVector<Tag_, DataType_> y(1000, DataType_(0)), r(1000, DataType_(1));
+      DenseVector<Tag_, DataType_> dummy;
+      DenseVector<Tag_, DataType_> l(1000, DataType_(1));
+
+      DenseVector<Tag_, DataType_> y_ref(1000, DataType_(0));
+
+      SumFunctorProxyLeft<Algo_, DenseVector<Tag_, DataType_> > sf(y, dummy, r);
+      TEST_CHECK_THROWS(sf.execute(), ScaRCError);
+
+      sf.substitute(l);
+      sf.execute();
+
+      Sum<Algo_>::value(y_ref, l, r);
+      TEST_CHECK_EQUAL(y, y_ref);
+
+      //---------------------------------------------------------------------------------------------
+
+      SumFunctorProxyResultLeft<Algo_, DenseVector<Tag_, DataType_> > sf1(dummy, dummy, r);
+      sf1.substitute(l);
+      sf1.execute();
+
+      TEST_CHECK_EQUAL(l, y_ref);
+
+      //---------------------------------------------------------------------------------------------
+
+      SumFunctor<Algo_, DenseVector<Tag_, DataType_> > sf2(y, r, r);
+      sf2.execute();
+
+      TEST_CHECK_EQUAL(y, y_ref);
+
+      //---------------------------------------------------------------------------------------------
+
+      //testing substitution in CompoundSolverFunctor
+      //a <- a + A*a := [[b <- A*a , a <- a + b]]
+      DenseVector<Tag_, DataType_> a(1000, DataType_(1)), b(1000, DataType_(1));
       SparseMatrixCOO<Mem::Main, DataType_> T(1000, 1000);
+      for(Index i(0) ; i < 1000 ; ++i)
+        T(i, i, DataType_(1));
+      SparseMatrixCSR<Tag_, DataType_> A(T);
+
+      CompoundSolverFunctor<Algo_, DenseVector<Tag_, DataType_> > cf;
+      cf.add_functor(new ProductFunctorProxyRight<Algo_, DenseVector<Tag_, DataType_>, SparseMatrixCSR<Tag_, DataType_> >(b, A, dummy));
+      cf.add_functor(new SumFunctorProxyResultLeft<Algo_, DenseVector<Tag_, DataType_> >(dummy, dummy, b));
+      cf.substitute(a);
+      cf.execute();
+
+      //reference
+      DenseVector<Tag_, DataType_> a_ref(1000, DataType_(1));
+      DenseVector<Tag_, DataType_> b_ref(1000, DataType_(1));
+      Product<Algo_>::value(b_ref, A, a_ref);
+      Sum<Algo_>::value(a_ref, b_ref, a_ref);
+
+      TEST_CHECK_EQUAL(a, a_ref);
+
+
+      //---------------------------------------------------------------------------------------------
+
+      //testing preconditioner functor
+      // u <- u + precon(u), precon(.) := A*(.) := [[v <- u, precon(u), u <- u + v]]
+      DenseVector<Tag_, DataType_> u(1000, DataType_(47.11));
+      DenseVector<Tag_, DataType_> v(1000, DataType_(0));
+      CompoundSolverFunctor<Algo_, DenseVector<Tag_, DataType_> > cf1;
+
+      cf1.add_functor(new CopyFunctor<Algo_, DenseVector<Tag_, DataType_> >(v, u));
+      cf1.add_functor(new PreconFunctor<Algo_, DenseVector<Tag_, DataType_> >(u));
+      cf1.add_functor(new SumFunctor<Algo_, DenseVector<Tag_, DataType_> >(u, u, v));
+
+      std::shared_ptr<SolverFunctorBase<DenseVector<Tag_, DataType_> > > p(new CompoundSolverFunctor<Algo_, DenseVector<Tag_, DataType_> >() );
+      ((CompoundSolverFunctor<Algo_, DenseVector<Tag_, DataType_> >* )(p.get()))->add_functor(new ProductFunctorProxyResultRight<Algo_, DenseVector<Tag_, DataType_> , SparseMatrixCSR<Tag_, DataType_> >(dummy, A, dummy) );
+
+      cf1.set_preconditioner(p);
+      cf1.execute();
+
+      DenseVector<Tag_, DataType_> u_ref(1000, DataType_(47.11));
+      DenseVector<Tag_, DataType_> v_ref(1000, DataType_(0));
+
+      copy(v_ref, u_ref);
+      Product<Algo_>::value(u_ref, A, u_ref);
+      Sum<Algo_>::value(u_ref, v_ref, u_ref);
+
+      TEST_CHECK_EQUAL(u, u_ref);
+
+      //---------------------------------------------------------------------------------------------
+      /*SparseMatrixCOO<Mem::Main, DataType_> T(1000, 1000);
 
       for(Index i(0) ; i < 1000 ; ++i)
         T(i, i, DataType_(1));
@@ -70,7 +153,7 @@ class SolverFunctorTest:
       ///substitute
       cf.substitute_first(precon);
       cf.execute();
-      TEST_CHECK_EQUAL(dn, c);
+      TEST_CHECK_EQUAL(dn, c);*/
     }
 };
 SolverFunctorTest<Mem::Main, Algo::Generic, double> sf_cpu_double("StorageType: std::vector, DataType: double");
--- kernel/scarc/solver_functor.hpp
+++ kernel/scarc/solver_functor.hpp
@@ -16,6 +16,622 @@ namespace FEAST
 {
   namespace ScaRC
   {
+    template<typename VT_>
+    class SolverFunctorBase
+    {
+      public:
+        ///needed in substitution of CSF
+        typedef VT_ vector_type_;
+
+        virtual void substitute(VT_& arg) = 0;
+
+        virtual void execute() = 0;
+        virtual const std::string type_name() = 0;
+
+        virtual ~SolverFunctorBase()
+        {
+        }
+
+        virtual bool is_complete()
+        {
+          return _complete;
+        }
+
+      protected:
+        bool _complete;
+    };
+
+
+    ///always substitute left argument
+    template<typename Algo_, typename VT_>
+    class SumFunctorProxyLeft : public SolverFunctorBase<VT_>
+    {
+      public:
+        SumFunctorProxyLeft(VT_& y, VT_& l, const VT_& r) :
+          _y(y),
+          _l(l),
+          _r(r)
+        {
+          this->_complete = false;
+        }
+
+        virtual const std::string type_name()
+        {
+          return "SumFunctor";
+        }
+
+        virtual void execute()
+        {
+          if(!this->_complete)
+            throw ScaRCError("Error: Incomplete SumFunctor can not be executed!");
+
+          LAFEM::Sum<Algo_>::value(_y, _l, _r);
+        }
+
+        SumFunctorProxyLeft& operator=(const SumFunctorProxyLeft& rhs)
+        {
+          if(this == &rhs)
+            return *this;
+
+          this->_y = rhs._y;
+          this->_l = rhs._l;
+          this->_r = rhs._r;
+          return *this;
+        }
+
+        SumFunctorProxyLeft(const SumFunctorProxyLeft& other) :
+          _y(other._y),
+          _l(other._l),
+          _r(other._r)
+        {
+        }
+
+        virtual void substitute(VT_& arg)
+        {
+          _l = arg;
+          this->_complete = true;
+        }
+
+      private:
+        VT_& _y;
+        VT_& _l;
+        const VT_& _r;
+    };
+
+
+    ///always substitute result and left arguments
+    template<typename Algo_, typename VT_>
+    class SumFunctorProxyResultLeft : public SolverFunctorBase<VT_>
+    {
+      public:
+        SumFunctorProxyResultLeft(VT_& y, VT_& l, const VT_& r) :
+          _y(y),
+          _l(l),
+          _r(r)
+        {
+          this->_complete = false;
+        }
+
+        virtual const std::string type_name()
+        {
+          return "SumFunctor";
+        }
+
+        virtual void execute()
+        {
+          if(!this->_complete)
+            throw ScaRCError("Error: Incomplete SumFunctor can not be executed!");
+
+          LAFEM::Sum<Algo_>::value(_y, _l, _r);
+        }
+
+        SumFunctorProxyResultLeft& operator=(const SumFunctorProxyResultLeft& rhs)
+        {
+          if(this == &rhs)
+            return *this;
+
+          this->_y = rhs._y;
+          this->_l = rhs._l;
+          this->_r = rhs._r;
+          return *this;
+        }
+
+        SumFunctorProxyResultLeft(const SumFunctorProxyResultLeft& other) :
+          _y(other._y),
+          _l(other._l),
+          _r(other._r)
+        {
+        }
+
+        virtual void substitute(VT_& arg)
+        {
+          _y = arg;
+          _l = arg;
+          this->_complete = true;
+        }
+
+      private:
+        VT_& _y;
+        VT_& _l;
+        const VT_& _r;
+    };
+
+    ///always substitute all arguments
+    template<typename Algo_, typename VT_>
+    class SumFunctorProxyAll : public SolverFunctorBase<VT_>
+    {
+      public:
+        SumFunctorProxyAll(VT_& y, VT_& l, VT_& r) :
+          _y(y),
+          _l(l),
+          _r(r)
+        {
+          this->_complete = false;
+        }
+
+        virtual const std::string type_name()
+        {
+          return "SumFunctor";
+        }
+
+        virtual void execute()
+        {
+          if(!this->_complete)
+            throw ScaRCError("Error: Incomplete SumFunctor can not be executed!");
+
+          LAFEM::Sum<Algo_>::value(_y, _l, _r);
+        }
+
+        SumFunctorProxyAll& operator=(const SumFunctorProxyAll& rhs)
+        {
+          if(this == &rhs)
+            return *this;
+
+          this->_y = rhs._y;
+          this->_l = rhs._l;
+          this->_r = rhs._r;
+          return *this;
+        }
+
+        SumFunctorProxyAll(const SumFunctorProxyAll& other) :
+          _y(other._y),
+          _l(other._l),
+          _r(other._r)
+        {
+        }
+
+        virtual void substitute(VT_& arg)
+        {
+          _y = arg;
+          _l = arg;
+          _r = arg;
+          this->_complete = true;
+        }
+
+      private:
+        VT_& _y;
+        VT_& _l;
+        VT_& _r;
+    };
+
+
+    template<typename Algo_, typename VT_>
+    class SumFunctor : public SolverFunctorBase<VT_>
+    {
+      public:
+        SumFunctor(VT_& y, const VT_& l, const VT_& r) :
+          _y(y),
+          _l(l),
+          _r(r)
+        {
+          this->_complete = true;
+        }
+
+        virtual const std::string type_name()
+        {
+          return "SumFunctor";
+        }
+
+        virtual void execute()
+        {
+          LAFEM::Sum<Algo_>::value(_y, _l, _r);
+        }
+
+        SumFunctor& operator=(const SumFunctor& rhs)
+        {
+          if(this == &rhs)
+            return *this;
+
+          this->_y = rhs._y;
+          this->_l = rhs._l;
+          this->_r = rhs._r;
+          return *this;
+        }
+
+        SumFunctor(const SumFunctor& other) :
+          _y(other._y),
+          _l(other._l),
+          _r(other._r)
+        {
+        }
+
+        virtual void substitute(VT_& arg)
+        {
+        }
+
+      private:
+        VT_& _y;
+        const VT_& _l;
+        const VT_& _r;
+    };
+
+    ///always substitute right argument
+    template<typename Algo_, typename VT_, typename MT_>
+    class ProductFunctorProxyRight : public SolverFunctorBase<VT_>
+    {
+      public:
+        ProductFunctorProxyRight(VT_& y, const MT_& l, VT_& r) :
+          _y(y),
+          _l(l),
+          _r(r)
+        {
+          this->_complete = false;
+        }
+
+        virtual const std::string type_name()
+        {
+          return "ProductFunctor";
+        }
+
+        virtual void execute()
+        {
+          if(!this->_complete)
+            throw ScaRCError("Error: Incomplete ProductFunctor can not be executed!");
+
+          LAFEM::Product<Algo_>::value(_y, _l, _r);
+        }
+
+        ProductFunctorProxyRight& operator=(const ProductFunctorProxyRight& rhs)
+        {
+          if(this == &rhs)
+            return *this;
+
+          this->_y = rhs._y;
+          this->_l = rhs._l;
+          this->_r = rhs._r;
+          return *this;
+        }
+
+        ProductFunctorProxyRight(const ProductFunctorProxyRight& other) :
+          _y(other._y),
+          _l(other._l),
+          _r(other._r)
+        {
+        }
+
+        virtual void substitute(VT_& arg)
+        {
+          _r = arg;
+          this->_complete = true;
+        }
+
+      private:
+        VT_& _y;
+        const MT_& _l;
+        VT_& _r;
+    };
+
+    ///always substitute result and right arguments
+    template<typename Algo_, typename VT_, typename MT_>
+    class ProductFunctorProxyResultRight : public SolverFunctorBase<VT_>
+    {
+      public:
+        ProductFunctorProxyResultRight(VT_& y, const MT_& l, VT_& r) :
+          _y(y),
+          _l(l),
+          _r(r)
+        {
+          this->_complete = false;
+        }
+
+        virtual const std::string type_name()
+        {
+          return "ProductFunctor";
+        }
+
+        virtual void execute()
+        {
+          if(!this->_complete)
+            throw ScaRCError("Error: Incomplete ProductFunctor can not be executed!");
+
+          LAFEM::Product<Algo_>::value(_y, _l, _r);
+        }
+
+        ProductFunctorProxyResultRight& operator=(const ProductFunctorProxyResultRight& rhs)
+        {
+          if(this == &rhs)
+            return *this;
+
+          this->_y = rhs._y;
+          this->_l = rhs._l;
+          this->_r = rhs._r;
+          return *this;
+        }
+
+        ProductFunctorProxyResultRight(const ProductFunctorProxyResultRight& other) :
+          _y(other._y),
+          _l(other._l),
+          _r(other._r)
+        {
+        }
+
+        virtual void substitute(VT_& arg)
+        {
+          _y = arg;
+          _r = arg;
+          this->_complete = true;
+        }
+
+      private:
+        VT_& _y;
+        const MT_& _l;
+        VT_& _r;
+    };
+
+    template<typename Algo_, typename VT_, typename MT_>
+    class ProductFunctor : public SolverFunctorBase<VT_>
+    {
+      public:
+        ProductFunctor(VT_& y, const MT_& l, const VT_& r) :
+          _y(y),
+          _l(l),
+          _r(r)
+        {
+          this->_complete = true;
+        }
+
+        virtual const std::string type_name()
+        {
+          return "ProductFunctor";
+        }
+
+        virtual void execute()
+        {
+          LAFEM::Product<Algo_>::value(_y, _l, _r);
+        }
+
+        ProductFunctor& operator=(const ProductFunctor& rhs)
+        {
+          if(this == &rhs)
+            return *this;
+
+          this->_y = rhs._y;
+          this->_l = rhs._l;
+          this->_r = rhs._r;
+          return *this;
+        }
+
+        ProductFunctor(const ProductFunctor& other) :
+          _y(other._y),
+          _l(other._l),
+          _r(other._r)
+        {
+        }
+
+        virtual void substitute(VT_& arg)
+        {
+        }
+
+      private:
+        VT_& _y;
+        const MT_& _l;
+        const VT_& _r;
+    };
+
+    template<typename Algo_, typename VT_>
+    class PreconFunctor : public SolverFunctorBase<VT_>
+    {
+      public:
+        PreconFunctor(VT_& y) :
+          _y(y),
+          _functor()
+        {
+          this->_complete = false;
+        }
+
+        virtual const std::string type_name()
+        {
+          return "PreconFunctor";
+        }
+
+        virtual void execute()
+        {
+          if(!this->_complete)
+            throw ScaRCError("Error: Incomplete PreconFunctor can not be executed!");
+
+          _functor->execute();
+        }
+
+        PreconFunctor& operator=(const PreconFunctor& rhs)
+        {
+          if(this == &rhs)
+            return *this;
+
+          this->_y = rhs._y;
+          this->_functor = rhs._functor;
+          return *this;
+        }
+
+        PreconFunctor(const PreconFunctor& other) :
+          _y(other._y),
+          _functor(other._functor)
+        {
+        }
+
+        virtual void substitute(VT_& arg)
+        {
+        }
+
+        void set_precon_functor(std::shared_ptr<SolverFunctorBase<VT_> >& functor)
+        {
+          functor->substitute(_y);
+          _functor = functor;
+          this->_complete = true;
+        }
+
+      private:
+        VT_& _y;
+        std::shared_ptr<SolverFunctorBase<VT_> > _functor;
+    };
+
+    template<typename Algo_, typename VT_>
+    class CopyFunctor : public SolverFunctorBase<VT_>
+    {
+      public:
+        CopyFunctor(VT_& y, const VT_& x) :
+          _y(y),
+          _x(x)
+        {
+          this->_complete = true;
+        }
+
+        virtual const std::string type_name()
+        {
+          return "CopyFunctor";
+        }
+
+        virtual void execute()
+        {
+          copy(_y, _x);
+        }
+
+        CopyFunctor& operator=(const CopyFunctor& rhs)
+        {
+          if(this == &rhs)
+            return *this;
+
+          this->_y = rhs._y;
+          this->_x = rhs._x;
+          return *this;
+        }
+
+        CopyFunctor(const CopyFunctor& other) :
+          _y(other._y),
+          _x(other._x)
+        {
+        }
+
+        virtual void substitute(VT_& arg)
+        {
+        }
+
+      private:
+        VT_& _y;
+        const VT_& _x;
+    };
+
+    template<typename Algo_, typename VT_, template<typename, typename> class StorageType_ = std::vector>
+    class CompoundSolverFunctor : public SolverFunctorBase<VT_>
+    {
+      public:
+        typedef StorageType_<std::shared_ptr<SolverFunctorBase<VT_> >, std::allocator<std::shared_ptr<SolverFunctorBase<VT_> > > > storage_type_;
+
+        CompoundSolverFunctor() :
+          _functors()
+        {
+          this->_complete = false;
+        }
+
+        virtual void substitute(VT_& arg)
+        {
+          for(Index i(0) ; i < (this->_functors).size() ; ++i)
+          {
+            (this->_functors).at(i)->substitute(arg);
+          }
+        }
+
+        virtual void execute()
+        {
+          for(Index i(0) ; i < (this->_functors).size() ; ++i)
+          {
+            (this->_functors).at(i)->execute();
+          }
+        }
+
+        virtual const std::string type_name()
+        {
+          return "CompoundSolverFunctor";
+        }
+
+        void add_functor(SolverFunctorBase<VT_>* functor)
+        {
+          _functors.push_back(std::shared_ptr<SolverFunctorBase<VT_> >(functor));
+        }
+
+        void add_functor(const std::shared_ptr<SolverFunctorBase<VT_> >& functor)
+        {
+          _functors.push_back(functor);
+        }
+
+        storage_type_& get_functors()
+        {
+          return _functors;
+        }
+
+        Index size()
+        {
+          return Index(_functors.size());
+        }
+
+        const storage_type_& get_functors() const
+        {
+          return _functors;
+        }
+
+        Index size() const
+        {
+          return Index(_functors.size());
+        }
+
+        CompoundSolverFunctor& operator=(const CompoundSolverFunctor& rhs)
+        {
+          if(this == &rhs)
+            return *this;
+
+          this->_functors = rhs._functors;
+          return *this;
+        }
+
+        CompoundSolverFunctor(const CompoundSolverFunctor& other) :
+          _functors(other._functors)
+        {
+        }
+
+        template<typename OVT_>
+        void set_preconditioner(SolverFunctorBase<OVT_>* precon_func)
+        {
+        }
+
+        template<typename OVT_>
+        void set_preconditioner(std::shared_ptr<SolverFunctorBase<OVT_> >& functor)
+        {
+          for(Index i(0) ; i < (this->_functors).size() ; ++i)
+          {
+            if( (this->_functors).at(i)->type_name() == "PreconFunctor")
+            {
+              ((PreconFunctor<Algo_, VT_>*)((this->_functors).at(i).get()))->set_precon_functor(functor);
+              return;
+            }
+          }
+          throw ScaRCError("Error: No PreconFunctor in functor list!");
+        }
+
+      protected:
+        storage_type_ _functors;
+
+    };
+
+/*
     ///ApplicableAsPrecon interface
     template<typename MyFunctorType_, typename VT_>
     class ApplicableAsPrecon
@@ -290,6 +906,7 @@ namespace FEAST
         const MT_& _A;
         const VT_& _x;
     };
+    */
   }
 }
 
