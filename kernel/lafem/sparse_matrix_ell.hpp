aa47a5ba1bbc00e39aeefab36278a26e67591998
Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>
2012-12-03 14:45:01 +0100

    ENH: Added SMELL(SMCOO) constructor
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@math.uni-dortmund.de>

--- kernel/lafem/sparse_matrix_ell.hpp
+++ kernel/lafem/sparse_matrix_ell.hpp
@@ -148,66 +148,68 @@ namespace FEAST
          *
          * \param[in] other The source matrix in COO format.
          *
-         * Creates a ELL matrix based on the COO source matrix from another memory architecture.
+         * Creates a ELL matrix based on the COO source matrix.
          */
-        /*template <typename Arch2_>
-        explicit SparseMatrixELL(const SparseMatrixCOO<Arch2_, DT_> & other) :
-          Container<Arch_, DT_>(other.size()),
-          _rows(other.rows()),
-          _columns(other.columns()),
-          _zero_element(other.zero_element()),
-          _used_elements(other.used_elements())
+        explicit SparseMatrixELL(const SparseMatrixCOO<Mem::Main, DT_> & other_orig) :
+          Container<Arch_, DT_>(other_orig.size()),
+          _rows(other_orig.rows()),
+          _columns(other_orig.columns()),
+          _zero_element(other_orig.zero_element()),
+          _used_elements(other_orig.used_elements())
         {
-          SparseMatrixELL<Archs::CPU, DT_> tother(other);
+          SparseMatrixCSR<Mem::Main, DT_> other(other_orig);
 
-          this->_size = tother.size();
-          this->_rows = tother.rows();
-          this->_columns = tother.columns();
-          this->_used_elements = tother.used_elements();
-          this->_zero_element = tother.zero_element();
+          _Arl = (Index*)MemoryPool<Mem::Main>::instance()->allocate_memory((_rows) * sizeof(Index));
 
-          this->_elements.push_back((DT_*)MemoryPool<Arch_>::instance()->allocate_memory(tother.used_elements() * sizeof(DT_)));
-          this->_elements_size.push_back(this->_used_elements);
-          this->_indices.push_back((Index*)MemoryPool<Arch_>::instance()->allocate_memory(tother.used_elements() * sizeof(Index)));
-          this->_indices_size.push_back(this->_used_elements);
-          this->_indices.push_back((Index*)MemoryPool<Arch_>::instance()->allocate_memory((_rows + 1) * sizeof(Index)));
-          this->_indices_size.push_back(_rows + 1);
+          _num_cols_per_row = 0;
+          for (Index i(0) ; i < _rows ; ++i)
+          {
+            _Arl[i] = other.row_ptr_end()[i] - other.row_ptr()[i];
+            if (_Arl[i] > _num_cols_per_row)
+              _num_cols_per_row = _Arl[i];
+          }
+
+          unsigned long alignment(32);
+          _stride = alignment * ((_rows + alignment - 1)/ alignment);
+
+
+          _Ax = (DT_*)MemoryPool<Mem::Main>::instance()->allocate_memory((_num_cols_per_row * _stride) * sizeof(DT_));
+          _Aj = (Index*)MemoryPool<Mem::Main>::instance()->allocate_memory((_num_cols_per_row * _stride) * sizeof(Index));
+
+          for (unsigned long row(0); row < _rows ; ++row)
+          {
+            unsigned long target(0);
+            for (unsigned long i(0) ; i < _Arl[row] ; ++i)
+            {
+              const Index row_start(other.row_ptr()[row]);
+              //if(other.val()[row_start + i] != DT_(0))
+              {
+                _Aj[row + target * _stride] = (other.col_ind())[row_start + i];
+                _Ax[row + target * _stride] = (other.val())[row_start + i];
+                target++;
+              }
+            }
+          }
+
+          this->_elements.push_back((DT_*)MemoryPool<Arch_>::instance()->allocate_memory(_num_cols_per_row * _stride * sizeof(DT_)));
+          this->_elements_size.push_back(_num_cols_per_row * _stride);
+          this->_indices.push_back((Index*)MemoryPool<Arch_>::instance()->allocate_memory(_num_cols_per_row * _stride * sizeof(Index)));
+          this->_indices_size.push_back(_num_cols_per_row * _stride);
           this->_indices.push_back((Index*)MemoryPool<Arch_>::instance()->allocate_memory(_rows * sizeof(Index)));
           this->_indices_size.push_back(_rows);
 
-          _col_ind = this->_indices.at(0);
-          _val = this->_elements.at(0);
-          _row_ptr = this->_indices.at(1);
-          _row_ptr_end = this->_indices.at(2);
+          MemoryPool<Arch_>::upload(this->get_elements().at(0), _Ax, _num_cols_per_row * _stride * sizeof(DT_));
+          MemoryPool<Arch_>::upload(this->get_indices().at(0), _Aj, _num_cols_per_row * _stride * sizeof(Index));
+          MemoryPool<Arch_>::upload(this->get_indices().at(1), _Arl, _rows * sizeof(Index));
+          MemoryPool<Mem::Main>::instance()->release_memory(_Ax);
+          MemoryPool<Mem::Main>::instance()->release_memory(_Aj);
+          MemoryPool<Mem::Main>::instance()->release_memory(_Arl);
 
-          Index src_size(tother.get_elements_size().at(0) * sizeof(DT_));
-          Index dest_size(tother.get_elements_size().at(0) * sizeof(DT_));
-          void * temp(::malloc(src_size));
-          MemoryPool<Arch2_>::download(temp, tother.get_elements().at(0), src_size);
-          MemoryPool<Arch_>::upload(this->get_elements().at(0), temp, dest_size);
-          ::free(temp);
+          _Ax = this->_elements.at(0);
+          _Aj = this->_indices.at(0);
+          _Arl = this->_indices.at(1);
 
-          src_size = (tother.get_indices_size().at(0) * sizeof(Index));
-          dest_size = (tother.get_indices_size().at(0) * sizeof(Index));
-          temp = (::malloc(src_size));
-          MemoryPool<Arch2_>::download(temp, tother.get_indices().at(0), src_size);
-          MemoryPool<Arch_>::upload(this->get_indices().at(0), temp, dest_size);
-          ::free(temp);
-
-          src_size = (tother.get_indices_size().at(1) * sizeof(Index));
-          dest_size = (tother.get_indices_size().at(1) * sizeof(Index));
-          temp = (::malloc(src_size));
-          MemoryPool<Arch2_>::download(temp, tother.get_indices().at(1), src_size);
-          MemoryPool<Arch_>::upload(this->get_indices().at(1), temp, dest_size);
-          ::free(temp);
-
-          src_size = (tother.get_indices_size().at(2) * sizeof(Index));
-          dest_size = (tother.get_indices_size().at(2) * sizeof(Index));
-          temp = (::malloc(src_size));
-          MemoryPool<Arch2_>::download(temp, tother.get_indices().at(2), src_size);
-          MemoryPool<Arch_>::upload(this->get_indices().at(2), temp, dest_size);
-          ::free(temp);
-        }*/
+        }
 
         /**
          * \brief Copy Constructor
