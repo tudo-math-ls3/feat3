5348797e44fa17506b2cca18edff54d47f3a9ccb
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2012-12-20 13:20:40 +0100

    ENH: added HaloBase for polymorphic data storage per patch
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

--- kernel/foundation/halo.hpp
+++ kernel/foundation/halo.hpp
@@ -14,32 +14,10 @@ namespace FEAST
 {
   namespace Foundation
   {
-    /**
-     * \brief Halo class template represents the interface between two meshes or patches of any kind
-     *
-     * A Halo is used for communication between patches of any kind. Currently, its policy is inspired by mesh patches.
-     * The overlap parameter is used in communication routines to destinct between minimal overlap and element layer overlap.
-     *
-     * \tparam delta_
-     * overlap parameter: delta = 0 => minimal overlap, delta = k > 0 => element layer overlap with k layers.
-     *
-     * \tparam MeshType_
-     * type of the mesh or patch
-     *
-     * \tparam StorageType_
-     * type for inner storage of indices
-     *
-     * \tparam IndexType_
-     * type of the indices
-     *
-     * \author Markus Geveler
-     */
-    template<unsigned delta_,
-             PolytopeLevels level_,
-             typename MeshType_,
+    template<typename MeshType_,
              template<typename, typename> class StorageType_ = std::vector,
              typename IndexType_ = Index>
-    class Halo :
+    class HaloBase :
       public Bufferable<BufferedData<StorageType_> >,
       public Communicateable<BufferedData<StorageType_>, com_send_receive>
     {
@@ -50,25 +28,21 @@ namespace FEAST
         typedef BufferedData<StorageType_> buffer_type_;
 
         ///CTOR
-        Halo(MeshType_ & mesh, IndexType_ other = 0, PolytopeLevels level = pl_edge) : //TODO move to template
+        HaloBase(MeshType_ & mesh, IndexType_ other = 0, PolytopeLevels level = pl_edge) : //TODO move to template
           _halo_elements(),
           _halo_element_counterparts(),
           _mesh(mesh),
-          _other(other),
-          _overlap(delta_),
-          _level(level)
+          _other(other)
         {
         }
 
         ///CTOR from buffer
         template<typename BufferType_>
-        Halo(const BufferType_& buffer, MeshType_& new_mesh, IndexType_ new_other_rank) :
+        HaloBase(const BufferType_& buffer, MeshType_& new_mesh, IndexType_ new_other_rank) :
           _halo_elements(),
           _halo_element_counterparts(),
           _mesh(new_mesh),
-          _other(new_other_rank),
-          _overlap(delta_),
-          _level(level_)
+          _other(new_other_rank)
         {
           for(IndexType_ i(0) ; i < buffer.size() ; ++i)
           {
@@ -77,90 +51,77 @@ namespace FEAST
           }
         }
 
+        ///copy-CTOR
+        HaloBase(const HaloBase& other) : //TODO move to template
+          _halo_elements(other._halo_elements),
+          _halo_element_counterparts(other._halo_element_counterparts),
+          _mesh(other._mesh),
+          _other(other._other)
+        {
+        }
+
         ///DTOR
-        ~Halo()
+        virtual ~HaloBase()
         {
         }
 
         ///Add correspondence of i
-        void add_element_pair(IndexType_ i, IndexType_ j)
+        virtual void add_element_pair(IndexType_ i, IndexType_ j)
         {
           _halo_elements.push_back(i);
           _halo_element_counterparts.push_back(j);
         }
 
         ///public access functions:
-        IndexType_ get_element_counterpart(IndexType_ index) const
+        virtual IndexType_ get_element_counterpart(IndexType_ index) const
         {
           return _halo_element_counterparts.at(index);
         }
 
-        IndexType_ get_element(IndexType_ index) const
+        virtual IndexType_ get_element(IndexType_ index) const
         {
           return _halo_elements.at(index);
         }
 
-        IndexType_ size() const
+        virtual IndexType_ size() const
         {
           return IndexType_(_halo_elements.size());
         }
 
-        const MeshType_ & get_mesh() const
+        virtual const MeshType_ & get_mesh() const
         {
           return _mesh;
         }
 
-        MeshType_ & get_mesh()
+        virtual MeshType_ & get_mesh()
         {
           return _mesh;
         }
 
-        IndexType_ get_other() const
+        virtual IndexType_ get_other() const
         {
           return _other;
         }
 
-        unsigned get_overlap() const
-        {
-          return _overlap;
-        }
+        virtual unsigned get_overlap() const = 0;
+        virtual PolytopeLevels get_level() const = 0;
 
-        PolytopeLevels get_level() const
-        {
-          return _level;
-        }
-
-        Halo& operator=(const Halo& rhs)
-        {
-          if(this == &rhs)
-            return *this;
-
-          this->_halo_elements = rhs._halo_elements;
-          this->_halo_element_counterparts = rhs._halo_element_counterparts;
-          this->_mesh = rhs._mesh;
-          this->_other = rhs._other;
-          this->_overlap = rhs._overlap;
-          this->_level = rhs._level;
-
-          return *this;
-        }
-
-        StorageType_<IndexType_, std::allocator<IndexType_> >& get_elements()
+        virtual StorageType_<IndexType_, std::allocator<IndexType_> >& get_elements()
         {
           return _halo_elements;
         }
 
-        const StorageType_<IndexType_, std::allocator<IndexType_> >& get_elements() const
+        virtual const StorageType_<IndexType_, std::allocator<IndexType_> >& get_elements() const
         {
           return _halo_elements;
         }
 
-        StorageType_<IndexType_, std::allocator<IndexType_> >& get_element_counterparts()
+        virtual StorageType_<IndexType_, std::allocator<IndexType_> >& get_element_counterparts()
         {
           return _halo_element_counterparts;
         }
 
-        const StorageType_<IndexType_, std::allocator<IndexType_> >& get_element_counterparts() const
+        virtual const StorageType_<IndexType_, std::allocator<IndexType_> >& get_element_counterparts() const
         {
           return _halo_element_counterparts;
         }
@@ -210,7 +171,7 @@ namespace FEAST
         }
 
         ///implementation of Communicateable interface
-        void send_recv(BufferedData<StorageType_>& sendbuffers,
+        virtual void send_recv(BufferedData<StorageType_>& sendbuffers,
                        int destrank,
                        BufferedData<StorageType_>& recvbuffers,
                        int sourcerank)
@@ -252,13 +213,105 @@ namespace FEAST
 #endif
         }
 
-      private:
+      protected:
         StorageType_<IndexType_, std::allocator<IndexType_> > _halo_elements;
         StorageType_<IndexType_, std::allocator<IndexType_> > _halo_element_counterparts;
 
         MeshType_ & _mesh;
         IndexType_ _other;
 
+    };
+
+    /**
+     * \brief Halo class template represents the interface between two meshes or patches of any kind
+     *
+     * A Halo is used for communication between patches of any kind. Currently, its policy is inspired by mesh patches.
+     * The overlap parameter is used in communication routines to destinct between minimal overlap and element layer overlap.
+     *
+     * \tparam delta_
+     * overlap parameter: delta = 0 => minimal overlap, delta = k > 0 => element layer overlap with k layers.
+     *
+     * \tparam MeshType_
+     * type of the mesh or patch
+     *
+     * \tparam StorageType_
+     * type for inner storage of indices
+     *
+     * \tparam IndexType_
+     * type of the indices
+     *
+     * \author Markus Geveler
+     */
+    template<unsigned delta_,
+             PolytopeLevels level_,
+             typename MeshType_,
+             template<typename, typename> class StorageType_ = std::vector,
+             typename IndexType_ = Index>
+    class Halo :
+      public HaloBase<MeshType_, StorageType_, IndexType_>
+    {
+      public:
+        ///type exports:
+        typedef typename HaloBase<MeshType_, StorageType_, IndexType_>::index_type_ index_type_;
+        typedef typename HaloBase<MeshType_, StorageType_, IndexType_>::mesh_type_ mesh_type_;
+        typedef typename HaloBase<MeshType_, StorageType_, IndexType_>::buffer_type_ buffer_type_;
+
+        ///CTOR
+        Halo(MeshType_ & mesh, IndexType_ other = 0, PolytopeLevels level = pl_edge) : //TODO move to template
+          HaloBase<MeshType_, StorageType_, IndexType_>(mesh, other, level),
+          _overlap(delta_),
+          _level(level)
+        {
+        }
+
+        ///CTOR from buffer
+        template<typename BufferType_>
+        Halo(const BufferType_& buffer, MeshType_& new_mesh, IndexType_ new_other_rank) :
+          HaloBase<MeshType_, StorageType_, IndexType_>(buffer, new_mesh, new_other_rank),
+          _overlap(delta_),
+          _level(level_)
+        {
+          for(IndexType_ i(0) ; i < buffer.size() ; ++i)
+          {
+            this->_halo_elements.push_back(buffer.get_element(i));
+            this->_halo_element_counterparts.push_back(buffer.get_element_counterpart(i));
+          }
+        }
+
+        ///copy-CTOR
+        Halo(const Halo& other) : //TODO move to template
+          HaloBase<MeshType_, StorageType_, IndexType_>(other),
+          _overlap(other._overlap),
+          _level(other._level)
+        {
+        }
+
+        virtual unsigned get_overlap() const
+        {
+          return _overlap;
+        }
+
+        virtual PolytopeLevels get_level() const
+        {
+          return _level;
+        }
+
+        Halo& operator=(const Halo& rhs)
+        {
+          if(this == &rhs)
+            return *this;
+
+          this->_halo_elements = rhs._halo_elements;
+          this->_halo_element_counterparts = rhs._halo_element_counterparts;
+          this->_mesh = rhs._mesh;
+          this->_other = rhs._other;
+          this->_overlap = rhs._overlap;
+          this->_level = rhs._level;
+
+          return *this;
+        }
+
+      private:
         unsigned _overlap;
 
         PolytopeLevels _level;
