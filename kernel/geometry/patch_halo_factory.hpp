cff9590cda59be8290c0c707f329b5be2c7cdb8b
Peter Zajac <pzajac@math.tu-dortmund.de>
2013-01-21 13:44:38 +0100

    ENH: added PatchHaloFactory class template
    Signed-off-by: Peter Zajac <pzajac@math.tu-dortmund.de>

---
+++ kernel/geometry/patch_halo_factory.hpp
@@ -0,0 +1,131 @@
+#pragma once
+#ifndef KERNEL_GEOMETRY_PATCH_HALO_FACTORY_HPP
+#define KERNEL_GEOMETRY_PATCH_HALO_FACTORY_HPP 1
+
+// includes, FEAST
+#include <kernel/geometry/cell_sub_set.hpp>
+
+// includes, system
+#include <map>
+
+namespace FEAST
+{
+  namespace Geometry
+  {
+    /// \cond internal
+    namespace Intern
+    {
+      template<typename Shape_>
+      struct PatchHaloWrapper;
+    };
+
+    /// \endcond
+    template<typename Mesh_>
+    class PatchHaloFactory DOXY({});
+
+    template<typename Shape_>
+    class PatchHaloFactory< CellSubSet<Shape_> > :
+      public Factory< CellSubSet<Shape_> >
+    {
+    public:
+      /// mesh type
+      typedef CellSubSet<Shape_> MeshType;
+      /// target set holder type
+      typedef typename MeshType::TargetSetHolderType TargetSetHolderType;
+
+    private:
+      const MeshType& _patch_set;
+      const MeshType& _halo_set;
+
+      typedef std::map<Index, Index> IndexMap;
+
+      IndexMap _ics[Shape_::dimension + 1];
+
+    public:
+      explicit PatchHaloFactory(const MeshType& patch_set, const MeshType& halo_set) :
+        _patch_set(patch_set),
+        _halo_set(halo_set)
+      {
+        Intern::PatchHaloWrapper<Shape_>::build(_ics, _patch_set.get_target_set_holder());
+      }
+
+      virtual Index get_num_entities(int dim)
+      {
+        return _halo_set.get_num_entities(dim);
+      }
+
+      virtual void fill_target_sets(TargetSetHolderType& target_set_holder)
+      {
+        Intern::PatchHaloWrapper<Shape_>::map(_ics, _halo_set.get_target_set_holder(), target_set_holder);
+      }
+    }; // class PatchHaloFactory<CellSubSet<...>>
+
+    /// \cond internal
+    namespace Intern
+    {
+      template<typename Shape_>
+      struct PatchHaloHelper
+      {
+        static void build(std::map<Index, Index>& idx_map, const TargetSet& trg_set)
+        {
+          const Index num_entities(trg_set.get_num_entities());
+          for(Index i(0); i < num_entities; ++i)
+          {
+            bool okay = idx_map.insert(std::make_pair(trg_set[i], i)).second;
+            ASSERT_(okay);
+          }
+        }
+
+        static void map(const std::map<Index, Index>& ics, const TargetSet& trg_in, TargetSet& trg_out)
+        {
+          const Index n(trg_in.get_num_entities());
+          for(Index i(0); i < n; ++i)
+          {
+            std::map<Index, Index>::const_iterator it(ics.find(trg_in[i]));
+            ASSERT_(it != ics.end());
+            trg_out[i] = it->second;
+          }
+        }
+      };
+
+      template<typename Shape_>
+      struct PatchHaloWrapper
+      {
+        static void build(std::map<Index, Index>* idx_map, const TargetSetHolder<Shape_>& trg_holder)
+        {
+          typedef typename Shape::FaceTraits<Shape_, Shape_::dimension - 1>::ShapeType FacetType;
+          PatchHaloWrapper<FacetType>::build(idx_map, trg_holder);
+          PatchHaloHelper<Shape_>::build(idx_map[Shape_::dimension],
+            trg_holder.template get_target_set<Shape_::dimension>());
+        }
+
+        static void map(std::map<Index, Index>* idx_map, const TargetSetHolder<Shape_>& trg_in, TargetSetHolder<Shape_>& trg_out)
+        {
+          typedef typename Shape::FaceTraits<Shape_, Shape_::dimension - 1>::ShapeType FacetType;
+          PatchHaloWrapper<FacetType>::map(idx_map, trg_in, trg_out);
+          PatchHaloHelper<Shape_>::map(idx_map[Shape_::dimension],
+            trg_in.template get_target_set<Shape_::dimension>(),
+            trg_out.template get_target_set<Shape_::dimension>());
+        }
+      };
+
+
+      template<>
+      struct PatchHaloWrapper<Shape::Vertex>
+      {
+        static void build(std::map<Index, Index>* idx_map, const TargetSetHolder<Shape::Vertex>& trg_holder)
+        {
+          PatchHaloHelper<Shape::Vertex>::build(idx_map[0], trg_holder.get_target_set<0>());
+        }
+
+        static void map(std::map<Index, Index>* idx_map, const TargetSetHolder<Shape::Vertex>& trg_in, TargetSetHolder<Shape::Vertex>& trg_out)
+        {
+          PatchHaloHelper<Shape::Vertex>::map(idx_map[0], trg_in.get_target_set<0>(), trg_out.get_target_set<0>());
+        }
+      };
+    }
+    /// \endcond
+  } // namespace Geometry
+} // namespace FEAST
+
+#endif // KERNEL_GEOMETRY_PATCH_HALO_FACTORY_HPP
---
+++ tools/dbg_patch/dbg_patch.cpp
@@ -0,0 +1,210 @@
+#include <kernel/geometry/conformal_mesh.hpp>
+#include <kernel/geometry/cell_sub_set.hpp>
+#include <kernel/geometry/patch_factory.hpp>
+#include <kernel/geometry/macro_factory.hpp>
+#include <kernel/geometry/patch_halo_factory.hpp>
+#include <kernel/geometry/test_aux/tetris_factory.hpp>
+
+using namespace FEAST;
+
+typedef Geometry::ConformalMesh<Shape::Quadrilateral> QuadMesh;
+typedef Geometry::CellSubSet<Shape::Quadrilateral> QuadCellSet;
+typedef Geometry::TestAux::TetrisFactory<QuadMesh> TetrisFactory;
+typedef Geometry::StandardRefinery<QuadMesh> Refinery;
+typedef Geometry::PatchFactory<QuadMesh> PatchFactory;
+typedef Geometry::MacroFactory<QuadMesh> MacroFactory;
+typedef Geometry::PatchHaloFactory<QuadCellSet> PatchMapFactory;
+
+//*
+//template<typename T_>
+class PatchSetFactory :
+  public Geometry::Factory<QuadCellSet>
+{
+public:
+  enum Region
+  {
+    lower_patch,
+    upper_patch,
+    lower_interface,
+    upper_interface,
+    base_interface
+  };
+
+protected:
+  Region _region;
+
+public:
+  explicit PatchSetFactory(Region region) :
+    _region(region)
+  {
+  }
+
+  virtual Index get_num_entities(int dim)
+  {
+    switch(_region)
+    {
+    case lower_patch:
+    case upper_patch:
+      switch(dim)
+      {
+      case 0:
+        return 6;
+      case 1:
+        return 7;
+      case 2:
+        return 2;
+      }
+
+    case lower_interface:
+    case upper_interface:
+    case base_interface:
+      switch(dim)
+      {
+      case 0:
+        return 2;
+      case 1:
+        return 1;
+      }
+    }
+    return 0;
+  }
+
+  virtual void fill_target_sets(TargetSetHolderType& target_set_holder)
+  {
+    switch(_region)
+    {
+    case lower_patch:
+      _fill_lower_patch(target_set_holder);
+      break;
+    case upper_patch:
+      _fill_upper_patch(target_set_holder);
+      break;
+    case lower_interface:
+      _fill_lower_interface(target_set_holder);
+      break;
+    case upper_interface:
+      _fill_upper_interface(target_set_holder);
+      break;
+    case base_interface:
+      _fill_base_interface(target_set_holder);
+      break;
+    }
+  }
+
+protected:
+  void _fill_lower_patch(TargetSetHolderType& target_set_holder)
+  {
+    Geometry::TargetSet& vi(target_set_holder.get_target_set<0>());
+    vi[0] = 0;
+    vi[1] = 1;
+    vi[2] = 2;
+    vi[3] = 4;
+    vi[4] = 5;
+    vi[5] = 6;
+
+    Geometry::TargetSet& ei(target_set_holder.get_target_set<1>());
+    ei[0] = 0;
+    ei[1] = 1;
+    ei[2] = 2;
+    ei[3] = 3;
+    ei[4] = 4;
+    ei[5] = 6;
+    ei[6] = 7;
+
+    Geometry::TargetSet& qi(target_set_holder.get_target_set<2>());
+    qi[0] = 0;
+    qi[1] = 1;
+  }
+
+  void _fill_upper_patch(TargetSetHolderType& target_set_holder)
+  {
+    Geometry::TargetSet& vi(target_set_holder.get_target_set<0>());
+    vi[0] = 3;
+    vi[1] = 4;
+    vi[2] = 5;
+    vi[3] = 7;
+    vi[4] = 8;
+    vi[5] = 9;
+
+    Geometry::TargetSet& ei(target_set_holder.get_target_set<1>());
+    ei[0] = 5;
+    ei[1] = 6;
+    ei[2] = 8;
+    ei[3] = 9;
+    ei[4] = 10;
+    ei[5] = 11;
+    ei[6] = 12;
+
+    Geometry::TargetSet& qi(target_set_holder.get_target_set<2>());
+    qi[0] = 2;
+    qi[1] = 3;
+  }
+
+  void _fill_lower_interface(TargetSetHolderType& target_set_holder)
+  {
+    Geometry::TargetSet& vi(target_set_holder.get_target_set<0>());
+    vi[0] = 3;
+    vi[1] = 4;
+
+    Geometry::TargetSet& ei(target_set_holder.get_target_set<1>());
+    ei[0] = 5;
+  }
+
+  void _fill_upper_interface(TargetSetHolderType& target_set_holder)
+  {
+    Geometry::TargetSet& vi(target_set_holder.get_target_set<0>());
+    vi[0] = 1;
+    vi[1] = 2;
+
+    Geometry::TargetSet& ei(target_set_holder.get_target_set<1>());
+    ei[0] = 1;
+  }
+
+  void _fill_base_interface(TargetSetHolderType& target_set_holder)
+  {
+    Geometry::TargetSet& vi(target_set_holder.get_target_set<0>());
+    vi[0] = 4;
+    vi[1] = 5;
+
+    Geometry::TargetSet& ei(target_set_holder.get_target_set<1>());
+    ei[0] = 6;
+  }
+};
+//*/
+
+int main(int /*argc*/, char** /*argv*/)
+{
+  // create base-mesh
+  TetrisFactory factory;
+  QuadMesh base_mesh(factory);
+
+  // build patch cellsets
+  PatchSetFactory patch_set_factory_l(PatchSetFactory::lower_patch);
+  PatchSetFactory patch_set_factory_u(PatchSetFactory::upper_patch);
+  QuadCellSet patchset_l(patch_set_factory_l);
+  QuadCellSet patchset_u(patch_set_factory_u);
+
+  // create patch meshes
+  PatchFactory patch_factory_l(base_mesh, patchset_l);
+  PatchFactory patch_factory_u(base_mesh, patchset_u);
+  QuadMesh patch_l(patch_factory_l);
+  QuadMesh patch_u(patch_factory_u);
+
+  // create base-interface cellset
+  PatchSetFactory interf_set_factory_b(PatchSetFactory::base_interface);
+  QuadCellSet interf_set_b(interf_set_factory_b);
+
+  // create patch-interface cellsets
+  PatchSetFactory interf_set_factory_l(PatchSetFactory::lower_interface);
+  PatchSetFactory interf_set_factory_u(PatchSetFactory::upper_interface);
+  QuadCellSet interf_set_l(interf_set_factory_l);
+  QuadCellSet interf_set_u(interf_set_factory_u);
+
+  // create interface cellsets by base-interface cellset and patchsets
+  PatchMapFactory interf_map_factory_l(patchset_l, interf_set_b);
+  PatchMapFactory interf_map_factory_u(patchset_u, interf_set_b);
+  QuadCellSet interf_map_l(interf_map_factory_l);
+  QuadCellSet interf_map_u(interf_map_factory_u);
+
+  // TODO: validate interface cellsets
+}
