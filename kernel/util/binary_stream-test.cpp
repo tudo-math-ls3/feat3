7852583fbd350a7084cc402924d8588ee414eaaf
Peter Zajac <pzajac@math.tu-dortmund.de>
2013-01-23 12:25:57 +0100

    ENH: BinaryStream class
    Signed-off-by: Peter Zajac <pzajac@math.tu-dortmund.de>

---
+++ kernel/util/binary_stream-test.cpp
@@ -0,0 +1,75 @@
+#include <test_system/test_system.hpp>
+#include <kernel/util/binary_stream.hpp>
+#include <stdint.h>
+
+using namespace FEAST;
+using namespace FEAST::TestSystem;
+
+/**
+ * \brief Test class for the BinaryStream class.
+ *
+ * \test Tests the BinaryStream class template.
+ *
+ * \author Peter Zajac
+ */
+class BinaryStreamTest
+  : public TaggedTest<Archs::None, Archs::None>
+{
+public:
+  BinaryStreamTest() :
+    TaggedTest<Archs::None, Archs::None>("binary_stream_test")
+  {
+  }
+
+  static bool little_endian()
+  {
+    int32_t x = 1;
+    return *((int16_t*)&x) == 1;
+  }
+
+  virtual void run() const
+  {
+    BinaryStream bs;
+
+    // write something to the stream
+    const uint32_t db = 0xDEADBEEF;
+    bs.write((const char*)&db, sizeof(uint32_t));
+
+    // check stream position
+    TEST_CHECK_EQUAL(size_t(bs.tellg()), sizeof(uint32_t));
+
+    // seek back 2 bytes
+    bs.seekp(-2, std::ios_base::cur);
+
+    // read 2 bytes
+    uint16_t d = 0;
+    bs.read((char*)&d, sizeof(uint16_t));
+    if(little_endian())
+      TEST_CHECK_EQUAL(d, 0xDEAD); // high word
+    else
+      TEST_CHECK_EQUAL(d, 0xBEEF); // low word
+
+    // write something else to the stream
+    const uint32_t lc = 0x1337C0DE;
+    bs.write((const char*)&lc, sizeof(uint32_t));
+
+    // check stream position
+    TEST_CHECK_EQUAL(size_t(bs.tellg()), 2*sizeof(uint32_t));
+
+    // seek to begin
+    bs.seekg(0);
+
+    // read the whole stream
+    uint32_t dblc[2] = {0, 0};
+    bs.read((char*)dblc, 2*sizeof(uint32_t));
+    TEST_CHECK_EQUAL(dblc[0], db);
+    TEST_CHECK_EQUAL(dblc[1], lc);
+
+    // check stream size
+    TEST_CHECK_EQUAL(bs.size(), std::streamsize(8));
+
+    // clear stream and check size
+    bs.clear();
+    TEST_CHECK_EQUAL(bs.size(), std::streamsize(0));
+  }
+} binary_stream_test;
---
+++ kernel/util/binary_stream.hpp
@@ -0,0 +1,205 @@
+#pragma once
+#ifndef KERNEL_UTIL_BINARY_STREAM_HPP
+#define KERNEL_UTIL_BINARY_STREAM_HPP 1
+
+// includes, system
+#include <vector>
+#include <iostream>
+
+namespace FEAST
+{
+  /**
+   * \brief Binary Stream class
+   *
+   * This class implements a dynamic binary memory input/output stream implementing the std::iostream interface.
+   *
+   * \warning This class is not yet fully matured and shall therefore be used with care!
+   *
+   * This class can be used to write and read binary data into/from memory to test functions which usually
+   * use the std::[i|o]fstream classes for input and output from/into files.
+   *
+   * This class is the binary counterpart of the text-based std::stringstream class.
+   *
+   * \author Peter Zajac
+   */
+  class BinaryStream :
+    public std::iostream
+  {
+  private:
+    /// \cond internal
+    /**
+     * \brief Binary Stream Buffer class
+     *
+     * This class implements the std::basic_streambuf<char> interface using a std::vector as a data container.
+     *
+     * \author Peter Zajac
+     */
+    class BinaryBuffer :
+      public std::basic_streambuf<char>
+    {
+    private:
+      typedef std::vector<char>::size_type datasize;
+      typedef std::streamsize streamsize;
+
+      /// the data vector
+      std::vector<char> _data;
+      /// the current stream position
+      datasize _data_pos;
+
+    public:
+      BinaryBuffer() :
+        _data_pos(0)
+      {
+      }
+
+      streamsize size() const
+      {
+        return streamsize(_data.size());
+      }
+
+      void clear()
+      {
+        _data.clear();
+        _data_pos = datasize(0);
+      }
+
+      char* data()
+      {
+        return _data.data();
+      }
+
+      const char* data() const
+      {
+        return _data.data();
+      }
+
+    protected:
+      virtual std::streamsize xsputn(const char* ptr, std::streamsize count)
+      {
+        streamsize copied(0);
+
+        // Ensure that the vector has sufficient length; if not, then resize the vector
+        if(_data_pos + datasize(count) > _data.size())
+          _data.resize(_data_pos + datasize(count));
+
+        // copy the data
+        for(; copied < count; ++_data_pos, ++copied, ++ptr)
+          _data[_data_pos] = *ptr;
+
+        return copied;
+      }
+
+      virtual std::streamsize xsgetn(char* ptr, std::streamsize count)
+      {
+        streamsize copied(0);
+        datasize len(_data.size());
+
+        // Ensure that we do not read beyond the end of the buffer.
+        if(_data_pos + datasize(count) > len)
+          count = streamsize(len - _data_pos);
+
+        // copy the data
+        for(; copied < count; ++_data_pos, ++copied, ++ptr)
+          *ptr = _data[_data_pos];
+
+        return copied;
+      }
+
+      virtual pos_type seekoff(
+        off_type off,
+        std::ios_base::seekdir way,
+        std::ios_base::openmode which = std::ios_base::in | std::ios_base::out)
+      {
+        if((which & (std::ios_base::in|std::ios_base::out)) == 0)
+          return pos_type(-1);
+
+        switch(way)
+        {
+        case std::ios_base::beg:
+          if((off < 0) || (off > off_type(_data.size())))
+            return pos_type(-1);
+          return _data_pos = datasize(off);
+
+        case std::ios_base::cur:
+          if((off < 0) && (-off > off_type(_data_pos)))
+            return pos_type(-1);
+          if((off > 0) && (off_type(_data_pos) + off > off_type(_data.size())))
+            return pos_type(-1);
+          return _data_pos = datasize(off_type(_data_pos) + off);
+
+        case std::ios_base::end:
+          if((off > 0) || (-off > off_type(_data.size())))
+            return pos_type(-1);
+          return _data_pos = datasize(off_type(_data.size()) + off);
+
+        default:
+          return pos_type(-1);
+        }
+      }
+
+      virtual pos_type seekpos(
+        pos_type pos,
+        std::ios_base::openmode which = std::ios_base::in | std::ios_base::out)
+      {
+        if((which & (std::ios_base::in|std::ios_base::out)) == 0)
+          return pos_type(-1);
+
+        // Ensure the position is not out-of-bounds
+        if(pos >= pos_type(_data.size()))
+          return pos_type(-1);
+
+        // move stream position
+        _data_pos = datasize(pos);
+
+        return pos_type(_data_pos);
+      }
+    };
+    /// \endcond
+
+    // the stream's buffer
+    BinaryBuffer _buffer;
+
+  public:
+    /// default CTOR
+    BinaryStream() :
+      std::iostream(&_buffer)
+    {
+    }
+
+    /**
+     * \brief Returns the current size of the stream.
+     * \returns The current size of the stream in bytes.
+     */
+    std::streamsize size() const
+    {
+      return _buffer.size();
+    }
+
+    /**
+     * \brief Clears the current stream.
+     */
+    void clear()
+    {
+      _buffer.clear();
+    }
+
+    /**
+     * \brief Returns the data array of the stream.
+     * \returns The data array of the stream.
+     */
+    char* data()
+    {
+      return _buffer.data();
+    }
+
+    /**
+     * \brief Returns the data array of the stream.
+     * \returns The data array of the stream.
+     */
+    const char* data() const
+    {
+      return _buffer.data();
+    }
+  }; // class BinaryStream
+} // namespace FEAST
+#endif // KERNEL_UTIL_BINARY_STREAM_HPP
