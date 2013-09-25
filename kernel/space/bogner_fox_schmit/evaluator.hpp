c07f341c0c00179b75bae9b29876a4b03ce9c2c7
Peter Zajac <pzajac@math.tu-dortmund.de>
2013-09-25 10:59:29 +0200

    FIX: erroneous friend declaration
    Signed-off-by: Peter Zajac <pzajac@math.tu-dortmund.de>

--- kernel/space/bogner_fox_schmit/evaluator.hpp
+++ kernel/space/bogner_fox_schmit/evaluator.hpp
@@ -142,7 +142,6 @@ namespace FEAST
       public:
         /// base-class typedef
         typedef ParametricEvaluator<Evaluator, TrafoEvaluator_, SpaceEvalTraits_, ReferenceCapabilities> BaseClass;
-        friend BaseClass;
 
         /// space type
         typedef Space_ SpaceType;
