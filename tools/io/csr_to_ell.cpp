2f9becade3815f87f4907ebb19c272d42983a982
Dirk Ribbrock <dirk.ribbrock@math.tu-dortmund.de>
2014-01-21 12:03:11 +0100

    ENH: Added csr2ell and ell2csr convertion tools
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@math.tu-dortmund.de>

---
+++ tools/io/csr_to_ell.cpp
@@ -0,0 +1,22 @@
+#include <kernel/lafem/sparse_matrix_ell.hpp>
+#include <kernel/lafem/sparse_matrix_csr.hpp>
+#include <iostream>
+
+using namespace FEAST;
+using namespace FEAST::LAFEM;
+
+int main(int argc, char ** argv)
+{
+    if (argc != 3)
+    {
+        std::cout<<"Usage 'csr2ell csr-file ell-file'"<<std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    String input(argv[1]);
+    String output(argv[2]);
+
+    SparseMatrixCSR<Mem::Main, double> csr(input);
+    SparseMatrixELL<Mem::Main, double> ell(csr);
+    ell.write_out(fm_ell, output);
+}
---
+++ tools/io/ell_to_csr.cpp
@@ -0,0 +1,22 @@
+#include <kernel/lafem/sparse_matrix_ell.hpp>
+#include <kernel/lafem/sparse_matrix_csr.hpp>
+#include <iostream>
+
+using namespace FEAST;
+using namespace FEAST::LAFEM;
+
+int main(int argc, char ** argv)
+{
+    if (argc != 3)
+    {
+        std::cout<<"Usage 'ell2csr ell-file csr-file'"<<std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    String input(argv[1]);
+    String output(argv[2]);
+
+    SparseMatrixELL<Mem::Main, double> ell(input);
+    SparseMatrixCSR<Mem::Main, double> csr(ell);
+    csr.write_out(fm_csr, output);
+}
