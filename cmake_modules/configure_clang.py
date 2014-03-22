97e5e0e0fb2bb51e3e92e107bda5ccca9146f8a4
Dirk Ribbrock <dirk.ribbrock@math.tu-dortmund.de>
2014-03-22 10:39:02 +0100

    SYS: Disabled clang gold linker
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@math.tu-dortmund.de>

--- cmake_modules/configure_clang.py
+++ cmake_modules/configure_clang.py
@@ -14,7 +14,7 @@ def configure_clang(cpu, buildmode):
   if buildmode == "debug":
     cxxflags += " -O0 -Wall -Wextra -Wshadow -Wundef -Wshorten-64-to-32 -Wconversion -Wstrict-aliasing=2 -Wunknown-pragmas -Wundef -Wno-unused-value -Wno-unused-parameter -fdiagnostics-show-template-tree -fdiagnostics-show-category=name"
   elif "opt" in buildmode:
-    cxxflags += " -O4"
+    cxxflags += " -O3"
     if cpu == "unknown":
       cxxflags += " -march=native"
 
