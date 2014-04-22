4b65aa8b612fbcb0546b8a32bf5158f931b8b7b0
Jordi Paul <jordi.paul@math.tu-dortmund.de>
2014-04-22 14:02:58 +0200

    ENH: Added distance function to common functions.
    Signed-off-by: Jordi Paul <jordi.paul@math.tu-dortmund.de>

--- kernel/assembly/common_functions.hpp
+++ kernel/assembly/common_functions.hpp
@@ -266,6 +266,128 @@ namespace FEAST
         {
         }
       }; // class ConstantFunction
+
+
+      /**
+       * \brief Analytic distance function
+       *
+       * This class implements the AnalyticFunction interface representing the distance function
+       * \f$ f(x) =\| x - x_0 \|_2 \f$
+       *
+       * This class supports function values, gradient and hessians for all dimensions.
+       *
+       * WARNING: Because the function is differentiable everywhere except for 0, Bad Things (TM) might happen if
+       * someone wants to compute the gradient or hessian there.
+       *
+       * \tparam ImgPointType_
+       * The type of \f$ x_0 \f$.
+       *
+       * \author Jordi Paul
+       */
+      template<typename ImgPointType_>
+      class DistanceFunction :
+        public AnalyticFunction
+      {
+      public:
+        /** \copydoc AnalyticFunction::FunctionCapabilites */
+        enum FunctionCapabilities
+        {
+          can_value = 1,
+          can_grad = 1,
+          can_hess = 1
+        };
+
+        /** \copydoc AnalyticFunction::ConfigTraits */
+        template<typename Config_>
+        struct ConfigTraits
+        {
+          struct TrafoConfig :
+            public Trafo::ConfigBase
+          {
+            enum
+            {
+              need_img_point = 1
+            };
+          };
+        };
+
+        /** \copydoc AnalyticFunction::Evaluator */
+        template<typename EvalTraits_>
+        class Evaluator :
+          public AnalyticFunction::Evaluator<EvalTraits_>
+        {
+        public:
+          /// trafo evaluator data
+          typedef typename EvalTraits_::TrafoEvaluator TrafoEvaluator;
+          /// trafo data type
+          typedef typename EvalTraits_::TrafoData TrafoData;
+          /// coefficient data type
+          typedef typename EvalTraits_::DataType DataType;
+          /// value type
+          typedef typename EvalTraits_::ValueType ValueType;
+          /// gradient type
+          typedef typename EvalTraits_::GradientType GradientType;
+          /// hessian type
+          typedef typename EvalTraits_::HessianType HessianType;
+          /// type for the points the analytic function is evaluated at
+          typedef typename EvalTraits_::ImagePointType ImgPointType;
+
+        private:
+          const DistanceFunction& _function;
+
+        public:
+          explicit Evaluator(const DistanceFunction& function) :
+            _function(function)
+          {
+          }
+
+          ValueType value(const TrafoData& tau) const
+          {
+            ImgPointType tmp (tau.img_point - _function._point);
+            return tmp.norm_euclid();
+          }
+
+          GradientType gradient(const TrafoData& tau) const
+          {
+            GradientType grad(DataType(0));
+            DataType norm(value(tau));
+
+            for(Index d(0); d < TrafoData::image_dim; ++d)
+              grad(d,tau.img_point[d] - _function._point(d));
+
+            return grad/norm;
+          }
+
+          HessianType hessian(const TrafoData& tau) const
+          {
+            HessianType hess(DataType(0));
+            DataType norm(value(tau));
+            norm = DataType(1)/norm;
+            DataType denom = Math::sqr(norm)*norm;
+
+            for(Index i(0); i < TrafoData::image_dim; ++i)
+            {
+              hess(i,i, DataType(1)*norm);
+              for(Index j(0); j < TrafoData::image_dim; ++i)
+                hess(i,j,
+                    ((tau.img_point[i] - _function._point(i)) * (tau.img_point[j] - _function._point(j)) )*denom);
+            }
+
+
+            return hess;
+          }
+        }; // class DistanceFunction::Evaluator<...>
+
+      public:
+        ImgPointType_ _point;
+
+      public:
+        explicit DistanceFunction(ImgPointType_ x0_) :
+          _point(x0_)
+        {
+        }
+      }; // class DistanceFunction
+
     } // namespace Common
   } // namespace Assembly
 } // namespace FEAST
