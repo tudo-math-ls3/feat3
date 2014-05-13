1d2f8ee126d4a34b6dc75f04c02bb1af5a0e9255
Christoph Lohmann <christoph.lohmann@math.tu-dortmund.de>
2014-05-13 13:31:26 +0200

    ENH: Add 'ScatterAxpy<LAFEM::SparseMatrixCOO<Mem::Main,...>>'
    Signed-off-by: Christoph Lohmann <christoph.lohmann@math.tu-dortmund.de>

--- kernel/lafem/gather_scatter.hpp
+++ kernel/lafem/gather_scatter.hpp
@@ -436,6 +436,136 @@ namespace FEAST
     }; // class GatherAxpy<LAFEM::SparseMatrixCSR<Mem::Main,...>>
 
     /**
+     * \brief Scatter-Axpy specialisation for SparseMatrixCOO
+     *
+     * \author Christoph Lohmann
+     */
+    template<typename DataType_, typename IndexType_>
+    class ScatterAxpy< LAFEM::SparseMatrixCOO<Mem::Main, DataType_, IndexType_> >
+    {
+    public:
+      typedef LAFEM::SparseMatrixCOO<Mem::Main, DataType_, IndexType_> MatrixType;
+      typedef Mem::Main MemType;
+      typedef DataType_ DataType;
+      typedef IndexType_ IndexType;
+
+    private:
+#ifdef DEBUG
+      IndexType_ _deadcode;
+#endif
+      Index _num_rows;
+      Index _num_cols;
+      Index _used_elements;
+      IndexType_* _row_idx;
+      IndexType_* _col_idx;
+      IndexType_* _col_ptr;
+      DataType_ *_data;
+
+    public:
+      explicit ScatterAxpy(MatrixType& matrix) :
+#ifdef DEBUG
+        _deadcode(~IndexType_(0)),
+#endif
+        _num_rows(matrix.rows()),
+        _num_cols(matrix.columns()),
+        _used_elements(matrix.used_elements()),
+        _row_idx(matrix.row_indices()),
+        _col_idx(matrix.column_indices()),
+        _col_ptr(nullptr),
+        _data(matrix.val())
+      {
+        // allocate column-pointer array
+        _col_ptr = new IndexType_[matrix.columns()];
+#ifdef DEBUG
+        for(Index i(0); i < _num_cols; ++i)
+        {
+          _col_ptr[i] = _deadcode;
+        }
+#endif
+      }
+
+      virtual ~ScatterAxpy()
+      {
+        if(_col_ptr != nullptr)
+        {
+          delete [] _col_ptr;
+        }
+      }
+
+      template<typename LocalData_>
+      void operator()(const LocalData_& loc_mat,
+                      DataType_ alpha = DataType_(1))
+      {
+        // loop over all local row entries
+        for(Index i(0); i < loc_mat.get_num_rows(); ++i)
+        {
+          // loop over all row entry contributations
+          for(Index ic(0); ic < loc_mat.get_num_row_contribs(i); ++ic)
+          {
+            // fetch row entry weight and pre-multiply by alpha
+            DataType_ iw = alpha * DataType_(loc_mat.get_row_weight(i, ic));
+
+            // fetch row index
+            Index ix = loc_mat.get_row_index(i, ic);
+
+            // build column pointer for this row entry contribution
+            IndexType_ k(0);
+            while (_row_idx[k] < ix)
+            {
+              ++k;
+            }
+            while (k < _used_elements && _row_idx[k] <= ix)
+            {
+              _col_ptr[_col_idx[k]] = k;
+              ++k;
+            }
+
+            // loop over all local column entries
+            for(Index j(0); j < loc_mat.get_num_cols(); ++j)
+            {
+              // loop over all column entry contributions
+              for(Index jc(0); jc < loc_mat.get_num_col_contribs(j); ++jc)
+              {
+                // fetch trial function dof weight
+                DataType_ jw = DataType_(loc_mat.get_col_weight(j, jc));
+
+                // fetch column index
+                Index jx = loc_mat.get_col_index(j, jc);
+
+#ifdef DEBUG
+                // ensure that the column pointer is valid for this index
+                ASSERT(_col_ptr[jx] != _deadcode, "invalid column index");
+#endif
+
+                // incorporate data into global matrix
+                _data[_col_ptr[jx]] += iw * jw * loc_mat(i,j);
+
+                // continue with next column contribution
+              }
+              // continue with next column entry
+            }
+
+#ifdef DEBUG
+            // reformat column-pointer array
+            k = IndexType_(0);
+            while (_row_idx[k] < ix)
+            {
+              ++k;
+            }
+            while (k < _used_elements && _row_idx[k] <= ix)
+            {
+              _col_ptr[_col_idx[k]] = _deadcode;
+              ++k;
+            }
+#endif
+            // continue with next row contribution
+          }
+          // continue with next row entry
+        }
+      }
+    }; // class ScatterAxpy<LAFEM::SparseMatrixCOO<Mem::Main,...>>
+
+    /**
      * \brief Scatter-Axpy specialisation for SparseMatrixELL
      *
      * \author Christoph Lohmann
