a94e2abb18b660dd49a7ad3ca33a2cd41cdc5e68
Christoph Lohmann <christoph.lohmann@math.tu-dortmund.de>
2014-06-17 20:53:32 +0200

    ENH: optimized product_matvec implementation for SparseMatrixBanded
    Signed-off-by: Christoph Lohmann <christoph.lohmann@math.tu-dortmund.de>

--- kernel/lafem/arch/product_matvec_generic.hpp
+++ kernel/lafem/arch/product_matvec_generic.hpp
@@ -95,157 +95,361 @@ void ProductMatVec<Mem::Main, Algo::Generic>::coo(DT_ * r, const DT_ * const val
   }
 }
 
-template <typename DT_, typename IT_>
-void ProductMatVec<Mem::Main, Algo::Generic>::banded(DT_ * r, const DT_ * const val, const IT_ * const offsets, const DT_ * const x, const Index num_of_offsets, const Index rows, const Index columns)
+
+namespace FEAST
 {
-  /*
-  const Index m(offsets[4] - offsets[1]);
-
-  const DT_ * ll(val);
-  const DT_ * ld(val + rows);
-  const DT_ * lu(val + 2 * rows);
-  const DT_ * dl(val + 3 * rows);
-  const DT_ * dd(val + 4 * rows);
-  const DT_ * du(val + 5 * rows);
-  const DT_ * ul(val + 6 * rows);
-  const DT_ * ud(val + 7 * rows);
-  const DT_ * uu(val + 8 * rows);
-
-  Index index(0);
-  r[index] = dd[index] * x[index]
-    + du[index] * x[index + 1]
-    + ul[index] * x[index + m - 1]
-    + ud[index] * x[index + m]
-    + uu[index] * x[index + m + 1];
-
-  index = 1;
-  for (Index si(index) ; si < m - 1 ; ++si)
+  namespace LAFEM
   {
-    r[si] = dd[si] * x[si]
-      + dl[si] * x[si - 1]
-      + du[si] * x[si + 1]
-      + ul[si] * x[si + m - 1]
-      + ud[si] * x[si + m]
-      + uu[si] * x[si + m + 1];
-  }
-
-  index = m - 1;
-  r[index] = dd[index] * x[index]
-    + lu[index] * x[index - m + 1]
-    + dl[index] * x[index - 1]
-    + du[index] * x[index + 1]
-    + ul[index] * x[index + m - 1]
-    + ud[index] * x[index + m]
-    + uu[index] * x[index + m + 1];
-
-    index = m;
-    r[index] = dd[index] * x[index]
-      + lu[index] * x[index - m + 1]
-      + ld[index] * x[index - m]
-      + dl[index] * x[index - 1]
-      + du[index] * x[index + 1]
-      + ul[index] * x[index + m - 1]
-      + ud[index] * x[index + m]
-      + uu[index] * x[index + m + 1];
-
-    index = m + 1;
-    for (Index si(index) ; si < rows - m - 1 ; ++si)
+    namespace Arch
     {
-      r[si] = dd[si] * x[si]
-        + ll[si] * x[si - m - 1]
-        + lu[si] * x[si - m + 1]
-        + ld[si] * x[si - m]
-        + dl[si] * x[si - 1]
-        + du[si] * x[si + 1]
-        + ul[si] * x[si + m - 1]
-        + ud[si] * x[si + m]
-        + uu[si] * x[si + m + 1];
-    }
-
-    index = rows - m - 1;
-    r[index] = dd[index] * x[index]
-      + ll[index] * x[index - m - 1]
-      + lu[index] * x[index - m + 1]
-      + ld[index] * x[index - m]
-      + dl[index] * x[index - 1]
-      + du[index] * x[index + 1]
-      + ul[index] * x[index + m - 1]
-      + ud[index] * x[index + m];
-
-    index = rows - m;
-    r[index] = dd[index] * x[index]
-      + ll[index] * x[index - m - 1]
-      + lu[index] * x[index - m + 1]
-      + ld[index] * x[index - m]
-      + dl[index] * x[index - 1]
-      + du[index] * x[index + 1]
-      + ul[index] * x[index + m - 1];
-
-    index = rows - m + 1;
-    for (Index si(index) ; si < rows - 1 ; ++si)
-    {
-      r[si] = dd[si]  * x[si]
-        + ll[si] * x[si - m - 1]
-        + lu[si] * x[si - m + 1]
-        + ld[si] * x[si - m]
-        + dl[si] * x[si - 1]
-        + du[si] * x[si + 1];
-    }
-
-    index = rows - 1;
-    r[index] = dd[index] * x[index]
-      + ll[index] * x[index - m - 1]
-      + lu[index] * x[index - m + 1]
-      + ld[index] * x[index - m]
-      + dl[index] * x[index - 1];
-    */
-
-#ifdef START_OFFSET
-#warning Overwriting definition of START_OFFSET
-#undef START_OFFSET
-#endif
-
-#ifdef END_OFFSET
-#error Overwriting definition of END_OFFSET
-#undef END_OFFSET
-#endif
-
-#define START_OFFSET(j) ((j == Index(-1)) ? rows : ((j == k) ? 0 : rows - offsets[j] - 1))
-#define END_OFFSET(j) ((j == Index(-1)) ? rows : ((j == num_of_offsets) ? 0 : columns + rows - offsets[j] - 1))
-
-  // Search first offset of the upper triangular matrix
-  Index k(0);
-  while (k < num_of_offsets && offsets[k] + 1 < rows)
-  {
-    ++k;
-  }
-
-  // iteration over all offsets of the lower triangular matrix
-  for (Index i(k + 1); i > 0;)
-  {
-    --i;
-
-    // iteration over all offsets of the upper triangular matrix
-    for (Index j(num_of_offsets + 1); j > 0;)
-    {
-      --j;
-
-      // iteration over all rows which contain the offsets between offset i and offset j
-      const Index start(Math::max(START_OFFSET(i), END_OFFSET(j)));
-      const Index stop(Math::min(START_OFFSET(i-1), END_OFFSET(j-1)));
-      for (Index l(start); l < stop; ++l)
+      namespace Intern
       {
-        DT_ s(0);
-        for (Index a(i); a < j; ++a)
+        namespace ProductMatVecBanded
         {
-          s += val[a * rows + l] * x[l + offsets[a] + 1 - rows];
-        }
-        r[l] = s;
-      }
-    }
+          template <typename DT_, typename IT_, Index i>
+          struct Single_Entry
+          {
+            static inline DT_ f(const DT_ * const val, const IT_ * const offsets,
+                                const DT_ * const x, const Index rows)
+            {
+              return Single_Entry<DT_, IT_, i-9>::f(val + 9*rows, offsets + 9, x , rows) +
+                *val * x[*offsets] +
+                val[rows] * x[offsets[1]] +
+                val[2 * rows] * x[offsets[2]] +
+                val[3 * rows] * x[offsets[3]] +
+                val[4 * rows] * x[offsets[4]] +
+                val[5 * rows] * x[offsets[5]] +
+                val[6 * rows] * x[offsets[6]] +
+                val[7 * rows] * x[offsets[7]] +
+                val[8 * rows] * x[offsets[8]];
+            }
+          };
+
+          template <typename DT_, typename IT_>
+          struct Single_Entry<DT_, IT_, 25>
+          {
+            static inline DT_ f(const DT_ * const val, const IT_ * const offsets,
+                                const DT_ * const x, const Index rows)
+            {
+              return *val * x[*offsets] +
+                val[rows] * x[offsets[1]] +
+                val[ 2 * rows] * x[offsets[ 2]] +
+                val[ 3 * rows] * x[offsets[ 3]] +
+                val[ 4 * rows] * x[offsets[ 4]] +
+                val[ 5 * rows] * x[offsets[ 5]] +
+                val[ 6 * rows] * x[offsets[ 6]] +
+                val[ 7 * rows] * x[offsets[ 7]] +
+                val[ 8 * rows] * x[offsets[ 8]] +
+                val[ 9 * rows] * x[offsets[ 9]] +
+                val[10 * rows] * x[offsets[10]] +
+                val[11 * rows] * x[offsets[11]] +
+                val[12 * rows] * x[offsets[12]] +
+                val[13 * rows] * x[offsets[13]] +
+                val[14 * rows] * x[offsets[14]] +
+                val[15 * rows] * x[offsets[15]] +
+                val[16 * rows] * x[offsets[16]] +
+                val[17 * rows] * x[offsets[17]] +
+                val[18 * rows] * x[offsets[18]] +
+                val[19 * rows] * x[offsets[19]] +
+                val[20 * rows] * x[offsets[20]] +
+                val[21 * rows] * x[offsets[21]] +
+                val[22 * rows] * x[offsets[22]] +
+                val[23 * rows] * x[offsets[23]] +
+                val[24 * rows] * x[offsets[24]];
+            }
+          };
+
+          template <typename DT_, typename IT_>
+          struct Single_Entry<DT_, IT_, 1>
+          {
+            static inline DT_ f(const DT_ * const val, const IT_ * const offsets,
+                                const DT_ * const x, const Index /*rows*/)
+            {
+              return *val * x[*offsets];
+            }
+          };
+
+          template <typename DT_, typename IT_>
+          struct Single_Entry<DT_, IT_, 2>
+          {
+            static inline DT_ f(const DT_ * const val, const IT_ * const offsets,
+                                const DT_ * const x, const Index rows)
+            {
+              return *val * x[*offsets] +
+                val[rows] * x[offsets[1]];
+            }
+          };
+
+          template <typename DT_, typename IT_>
+          struct Single_Entry<DT_, IT_, 3>
+          {
+            static inline DT_ f(const DT_ * const val, const IT_ * const offsets,
+                                const DT_ * const x, const Index rows)
+            {
+              return *val * x[*offsets] +
+                val[rows] * x[offsets[1]] +
+                val[2 * rows] * x[offsets[2]];
+            }
+          };
+
+          template <typename DT_, typename IT_>
+          struct Single_Entry<DT_, IT_, 4>
+          {
+            static inline DT_ f(const DT_ * const val, const IT_ * const offsets,
+                                const DT_ * const x, const Index rows)
+            {
+              return *val * x[*offsets] +
+                val[rows] * x[offsets[1]] +
+                val[2 * rows] * x[offsets[2]] +
+                val[3 * rows] * x[offsets[3]];
+            }
+          };
+
+          template <typename DT_, typename IT_>
+          struct Single_Entry<DT_, IT_, 5>
+          {
+            static inline DT_ f(const DT_ * const val, const IT_ * const offsets,
+                                const DT_ * const x, const Index rows)
+            {
+              return *val * x[*offsets] +
+                val[rows] * x[offsets[1]] +
+                val[2 * rows] * x[offsets[2]] +
+                val[3 * rows] * x[offsets[3]] +
+                val[4 * rows] * x[offsets[4]];
+            }
+          };
+
+          template <typename DT_, typename IT_>
+          struct Single_Entry<DT_, IT_, 6>
+          {
+            static inline DT_ f(const DT_ * const val, const IT_ * const offsets,
+                                const DT_ * const x, const Index rows)
+            {
+              return *val * x[*offsets] +
+                val[rows] * x[offsets[1]] +
+                val[2 * rows] * x[offsets[2]] +
+                val[3 * rows] * x[offsets[3]] +
+                val[4 * rows] * x[offsets[4]] +
+                val[5 * rows] * x[offsets[5]];
+            }
+          };
+
+          template <typename DT_, typename IT_>
+          struct Single_Entry<DT_, IT_, 7>
+          {
+            static inline DT_ f(const DT_ * const val, const IT_ * const offsets,
+                                const DT_ * const x, const Index rows)
+            {
+              return *val * x[*offsets] +
+                val[rows] * x[offsets[1]] +
+                val[2 * rows] * x[offsets[2]] +
+                val[3 * rows] * x[offsets[3]] +
+                val[4 * rows] * x[offsets[4]] +
+                val[5 * rows] * x[offsets[5]] +
+                val[6 * rows] * x[offsets[6]];
+            }
+          };
+
+          template <typename DT_, typename IT_>
+          struct Single_Entry<DT_, IT_, 8>
+          {
+            static inline DT_ f(const DT_ * const val, const IT_ * const offsets,
+                                const DT_ * const x, const Index rows)
+            {
+              return *val * x[*offsets] +
+                val[rows] * x[offsets[1]] +
+                val[2 * rows] * x[offsets[2]] +
+                val[3 * rows] * x[offsets[3]] +
+                val[4 * rows] * x[offsets[4]] +
+                val[5 * rows] * x[offsets[5]] +
+                val[6 * rows] * x[offsets[6]] +
+                val[7 * rows] * x[offsets[7]];
+            }
+          };
+
+          template <typename DT_, typename IT_>
+          struct Single_Entry<DT_, IT_, 9>
+          {
+            static inline DT_ f(const DT_ * const val, const IT_ * const offsets,
+                                const DT_ * const x, const Index rows)
+            {
+              return *val * x[*offsets] +
+                val[rows] * x[offsets[1]] +
+                val[2 * rows] * x[offsets[2]] +
+                val[3 * rows] * x[offsets[3]] +
+                val[4 * rows] * x[offsets[4]] +
+                val[5 * rows] * x[offsets[5]] +
+                val[6 * rows] * x[offsets[6]] +
+                val[7 * rows] * x[offsets[7]] +
+                val[8 * rows] * x[offsets[8]];
+            }
+          };
+
+          /********************************************************************/
+
+          template <typename IT_>
+          inline Index start_offset(const Index i, const IT_ * const offsets,
+                                    const Index rows, const Index columns, const Index noo)
+          {
+            if (i == Index(-1))
+            {
+              return rows;
+            }
+            else if (i == noo)
+            {
+              return Index(0);
+            }
+            else
+            {
+              return std::max(columns + Index(1), rows + columns - offsets[i]) - columns - Index(1);
+            }
+          }
+
+          template <typename IT_>
+          inline Index end_offset(const Index i, const IT_ * const offsets,
+                                  const Index rows, const Index columns, const Index noo)
+          {
+            if (i == Index (-1))
+            {
+              return rows;
+            }
+            else if (i == noo)
+            {
+              return Index(0);
+            }
+            else
+            {
+              return std::min(rows, columns + rows - offsets[i] - Index(1));
+            }
+          }
+
+          /********************************************************************/
+
+          template <typename DT_, typename IT_, Index noo, Index i, Index j>
+          struct Iteration_Left
+          {
+            static void f(DT_ * const r, const DT_ * const val, const IT_ * const offsets,
+                          const DT_ * const x, const Index rows, const Index columns)
+            {
+              Index start(std::max(start_offset(j-1, offsets, rows, columns, noo),
+                                   end_offset(i-1, offsets, rows, columns, noo)));
+              Index end  (std::min(start_offset(j-2, offsets, rows, columns, noo),
+                                   end_offset(i-2, offsets, rows, columns, noo)));
+
+              FEAST_IVDEP
+              for (Index l(start); l < end; ++l)
+              {
+                r[l] = Single_Entry<DT_, IT_, i-j>::f(val + (j-1) * rows + l,
+                                                      offsets + (j-1), x + l + 1 - rows, rows);
+              }
+
+              Iteration_Left<DT_, IT_, noo, i, j-1>::f(r, val, offsets, x, rows, columns);
+            }
+          };
+
+          template <typename DT_, typename IT_, Index noo, Index i>
+          struct Iteration_Left<DT_, IT_, noo, i, 0>
+          {
+            static void f(DT_ * const /*r*/, const DT_ * const /*val*/, const IT_ * const /*offsets*/,
+                          const DT_ * const /*x*/, const Index /*rows*/, const Index /*columns*/)
+            {
+            }
+          };
+
+          /********************************************************************/
+
+          template <typename DT_, typename IT_, Index noo, Index i>
+          struct Iteration_Right
+          {
+            static void f(DT_ * const r, const DT_ * const val, const IT_ * const offsets,
+                          const DT_ * const x, const Index rows, const Index columns)
+            {
+              Iteration_Left<DT_, IT_, noo, i, i-1>::f(r, val, offsets, x, rows, columns);
+              Iteration_Right<DT_, IT_, noo, i-1>::f(r, val, offsets, x, rows, columns);
+            }
+          };
+
+          template <typename DT_, typename IT_, Index noo>
+          struct Iteration_Right<DT_, IT_, noo, 0>
+          {
+            static void f(DT_ * const /*r*/, const DT_ * const /*val*/, const IT_ * const /*offsets*/,
+                          const DT_ * const /*x*/, const Index /*rows*/, const Index /*columns*/)
+            {
+            }
+          };
+
+          /********************************************************************/
+
+          template <typename DT_, typename IT_>
+          void product_matvec_banded_generic(DT_ * r, const DT_ * const val,
+                                             const IT_ * const offsets, const DT_ * const x,
+                                             const Index num_of_offsets, const Index rows, const Index columns)
+          {
+            // Search first offset of the upper triangular matrix
+            Index k(0);
+            while (k < num_of_offsets && offsets[k] + 1 < rows)
+            {
+              ++k;
+            }
+
+            // iteration over all offsets of the lower triangular matrix
+            for (Index i(k + 1); i > 0;)
+            {
+              --i;
+
+              // iteration over all offsets of the upper triangular matrix
+              for (Index j(num_of_offsets + 1); j > 0;)
+              {
+                --j;
+
+                // iteration over all rows which contain the offsets between offset i and offset j
+                const Index start(Math::max(start_offset(  i, offsets, rows, columns, num_of_offsets),
+                                            end_offset  (  j, offsets, rows, columns, num_of_offsets)));
+                const Index stop (Math::min(start_offset(i-1, offsets, rows, columns, num_of_offsets),
+                                            end_offset  (j-1, offsets, rows, columns, num_of_offsets)));
+                for (Index l(start); l < stop; ++l)
+                {
+                  DT_ s(0);
+                  for (Index a(i); a < j; ++a)
+                  {
+                    s += val[a * rows + l] * x[l + offsets[a] + 1 - rows];
+                  }
+                  r[l] = s;
+                }
+              }
+            }
+          }
+        } // namespace ProductMatVecBanded
+      } // namespace Intern
+    } // namespace Arch
+  } // namespace LAFEM
+} // namespace FEAST
+
+template <typename DT_, typename IT_>
+void ProductMatVec<Mem::Main, Algo::Generic>::banded(DT_ * r, const DT_ * const val,
+                                                     const IT_ * const offsets, const DT_ * const x,
+                                                     const Index num_of_offsets, const Index rows, const Index columns)
+{
+  switch (num_of_offsets)
+  {
+  case 3:
+    Intern::ProductMatVecBanded::Iteration_Right<DT_, IT_, 3, 4>::f(r, val, offsets, x, rows, columns);
+    break;
+  case 5:
+    Intern::ProductMatVecBanded::Iteration_Right<DT_, IT_, 5, 6>::f(r, val, offsets, x, rows, columns);
+    break;
+  case 9:
+    Intern::ProductMatVecBanded::Iteration_Right<DT_, IT_, 9,10>::f(r, val, offsets, x, rows, columns);
+    break;
+  case 25:
+    Intern::ProductMatVecBanded::Iteration_Right<DT_, IT_,25,26>::f(r, val, offsets, x, rows, columns);
+    break;
+  default:
+#if DEBUG
+    std::cout << "ProductMatVec not optimized for " << num_of_offsets << " offsets!" << std::endl;
+#endif
+    Intern::ProductMatVecBanded::product_matvec_banded_generic(r, val, offsets, x, num_of_offsets, rows, columns);
   }
-#undef START_OFFSET
-#undef END_OFFSET
 }
 
 #endif // KERNEL_LAFEM_ARCH_PRODUCT_MATVEC_GENERIC_HPP
