c82cb68146b352d94781d56e51376bbbb170e2bc
Jordi Paul <jordi.paul@math.tu-dortmund.de>
2014-06-23 13:08:18 +0200

    ENH: Added FEAST_DISABLE_WARNINGS for clang
    Signed-off-by: Jordi Paul <jordi.paul@math.tu-dortmund.de>

--- kernel/util/compiler_clang.hpp
+++ kernel/util/compiler_clang.hpp
@@ -3,7 +3,7 @@
 #define KERNEL_UTIL_COMPILER_CLANG_HPP 1
 
 /**
- * \file compiler_gnu.hpp
+ * \file compiler_clang.hpp
  *
  * \brief Compiler detection header for Clang C++ compiler.
  *
@@ -13,11 +13,17 @@
 #if !defined(FEAST_COMPILER) && defined(__clang__)
 
 // calc linear sortable clang version
-#  define _CLANG_VER (__clang_major__ * 10000 + __clang_minor__ * 100 + __clang_patchlevel__)
+#define _CLANG_VER (__clang_major__ * 10000 + __clang_minor__ * 100 + __clang_patchlevel__)
 
-#  define FEAST_COMPILER_CLANG _CLANG_VER
+#define FEAST_COMPILER_CLANG _CLANG_VER
 
-#  define FEAST_COMPILER "Clang Compiler" // __clang_version__ contains details
+#define FEAST_COMPILER "Clang Compiler" // __clang_version__ contains details
+
+#define FEAST_DISABLE_WARNINGS _Pragma("clang diagnostic push") \
+  _Pragma("clang diagnostic ignored \"-Wunused-variable\"") \
+  _Pragma("clang diagnostic ignored \"-Wignored-qualifiers\"")
+
+#define FEAST_RESTORE_WARNINGS _Pragma("clang diagnostic pop")
 
 // define the noinline specifier
 #define NOINLINE __attribute__((noinline))
