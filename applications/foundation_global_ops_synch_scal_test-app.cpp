80b9e39f81b966886ae4f2be1432225d131206fd
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2014-08-20 14:44:13 +0200

    ENH: added global synch scal operation
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

---
+++ applications/foundation_global_ops_synch_scal_test-app.cpp
@@ -0,0 +1,79 @@
+#include <kernel/base_header.hpp>
+#include <kernel/foundation/comm_base.hpp>
+#include <kernel/foundation/global_synch_scal.hpp>
+#include <kernel/archs.hpp>
+
+#include <iostream>
+#include <limits>
+
+using namespace FEAST;
+using namespace Foundation;
+
+template<typename DT1_, typename DT2_, typename DT3_>
+struct TestResult
+{
+  TestResult(DT1_ l, DT2_ r, DT3_ eps) :
+    left(l),
+    right(r),
+    epsilon(eps)
+  {
+    //passed = std::abs(l - r) < eps ? true : false;
+    passed = (l < r ? r - l : l - r) < eps ? true : false;
+  }
+
+  TestResult()
+  {
+  }
+
+  DT1_ left;
+  DT2_ right;
+  DT3_ epsilon;
+  bool passed;
+};
+
+template<typename DT1_, typename DT2_, typename DT3_>
+TestResult<DT1_, DT2_, DT3_> test_check_equal_within_eps(DT1_ l, DT2_ r, DT3_ eps)
+{
+  return TestResult<DT1_, DT2_, DT3_>(l, r, eps);
+}
+
+template<typename DT_>
+void check_global_synch_scal0(Index rank)
+{
+  auto nprocs(Comm::size());
+
+  DT_ x(2), r;
+
+  GlobalSynchScal0<Mem::Main, Algo::Generic>::value(r, x);
+
+  TestResult<double, double, double> res;
+  res = test_check_equal_within_eps(r, x * nprocs, std::numeric_limits<DT_>::epsilon());
+  if(res.passed)
+    std::cout << "PASSED (rank " << rank <<"): foundation_global_ops_synch_scal0" << std::endl;
+  else
+    std::cout << "FAILED: " << res.left << " not within range (eps = " << res.epsilon << ") of " << res.right << "!" << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+  int me(0);
+#ifndef SERIAL
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &me);
+#endif
+  (void)argc;
+  (void)argv;
+  std::cout<<"CTEST_FULL_OUTPUT"<<std::endl;
+
+//#ifndef SERIAL
+  check_global_synch_scal0<double>((Index)me);
+//#else
+//  std::cout << "Parallel tests unavailable on sole process " << me << std::endl;
+//#endif
+
+#ifndef SERIAL
+  MPI_Finalize();
+#endif
+
+  return 0;
+}
---
+++ kernel/foundation/global_synch_scal.hpp
@@ -0,0 +1,66 @@
+#pragma once
+#ifndef FOUNDATION_GUARD_GLOBAL_SYNCH_SCAL_HPP
+#define FOUNDATION_GUARD_GLOBAL_SYNCH_SCAL_HPP 1
+
+#include<kernel/foundation/comm_base.hpp>
+#include<kernel/foundation/communication.hpp>
+#include<kernel/lafem/arch/scale.hpp>
+#include<kernel/lafem/arch/component_product.hpp>
+#include<kernel/lafem/arch/dot_product.hpp>
+
+using namespace FEAST;
+using namespace FEAST::LAFEM;
+using namespace FEAST::LAFEM::Arch;
+
+///TODO add communicators
+namespace FEAST
+{
+  namespace Foundation
+  {
+      template <typename Mem_, typename Algo_>
+      struct GlobalSynchScal0
+      {
+      };
+
+      template <>
+      struct GlobalSynchScal0<Mem::Main, Algo::Generic>
+      {
+        public:
+#ifndef SERIAL
+          template<typename DT_>
+          static DT_ value(DT_& r,
+                           DT_& x)
+          {
+            DT_ sendbuf(x), recvbuf;
+
+            Status stat;
+
+// The current MS-MPI implementation does not offer the MPI_Iallreduce function...
+/// \todo Remove this workaround once MS does its homework.
+#ifdef MSMPI_VER
+            Comm::allreduce(&sendbuf, Index(1), &recvbuf);
+#else
+            Request req;
+
+            Comm::iallreduce(&sendbuf, Index(1), &recvbuf, req);
+
+            Comm::wait(req, stat);
+#endif // MSMPI_VER
+            r = recvbuf;
+            return r;
+          }
+#else
+          template<typename DT_>
+          static DT_ value(DT_& r,
+                           DT_& x)
+          {
+            r = x;
+            return r;
+          }
+#endif
+      };
+  }
+}
+
+
+#endif
