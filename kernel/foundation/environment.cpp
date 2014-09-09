1fd401a5c56206a45987350cf88190c2b32111ec
Markus Geveler <markus.geveler@math.tu-dortmund.de>
2014-09-09 12:27:32 +0200

    ENH: moved static var inits to cpp file
    Signed-off-by: Markus Geveler <markus.geveler@math.tu-dortmund.de>

---
+++ kernel/foundation/environment.cpp
@@ -0,0 +1,12 @@
+#include <kernel/foundation/environment.hpp>
+#include <string>
+
+using namespace FEAST;
+using namespace Foundation;
+
+#ifndef SERIAL
+    const Index Environment::MIN_TAG_UB = Index(32767); //given by MPI 3 standard
+#else
+    const Index Environment::MIN_TAG_UB = Index(0); //no op should use tags
+#endif
+    Index Environment::tags_reserved_low = Index(0);
--- kernel/foundation/environment.hpp
+++ kernel/foundation/environment.hpp
@@ -40,12 +40,6 @@ namespace FEAST
         static Index tags_reserved_low;
     };
 
-#ifndef SERIAL
-    const Index Environment::MIN_TAG_UB = Index(32767); //given by MPI 3 standard
-#else
-    const Index Environment::MIN_TAG_UB = Index(0); //no op should use tags
-#endif
-    Index Environment::tags_reserved_low = Index(0);
   }
 }
 
