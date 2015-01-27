192795616c8d85e71b0f7df0a8eb34a2ce9404e6
Dirk Ribbrock <dirk.ribbrock@math.tu-dortmund.de>
2015-01-27 14:49:18 +0100

    ENH: noscarc, just data parallel operations
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@math.tu-dortmund.de>

---
+++ applications/noscarc/noscarc.cpp
@@ -0,0 +1,14 @@
+#include <kernel/base_header.hpp>
+
+#include <mpi.h>
+
+
+int main(int argc, char* argv[])
+{
+  int rank(0), nprocs(1);
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+  MPI_Finalize();
+  return 0;
+}
