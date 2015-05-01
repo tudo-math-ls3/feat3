6626759035f729c39e4e24ba1a977df86d40b6d1
Peter Zajac <pzajac@math.tu-dortmund.de>
2015-05-01 14:26:36 +0200

    enhanced TimeStamp and StopWatch classes
    The TimeStamp class' functionality of writing out the elapsed time as a
    formatted string has been outsourced into a static function named
    'format_micros()'. Moreover, this new function and the elapsed_string
    function have additional formatting parameters which specify whether hours
    and milliseconds are to be printed.
    
    The StopWatch class has been enhanced in two ways:
    First, now keeps track of whether it is currently running or not. If the
    stop watch is running, the 'elapsed_*' functions returns the current
    running time. If the stop watch is not running, the 'elapsed_*' functions
    return the time that elapsed while the watch was running.
    In other words, the StopWatch class now behaves like a real-life
    stop-watch.
    
    Furthermore, the StopWatch class also supports a 'elapsed_string()'
    function, which returns the elapsed time as a formatted string in analogy
    to the TimeStamp class.
    
    Signed-off-by: Peter Zajac <pzajac@math.tu-dortmund.de>

--- kernel/util/stop_watch.hpp
+++ kernel/util/stop_watch.hpp
@@ -19,10 +19,12 @@ namespace FEAST
   private:
     TimeStamp _start_time;
     TimeStamp _stop_time;
+    bool _running;
     long long _micros;
 
   public:
     StopWatch() :
+      _running(false),
       _micros(0ll)
     {
     }
@@ -30,34 +32,72 @@ namespace FEAST
     /// Resets the elapsed time.
     void reset()
     {
+      _running = false;
       _micros = 0ll;
     }
 
     /// Starts the stop-watch.
     void start()
     {
-      _start_time.stamp();
+      if(!_running)
+        _start_time.stamp();
+      _running = true;
     }
 
     /// Stops the stop-watch and increments elapsed time.
     void stop()
     {
-      _stop_time.stamp();
+      if(_running)
+      {
+        _stop_time.stamp();
 
-      // update elapsed time
-      _micros += _stop_time.elapsed_micros(_start_time);
+        // update elapsed time
+        _micros += _stop_time.elapsed_micros(_start_time);
+      }
+      _running = false;
+    }
+
+    /// Returns \c true if the stop-watch is currently running.
+    bool running() const
+    {
+      return _running;
     }
 
     /// Returns the total elapsed time in seconds.
     double elapsed() const
     {
-      return 1E-6 * double(_micros);
+      return 1E-6 * double(elapsed_micros());
     }
 
     /// Returns the total elapsed time in micro-seconds.
     long long elapsed_micros() const
     {
-      return _micros;
+      if(!_running)
+        return _micros;
+      else
+      {
+        // stop-watch is currently running, so compute current time
+        return _micros + TimeStamp().elapsed_micros(_start_time);
+      }
+    }
+
+    /**
+     * \brief Return the time elapsed in the stop-watch.
+     *
+     * See TimeStamp::format_micros() for more information about the formatting options.
+     *
+     * \param[in] bhours
+     * Specifies whether hours are to be printed.
+     *
+     * \param[in] bmillis
+     * Specifies whether milliseconds are to be printed.
+     *
+     * \returns
+     * The time elapsed between in the stop-watch as a formatted string <code>h:mm:ss.nnn</code>.
+     */
+    String elapsed_string(bool bhours = true, bool bmillis = true) const
+    {
+      return TimeStamp::format_micros(elapsed_micros(), bhours, bmillis);
     }
   }; // class StopWatch
 } // namespace FEAST
--- kernel/util/time_stamp.hpp
+++ kernel/util/time_stamp.hpp
@@ -119,32 +119,96 @@ namespace FEAST
     }
 
     /**
+     * \brief Formats an elapsed time in microseconds as a string.
+     *
+     * This function formats the given elapsed time as a string, where the format depends
+     * on the \p bhours and \p bmillis parameters:
+     * - If \p bhours = \c true, then the least granular unit is an hour and the string is
+     *   of the format <c>h:mm:ss[.nnn]</c>.
+     * - If \p bhours = \c false, then the least granular unit is a minute and the string
+     *   is of the format <c>m:ss[.nnn]</c>.
+     * - If \p bmillis = \c true, then the most granular unit is a millisecond and the string is
+     *   of the format <c>[h:m]m:ss.nnn</c>.
+     * - If \p bmillis = \c false, then the most granular unit is a second and the string is
+     *   of the format <c>[h:m]m:ss</c>.
+     *
+     * \param[in] micros
+     * The elapsed time to be formatted.
+     *
+     * \param[in] bhours
+     * Specifies whether hours are to be printed.
+     *
+     * \param[in] bmillis
+     * Specifies whether milliseconds are to be printed.
+     *
+     * \returns
+     * The time elapsed as a formatted string <code>[h:m]m:ss[.nnn]</code>.
+     */
+    static String format_micros(long long micros, bool bhours = true, bool bmillis = true)
+    {
+      // check whether the time is negative
+      bool neg(false);
+      if(micros < 0)
+      {
+        neg = true;
+        micros = -micros;
+      }
+
+      std::ostringstream oss;
+      oss << std::setfill('0');
+      if(bhours)
+      {
+        // write hours
+        oss << (micros / 3600000000ll);
+        // write minutes
+        oss << ":" << std::setw(2) << ((micros / 60000000ll) % 60ll);
+      }
+      else
+      {
+        // write minutes
+        oss << ":" << (micros / 60000000ll);
+      }
+      if(bmillis)
+      {
+        // write seconds
+        oss << ":" << std::setw(2) << (((micros + 500) / 1000000ll) % 60ll);
+        // write milli-seconds by rounding
+        oss << "." << std::setw(3) << (((micros + 500) / 1000ll) % 1000ll);
+      }
+      else
+      {
+        // write seconds by rounding
+        oss << ":" << std::setw(2) << (((micros + 500000) / 1000000ll) % 60ll);
+      }
+
+      // return formatted string
+      String str(oss.str());
+      if(neg)
+        str.push_front('-');
+      return str;
+    }
+
+    /**
      * \brief Return the time elapsed between two time stamps as a string.
+     *
+     * See TimeStamp::format_micros() for more information about the formatting options.
      *
      * \param[in] before
      * A time stamp that represents a previous moment.
+     *
+     * \param[in] bhours
+     * Specifies whether hours are to be printed.
+     *
+     * \param[in] bmillis
+     * Specifies whether milliseconds are to be printed.
      *
      * \returns
      * The time elapsed between the time stamps \p before and \c this as a formatted string
      * <code>h:mm:ss.nnn</code>.
      */
-    String elapsed_string(const TimeStamp& before) const
+    String elapsed_string(const TimeStamp& before, bool bhours = true, bool bmillis = true) const
     {
-      if(*this < before)
-        return String("-").append(before.elapsed_string(*this));
-
-      long long micros(elapsed_micros(before));
-      std::ostringstream oss;
-      oss << std::setfill('0');
-      // write hours
-      oss << (micros / 3600000000ll);
-      // write minutes
-      oss << ":" << std::setw(2) << ((micros /   60000000ll) %   60ll);
-      // write seconds
-      oss << ":" << std::setw(2) << ((micros /    1000000ll) %   60ll);
-      // write milli-seconds
-      oss << "." << std::setw(3) << ((micros /       1000ll) % 1000ll);
-      return oss.str();
+      return format_micros(elapsed_micros(before), bhours, bmillis);
     }
 
     /**
