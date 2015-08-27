40896cf65c9aa55564623ad4d77d20b477aa5da7
Peter Zajac <pzajac@math.tu-dortmund.de>
2015-08-27 10:40:32 +0200

    fixed Index vs int
    Signed-off-by: Peter Zajac <pzajac@math.tu-dortmund.de>

--- kernel/geometry/index_set.hpp
+++ kernel/geometry/index_set.hpp
@@ -251,7 +251,7 @@ namespace FEAST
         CONTEXT(name() + "::operator()()");
         ASSERT_(_indices != nullptr);
         ASSERT_(i < _num_entities);
-        ASSERT_(j < num_indices);
+        ASSERT_(j < Index(num_indices));
         return _indices[i][j];
       }
 
