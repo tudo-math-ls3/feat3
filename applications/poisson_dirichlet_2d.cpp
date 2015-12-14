bf86a3c6b264c509e072a4b275292d6d466970fd
Dirk Ribbrock <dirk.ribbrock@math.tu-dortmund.de>
2015-12-14 15:15:40 +0100

    User defined solver verbosity
    '--statistics' only yields a coarse overview
    '--statistics full' lists detailed defect/timing informations
    
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@math.tu-dortmund.de>

--- applications/poisson_dirichlet_2d.cpp
+++ applications/poisson_dirichlet_2d.cpp
@@ -375,13 +375,15 @@ namespace PoissonDirichlet2D
 
     if (rank == 0 && args.check("statistics") >= 0)
     {
-      String flops = Statistics::get_formated_flops(bt.elapsed(at), nprocs);
       std::cout<<std::endl<<solver->get_formated_solver_tree().trim()<<std::endl;
       String flops = Statistics::get_formated_flops(bt.elapsed(at), nprocs);
       std::cout<<"\nComplete solver TOE: "<<bt.elapsed(at)<<std::endl;
-      std::cout<<flops<<std::endl;
-      std::cout<<Statistics::get_formated_times(bt.elapsed(at))<<std::endl;
-      std::cout<<Statistics::get_formated_solvers()<<std::endl;
+      std::cout<<flops<<std::endl<<std::endl;
+      std::cout<<Statistics::get_formated_times(bt.elapsed(at))<<std::endl<<std::endl;
+      if (args.check("statistics") > 0) // provided parameter full or whatever
+      {
+        std::cout<<Statistics::get_formated_solvers();
+      }
     }
 
     // release solver
