1f7611198ef49f0f006d62c8ed90b0732e45d987
Jordi Paul <jordi.paul@math.tu-dortmund.de>
2016-01-27 16:41:59 +0100

    ENH: Added VectorMirrorBlocked
    Signed-off-by: Jordi Paul <jordi.paul@math.tu-dortmund.de>

--- kernel/lafem/vector_mirror-test.cpp
+++ kernel/lafem/vector_mirror-test.cpp
@@ -95,7 +95,7 @@ public:
     VectorType vec_buf_ab(Index(1), DT_(0));
     VectorType vec_buf_ac(Index(1), DT_(0));
     VectorType vec_buf_bc(Index(1), DT_(0));
-    VectorType vec_buf_abc(Index(1), DT_(0));
+    auto vec_buf_abc(mirror0.create_buffer_vector());
     VectorType vec_buf_tmp(Index(1), DT_(0));
 
     // gather from a
@@ -202,7 +202,7 @@ public:
     MirrorType mirror2(std::move(mat_gather2), std::move(mat_scatter2));
 
     // create four buffer and one temporary vectors
-    VectorType vec_buf_ab(Index(1), DT_(0));
+    auto vec_buf_ab(mirror0.create_buffer_vector());
     VectorType vec_buf_ac(Index(1), DT_(0));
     VectorType vec_buf_bc(Index(1), DT_(0));
     VectorType vec_buf_abc(Index(1), DT_(0));
@@ -249,3 +249,282 @@ public:
 };
 
 VectorMirrorTest<Mem::Main, double, Index> vector_mirror_test_generic_d;
+
+/**
+ * \brief Test class for VectorMirrorBlocked class template
+ *
+ * \author Jordi Paul
+ */
+template < typename Mem_, typename DT_, typename IT_, int BS_>
+class VectorMirrorBlockedTest
+  : public FullTaggedTest<Mem_, DT_, IT_>
+{
+  typedef DenseVectorBlocked<Mem_, DT_, IT_, BS_> VectorType;
+  typedef DenseVector<Mem_, IT_, IT_> IVectorType;
+  typedef SparseMatrixCSR<Mem_, DT_, IT_> MatrixType;
+  typedef DenseVector<Mem_, DT_, IT_> BufferVectorType;
+  typedef VectorMirrorBlocked<Mem_, DT_, IT_, BS_> MirrorType;
+
+  static constexpr int BlockSize = BS_;
+
+public:
+  VectorMirrorBlockedTest()
+    : FullTaggedTest<Mem_, DT_, IT_>("VectorMirrorBlockedTest")
+  {
+  }
+
+  virtual void run() const override
+  {
+    test_1(); // tests gather_prim/scatter_prim
+    test_2(); // tests gather_axpy_prim/scatter_axpy_prim
+  }
+
+  void test_1() const
+  {
+    const DT_ tol = Math::pow(Math::eps<DT_>(), DT_(0.9));
+
+    VectorType a1(Index(3), DT_(0));
+    VectorType a2(Index(3), DT_(0));
+    VectorType b1(Index(3), DT_(0));
+    VectorType b2(Index(3), DT_(0));
+    VectorType c1(Index(3), DT_(0));
+    VectorType c2(Index(3), DT_(0));
+
+    Tiny::Vector<DT_, BS_> tiny_tmp(DT_(0));
+
+    // Vectors to gather/scatter
+    tiny_tmp(0) = DT_(-3), tiny_tmp(1) = DT_(0.5), tiny_tmp(BlockSize-1) = -DT_(1);
+    a1(Index(0), tiny_tmp);
+    tiny_tmp(0) = DT_(-7), tiny_tmp(1) = DT_(5), tiny_tmp(BlockSize-1) = -DT_(1000);
+    a1(Index(1), tiny_tmp);
+    tiny_tmp(0) = DT_(-0.3), tiny_tmp(1) = DT_(0.1), tiny_tmp(BlockSize-1) = DT_(2);
+    a1(Index(2), tiny_tmp);
+
+    tiny_tmp(0) = DT_(0.3), tiny_tmp(1) = DT_(0.001), tiny_tmp(BlockSize-1) = DT_(2.2);
+    b1(Index(0), tiny_tmp);
+    tiny_tmp(0) = DT_(-3.7), tiny_tmp(1) = DT_(8.001), tiny_tmp(BlockSize-1) = -DT_(9.002);
+    b1(Index(1), tiny_tmp);
+    tiny_tmp(0) = DT_(19), tiny_tmp(1) = DT_(-111), tiny_tmp(BlockSize-1) = -DT_(111);
+    b1(Index(2), tiny_tmp);
+
+    tiny_tmp(0) = DT_(4711), tiny_tmp(1) = DT_(0), tiny_tmp(BlockSize-1) = DT_(0);
+    c1(Index(0), tiny_tmp);
+    tiny_tmp(0) = DT_(0), tiny_tmp(1) = DT_(1), tiny_tmp(BlockSize-1) = DT_(7);
+    c1(Index(1), tiny_tmp);
+    tiny_tmp(0) = DT_(0.001), tiny_tmp(1) = DT_(1), tiny_tmp(BlockSize-1) = -DT_(7.7);
+    c1(Index(2), tiny_tmp);
+
+    // Supposed results. See the gather operations below for this to make sense.
+    a2(Index(0), a1(0)+b1(0)+c1(0));
+    a2(Index(1), a1(1)+c1(2));
+    a2(Index(2), a1(2)+b1(1));
+
+    b2(Index(0), a1(0)+b1(0)+c1(0));
+    b2(Index(1), a1(2)+b1(1));
+    b2(Index(2), b1(2)+c1(1));
+
+    c2(Index(0), a1(0)+b1(0)+c1(0));
+    c2(Index(1), b1(2)+c1(1));
+    c2(Index(2), a1(1)+c1(2));
+
+    // Create mirror vectors. Every mirror gathers one entry from / scatters one entry to a DenseVectorBlocked of
+    // length 3
+    IVectorType col_idx0(Index(1), IT_(0));
+    IVectorType col_idx1(Index(1), IT_(1));
+    IVectorType col_idx2(Index(1), IT_(2));
+    BufferVectorType mir_val(Index(1), DT_(1));
+    IVectorType row_ptr(Index(2));
+    row_ptr(Index(0), IT_(0));
+    row_ptr(Index(1), IT_(1));
+    MatrixType mat_gather0(Index(1), Index(3), col_idx0, mir_val, row_ptr);
+    MatrixType mat_gather1(Index(1), Index(3), col_idx1, mir_val, row_ptr);
+    MatrixType mat_gather2(Index(1), Index(3), col_idx2, mir_val, row_ptr);
+    MatrixType mat_scatter0(mat_gather0.transpose());
+    MatrixType mat_scatter1(mat_gather1.transpose());
+    MatrixType mat_scatter2(mat_gather2.transpose());
+
+    // Create mirrors. mirror{k} gathers/scatters to/from entry k
+    MirrorType mirror0(std::move(mat_gather0), std::move(mat_scatter0));
+    MirrorType mirror1(std::move(mat_gather1), std::move(mat_scatter1));
+    MirrorType mirror2(std::move(mat_gather2), std::move(mat_scatter2));
+
+    // Create four buffer and one temporary vectors
+    // a1(2)+b1(1)
+    BufferVectorType vec_buf_ab(Index(BlockSize)*Index(1), DT_(0));
+    // a1(1)+c1(2)
+    BufferVectorType vec_buf_ac(Index(BlockSize)*Index(1), DT_(0));
+    // b1(2)+c(1)
+    BufferVectorType vec_buf_bc(Index(BlockSize)*Index(1), DT_(0));
+    // a1(0) + b1(0) + c1(0)
+    auto vec_buf_abc(mirror0.create_buffer_vector());
+    // Generic buffer for adding
+    BufferVectorType vec_buf_tmp(Index(BlockSize)*Index(1), DT_(0));
+
+    // Gather from a
+    mirror0.gather_prim(vec_buf_tmp, a1);
+    vec_buf_abc.axpy(vec_buf_tmp, vec_buf_abc);
+    mirror1.gather_prim(vec_buf_tmp, a1);
+    vec_buf_ac.axpy(vec_buf_tmp, vec_buf_ac);
+    mirror2.gather_prim(vec_buf_tmp, a1);
+    vec_buf_ab.axpy(vec_buf_tmp, vec_buf_ab);
+
+    // Gather from b
+    mirror0.gather_prim(vec_buf_tmp, b1);
+    vec_buf_abc.axpy(vec_buf_tmp, vec_buf_abc);
+    mirror1.gather_prim(vec_buf_tmp, b1);
+    vec_buf_ab.axpy(vec_buf_tmp, vec_buf_ab);
+    mirror2.gather_prim(vec_buf_tmp, b1);
+    vec_buf_bc.axpy(vec_buf_tmp, vec_buf_bc);
+
+    // Gather from c
+    mirror0.gather_prim(vec_buf_tmp, c1);
+    vec_buf_abc.axpy(vec_buf_tmp, vec_buf_abc);
+    mirror1.gather_prim(vec_buf_tmp, c1);
+    vec_buf_bc.axpy(vec_buf_tmp, vec_buf_bc);
+    mirror2.gather_prim(vec_buf_tmp, c1);
+    vec_buf_ac.axpy(vec_buf_tmp, vec_buf_ac);
+
+    // Now we no longer need a1, b1, c1 and overwrite them by scattering the buffers back into them
+    mirror0.scatter_prim(a1, vec_buf_abc);
+    mirror1.scatter_prim(a1, vec_buf_ac);
+    mirror2.scatter_prim(a1, vec_buf_ab);
+
+    mirror0.scatter_prim(b1, vec_buf_abc);
+    mirror1.scatter_prim(b1, vec_buf_ab);
+    mirror2.scatter_prim(b1, vec_buf_bc);
+
+    mirror0.scatter_prim(c1, vec_buf_abc);
+    mirror1.scatter_prim(c1, vec_buf_bc);
+    mirror2.scatter_prim(c1, vec_buf_ac);
+
+    // Subtract reference results
+    a1.axpy(a2, a1, -DT_(1));
+    b1.axpy(b2, b1, -DT_(1));
+    c1.axpy(c2, c1, -DT_(1));
+
+    // Check norms
+    TEST_CHECK_EQUAL_WITHIN_EPS(a1.norm2(), DT_(0), tol);
+    TEST_CHECK_EQUAL_WITHIN_EPS(b1.norm2(), DT_(0), tol);
+    TEST_CHECK_EQUAL_WITHIN_EPS(c1.norm2(), DT_(0), tol);
+  }
+
+  void test_2() const
+  {
+    const DT_ tol = Math::pow(Math::eps<DT_>(), DT_(0.9));
+
+    VectorType a1(Index(3), DT_(0));
+    VectorType a2(Index(3), DT_(0));
+    VectorType b1(Index(3), DT_(0));
+    VectorType b2(Index(3), DT_(0));
+    VectorType c1(Index(3), DT_(0));
+    VectorType c2(Index(3), DT_(0));
+
+    Tiny::Vector<DT_, BS_> tiny_tmp(DT_(0));
+
+    // Vectors to gather/scatter
+    tiny_tmp(0) = DT_(-0.3), tiny_tmp(1) = DT_(0.1), tiny_tmp(BlockSize-1) = DT_(1.999);
+    a1(Index(0), tiny_tmp);
+    tiny_tmp(0) = DT_(3.01), tiny_tmp(1) = -DT_(0.5), tiny_tmp(BlockSize-1) = -DT_(219);
+    a1(Index(1), tiny_tmp);
+    tiny_tmp(0) = DT_(7), tiny_tmp(1) = -DT_(5), tiny_tmp(BlockSize-1) = -DT_(1000);
+    a1(Index(2), tiny_tmp);
+
+    tiny_tmp(0) = DT_(0.3), tiny_tmp(1) = DT_(1.00), tiny_tmp(BlockSize-1) = -DT_(2.9);
+    b1(Index(0), tiny_tmp);
+    tiny_tmp(0) = -DT_(9), tiny_tmp(1) = DT_(11), tiny_tmp(BlockSize-1) = -DT_(1011);
+    b1(Index(1), tiny_tmp);
+    tiny_tmp(0) = DT_(-302.7), tiny_tmp(1) = DT_(8.1), tiny_tmp(BlockSize-1) = DT_(2.009);
+    b1(Index(2), tiny_tmp);
+
+    tiny_tmp(0) = -DT_(0.1), tiny_tmp(1) = DT_(1), tiny_tmp(BlockSize-1) = -DT_(8.7);
+    c1(Index(0), tiny_tmp);
+    tiny_tmp(0) = DT_(0), tiny_tmp(1) = DT_(1), tiny_tmp(BlockSize-1) = DT_(79);
+    c1(Index(1), tiny_tmp);
+    tiny_tmp(0) = DT_(4711), tiny_tmp(1) = DT_(9.6), tiny_tmp(BlockSize-1) = DT_(0);
+    c1(Index(2), tiny_tmp);
+
+    // Supposed results. See the gather operations below for this to make sense.
+    a2(Index(0), a1(0)+b1(0)+c1(0));
+    a2(Index(1), a1(1)+c1(2));
+    a2(Index(2), a1(2)+b1(1));
+
+    b2(Index(0), a1(0)+b1(0)+c1(0));
+    b2(Index(1), a1(2)+b1(1));
+    b2(Index(2), b1(2)+c1(1));
+
+    c2(Index(0), a1(0)+b1(0)+c1(0));
+    c2(Index(1), b1(2)+c1(1));
+    c2(Index(2), a1(1)+c1(2));
+
+    // Create mirror vectors. Every mirror gathers one entry from / scatters one entry to a DenseVectorBlocked of
+    // length 3
+    IVectorType col_idx0(Index(1), IT_(0));
+    IVectorType col_idx1(Index(1), IT_(1));
+    IVectorType col_idx2(Index(1), IT_(2));
+    BufferVectorType mir_val(Index(1), DT_(1));
+    IVectorType row_ptr(Index(2));
+    row_ptr(Index(0), IT_(0));
+    row_ptr(Index(1), IT_(1));
+    MatrixType mat_gather0(Index(1), Index(3), col_idx0, mir_val, row_ptr);
+    MatrixType mat_gather1(Index(1), Index(3), col_idx1, mir_val, row_ptr);
+    MatrixType mat_gather2(Index(1), Index(3), col_idx2, mir_val, row_ptr);
+    MatrixType mat_scatter0(mat_gather0.transpose());
+    MatrixType mat_scatter1(mat_gather1.transpose());
+    MatrixType mat_scatter2(mat_gather2.transpose());
+
+    // Create mirrors. mirror{k} gathers/scatters to/from entry k
+    MirrorType mirror0(std::move(mat_gather0), std::move(mat_scatter0));
+    MirrorType mirror1(std::move(mat_gather1), std::move(mat_scatter1));
+    MirrorType mirror2(std::move(mat_gather2), std::move(mat_scatter2));
+
+    // Create four buffer and one temporary vectors
+    // a1(2)+b1(1)
+    BufferVectorType vec_buf_ab(Index(BlockSize)*Index(1), DT_(0));
+    // a1(1)+c1(2)
+    auto vec_buf_ac(mirror1.create_buffer_vector());
+    // b1(2)+c(1)
+    BufferVectorType vec_buf_bc(Index(BlockSize)*Index(1), DT_(0));
+    // a1(0) + b1(0) + c1(0)
+    BufferVectorType vec_buf_abc(Index(BlockSize)*Index(1), DT_(0));
+    BufferVectorType vec_buf_tmp(Index(BlockSize)*Index(1), DT_(0));
+
+    // Gather from a
+    mirror0.gather_axpy_prim(vec_buf_abc, a1);
+    mirror1.gather_axpy_prim(vec_buf_ac, a1);
+    mirror2.gather_axpy_prim(vec_buf_ab, a1);
+
+    // Gather from b
+    mirror0.gather_axpy_prim(vec_buf_abc, b1);
+    mirror1.gather_axpy_prim(vec_buf_ab, b1);
+    mirror2.gather_axpy_prim(vec_buf_bc, b1);
+
+    // Gather from c
+    mirror0.gather_axpy_prim(vec_buf_abc, c1);
+    mirror1.gather_axpy_prim(vec_buf_bc, c1);
+    mirror2.gather_axpy_prim(vec_buf_ac, c1);
+
+    // Now we no longer need a1, b1, c1 and overwrite them by scattering the buffers back into them
+    a1.copy(a2);
+    mirror0.scatter_axpy_prim(a1, vec_buf_abc, -DT_(1));
+    mirror1.scatter_axpy_prim(a1, vec_buf_ac, -DT_(1));
+    mirror2.scatter_axpy_prim(a1, vec_buf_ab, -DT_(1));
+
+    b1.copy(b2);
+    mirror0.scatter_axpy_prim(b1, vec_buf_abc, -DT_(1));
+    mirror1.scatter_axpy_prim(b1, vec_buf_ab, -DT_(1));
+    mirror2.scatter_axpy_prim(b1, vec_buf_bc, -DT_(1));
+
+    c1.copy(c2);
+    mirror0.scatter_axpy_prim(c1, vec_buf_abc, -DT_(1));
+    mirror1.scatter_axpy_prim(c1, vec_buf_bc, -DT_(1));
+    mirror2.scatter_axpy_prim(c1, vec_buf_ac, -DT_(1));
+
+    // Check norms
+    TEST_CHECK_EQUAL_WITHIN_EPS(a1.norm2(), DT_(0), tol);
+    TEST_CHECK_EQUAL_WITHIN_EPS(b1.norm2(), DT_(0), tol);
+    TEST_CHECK_EQUAL_WITHIN_EPS(c1.norm2(), DT_(0), tol);
+  }
+};
+
+VectorMirrorBlockedTest<Mem::Main, float, Index, 2> vector_mirror_blocked_test_generic_f;
+VectorMirrorBlockedTest<Mem::Main, double, unsigned int, 3> vector_mirror_blocked_test_generic_d;
--- kernel/lafem/vector_mirror.hpp
+++ kernel/lafem/vector_mirror.hpp
@@ -482,6 +482,472 @@ namespace FEAST
         this->scatter_axpy_prim<Tx_, Ix_, Ty_, Iy_>(vector, buffer, alpha, buffer_offset);
       }
     }; // class VectorMirror<...>
+
+    /**
+     * \brief Mirror class template for DenseVectorBlocked
+     *
+     * \author Jordi Paul
+     */
+    template<typename Mem_, typename DT_, typename IT_, int BlockSize_>
+    class VectorMirrorBlocked
+    {
+    public:
+      /// arch typedef
+      typedef Mem_ MemType;
+      /// data-type typedef
+      typedef DT_ DataType;
+      /// index-type typedef
+      typedef IT_ IndexType;
+      /// Our block size
+      static constexpr int BlockSize = BlockSize_;
+
+      /// mirror matrix typedef
+      typedef SparseMatrixCSR<Mem_, DT_, IT_> MirrorMatrixType;
+
+      /// corresponding vector
+      typedef DenseVector<Mem_, DT_, IT_> BufferVectorType;
+
+    protected:
+      /// gather-mirror matrix
+      MirrorMatrixType _mirror_gather;
+      /// scatter-mirror matrix
+      MirrorMatrixType _mirror_scatter;
+
+    public:
+      /// default constructor
+      VectorMirrorBlocked() :
+        _mirror_gather(),
+        _mirror_scatter()
+      {
+      }
+
+      /// explicit constructor
+      explicit VectorMirrorBlocked(MirrorMatrixType&& mirror_gather, MirrorMatrixType&& mirror_scatter) :
+        _mirror_gather(std::move(mirror_gather)),
+        _mirror_scatter(std::move(mirror_scatter))
+      {
+      }
+
+      /// move-ctor
+      VectorMirrorBlocked(VectorMirrorBlocked&& other) :
+        _mirror_gather(std::move(other._mirror_gather)),
+        _mirror_scatter(std::move(other._mirror_scatter))
+      {
+      }
+
+      /// move-assign operator
+      VectorMirrorBlocked& operator=(VectorMirrorBlocked&& other)
+      {
+        if(this == &other)
+          return *this;
+        _mirror_gather = std::move(other._mirror_gather);
+        _mirror_scatter = std::move(other._mirror_scatter);
+        return *this;
+      }
+
+      /**
+       * \brief Clone operation
+       *
+       * \returns A deep copy of this vector mirror.
+       */
+      VectorMirrorBlocked clone() const
+      {
+        return VectorMirrorBlocked(_mirror_gather.clone(), _mirror_scatter.clone());
+      }
+
+      /**
+       * \brief Conversion method
+       *
+       * \param[in] other
+       * The source mirror.
+       */
+      template<typename Mem2_, typename DT2_, typename IT2_>
+      void convert(const VectorMirrorBlocked<Mem2_, DT2_, IT2_, BlockSize>& other)
+      {
+        this->_mirror_gather.convert(other._mirror_gather);
+        this->_mirror_scatter.convert(other._mirror_scatter);
+      }
+
+      /// \cond internal
+      const MirrorMatrixType& get_gather_prim() const
+      {
+        return _mirror_gather;
+      }
+
+      const MirrorMatrixType& get_gather_dual() const
+      {
+        return _mirror_gather;
+      }
+
+      const MirrorMatrixType& get_scatter_prim() const
+      {
+        return _mirror_scatter;
+      }
+
+      const MirrorMatrixType& get_scatter_dual() const
+      {
+        return _mirror_scatter;
+      }
+
+      MirrorMatrixType& get_gather_prim()
+      {
+        return _mirror_gather;
+      }
+
+      MirrorMatrixType& get_gather_dual()
+      {
+        return _mirror_gather;
+      }
+
+      MirrorMatrixType& get_scatter_prim()
+      {
+        return _mirror_scatter;
+      }
+
+      MirrorMatrixType& get_scatter_dual()
+      {
+        return _mirror_scatter;
+      }
+      /// \endcond
+
+      /**
+       * \brief Returns the number of entries in the mirror.
+       */
+      Index size() const
+      {
+        return _mirror_gather.rows();
+      }
+
+      /**
+       * \brief Creates a new buffer vector.
+       */
+       BufferVectorType create_buffer_vector() const
+      {
+        return BufferVectorType(Index(BlockSize)*_mirror_gather.rows());
+      }
+
+      /**
+       * \brief Creates a new (local) vector.
+       */
+      DenseVectorBlocked<Mem_, DT_, IT_, BlockSize_>create_vector() const
+      {
+        return DenseVectorBlocked<Mem_, DT_, IT_, BlockSize_>(_mirror_gather.columns());
+      }
+
+      /**
+       * \brief Performs a gather-operation on a primal vector.
+       *
+       * \param[in,out] buffer
+       * A reference to a buffer vector.
+       *
+       * \param[in] vector
+       * A primal vector whose entries are to be gathered.
+       *
+       * \param[in] buffer_offset
+       * The offset within the buffer vector.
+       */
+      template< typename Tx_, typename Ix_, typename Ty_, typename Iy_, int BS_>
+      void gather_prim(LAFEM::DenseVector<Mem::Main, Tx_, Ix_>& buffer,
+                       const LAFEM::DenseVectorBlocked<Mem::Main, Ty_, Iy_, BS_>& vector,
+                       const Index buffer_offset = Index(0)) const
+      {
+        // skip on empty mirror
+        if(_mirror_gather.empty())
+          return;
+
+        Tx_* x(buffer.elements());
+        const Ty_ * y(vector.template elements<Perspective::pod>());
+        const IndexType* col_idx(_mirror_gather.col_ind());
+        const DataType* val(_mirror_gather.val());
+        const IndexType* row_ptr(_mirror_gather.row_ptr());
+        Index num_rows(_mirror_gather.rows());
+
+        ASSERT_(Index(BS_)*num_rows + buffer_offset <= buffer.size());
+
+        // loop over all gather-matrix rows
+        for (Index row(0) ; row < num_rows ; ++row)
+        {
+          for(Index j(0); j < Index(BS_); ++j)
+          {
+            Tx_ sum(Tx_(0));
+            for (Index i(row_ptr[row]) ; i < row_ptr[row + 1] ; ++i)
+            {
+              sum += Tx_(val[i]) * Tx_(y[Index(BS_)*col_idx[i] +j]);
+            }
+            x[buffer_offset + Index(BS_)*row + j] = sum;
+          }
+        }
+      }
+
+      /**
+       * \brief Performs a gather-axpy-operation on a primal vector.
+       *
+       * \param[in,out] buffer
+       * A reference to a buffer vector.
+       *
+       * \param[in] vector
+       * A primal vector whose entries are to be gathered.
+       *
+       * \param[in] alpha
+       * A scaling factor for the operation.
+       *
+       * \param[in] buffer_offset
+       * The offset within the buffer vector.
+       */
+      template<
+        typename Tx_,
+        typename Ix_,
+        typename Ty_,
+        typename Iy_,
+        int BS_
+      >
+      void gather_axpy_prim(LAFEM::DenseVector<Mem::Main, Tx_, Ix_>& buffer,
+                            const LAFEM::DenseVectorBlocked<Mem::Main, Ty_, Iy_, BS_>& vector,
+                            const Tx_ alpha = Tx_(1),
+                            const Index buffer_offset = Index(0)) const
+      {
+        // skip on empty mirror
+        if(_mirror_gather.empty())
+          return;
+
+        Tx_* x(buffer.elements());
+        const Ty_* y(vector.template elements<Perspective::pod>());
+        const IndexType* col_idx(_mirror_gather.col_ind());
+        const DataType* val(_mirror_gather.val());
+        const IndexType* row_ptr(_mirror_gather.row_ptr());
+        Index num_rows(_mirror_gather.rows());
+
+        ASSERT_(Index(BS_)*num_rows + buffer_offset <= buffer.size());
+
+        // loop over all gather-matrix rows
+        for (Index row(0) ; row < num_rows ; ++row)
+        {
+          for (Index j(0); j < Index(BS_); ++j)
+          {
+            Tx_ sum(Tx_(0));
+            for (Index i(row_ptr[row]) ; i < row_ptr[row + 1] ; ++i)
+            {
+              sum += Tx_(val[i]) * Tx_(y[Index(BS_)*col_idx[i] + j]);
+            }
+            x[buffer_offset + Index(BS_)*row + j] += alpha*sum;
+          }
+        }
+      }
+
+      /**
+       * \brief Performs a scatter-operation on a primal vector.
+       *
+       * \param[in,out] vector
+       * A reference to a primal vector.
+       *
+       * \param[in] buffer
+       * A reference to the buffer vector whose entries are to be scattered.
+       *
+       * \param[in] buffer_offset
+       * The offset within the buffer vector.
+       */
+      template<
+        typename Tx_,
+        typename Ix_,
+        typename Ty_,
+        typename Iy_,
+        int BS_
+      >
+      void scatter_prim( LAFEM::DenseVectorBlocked<Mem::Main, Tx_, Ix_, BS_>& vector,
+                        const LAFEM::DenseVector<Mem::Main, Ty_, Iy_>& buffer,
+                        const Index buffer_offset = Index(0)) const
+      {
+        // skip on empty mirror
+        if(_mirror_scatter.empty())
+          return;
+
+        Tx_* x(vector.template elements<Perspective::pod>());
+        const Ty_* y(buffer.elements());
+        const IndexType* col_idx(_mirror_scatter.col_ind());
+        const DataType* val(_mirror_scatter.val());
+        const IndexType* row_ptr(_mirror_scatter.row_ptr());
+        const Index num_rows(_mirror_scatter.rows());
+        const Index num_cols(_mirror_scatter.columns());
+
+        ASSERT_(Index(BS_)*num_cols + buffer_offset <= buffer.size());
+#ifndef DEBUG
+        (void)num_cols;
+#endif
+
+        // loop over all scatter-matrix rows
+        for (Index row(0) ; row < num_rows ; ++row)
+        {
+          // skip empty rows
+          if(row_ptr[row] >= row_ptr[row + 1])
+            continue;
+
+          for(Index j(0); j < Index(BS_); ++j)
+          {
+            Tx_ sum(Tx_(0));
+            for (Index i(row_ptr[row]) ; i < row_ptr[row + 1] ; ++i)
+            {
+              sum += Tx_(val[i]) * Tx_(y[buffer_offset + Index(BS_)*col_idx[i] + j]);
+            }
+            x[Index(BS_)*row + j] = sum;
+          }
+        }
+      }
+
+      /**
+       * \brief Performs a scatter-axpy-operation on a primal vector.
+       *
+       * \param[in,out] vector
+       * A reference to a primal vector.
+       *
+       * \param[in] buffer
+       * A reference to the buffer vector whose entries are to be scattered.
+       *
+       * \param[in] alpha
+       * A scaling factor for the operation.
+       *
+       * \param[in] buffer_offset
+       * The offset within the buffer vector.
+       */
+      template<
+        typename Tx_,
+        typename Ix_,
+        typename Ty_,
+        typename Iy_,
+        int BS_
+      >
+      void scatter_axpy_prim(LAFEM::DenseVectorBlocked<Mem::Main, Tx_, Ix_, BS_>& vector,
+                             const LAFEM::DenseVector<Mem::Main, Ty_, Iy_>& buffer,
+                             const Tx_ alpha = Tx_(1),
+                             const Index buffer_offset = Index(0)) const
+      {
+        // skip on empty mirror
+        if(_mirror_scatter.empty())
+          return;
+
+        Tx_* x(vector.template elements<Perspective::pod>());
+        const Ty_* y(buffer.elements());
+        const IndexType* col_idx(_mirror_scatter.col_ind());
+        const DataType* val(_mirror_scatter.val());
+        const IndexType* row_ptr(_mirror_scatter.row_ptr());
+        const Index num_rows(_mirror_scatter.rows());
+        const Index num_cols(_mirror_scatter.columns());
+
+        ASSERT_(Index(BS_)*num_cols + buffer_offset <= buffer.size());
+#ifndef DEBUG
+        (void)num_cols;
+#endif
+
+        // loop over all scatter-matrix rows
+        for (Index row(0) ; row < num_rows ; ++row)
+        {
+          // skip empty rows
+          if(row_ptr[row] >= row_ptr[row + 1])
+            continue;
+
+          for(Index j(0); j < Index(BS_); ++j)
+          {
+          Tx_ sum(Tx_(0));
+          for (Index i(row_ptr[row]) ; i < row_ptr[row + 1] ; ++i)
+          {
+            sum += Tx_(val[i]) * Tx_(y[buffer_offset + Index(BS_)*col_idx[i] + j]);
+          }
+          x[Index(BS_)*row + j] += alpha*sum;
+          }
+        }
+      }
+
+      /**
+       * \brief Performs a gather-operation on a dual vector.
+       *
+       * \param[in,out] buffer
+       * A reference to a buffer vector.
+       *
+       * \param[in] vector
+       * A dual vector whose entries are to be gathered.
+       *
+       * \param[in] buffer_offset
+       * The offset within the buffer vector.
+       */
+      template<typename Tx_, typename Ix_, typename Ty_, typename Iy_, int BS_>
+      void gather_dual( LAFEM::DenseVector<Mem::Main, Tx_, Ix_>& buffer,
+                       const LAFEM::DenseVectorBlocked<Mem::Main, Ty_, Iy_, BS_>& vector,
+                       const Index buffer_offset = Index(0)) const
+      {
+        this->gather_prim<Tx_, Ix_, Ty_, Iy_, BS_>(buffer, vector, buffer_offset);
+      }
+
+      /**
+       * \brief Performs a gather-axpy-operation on a dual vector.
+       *
+       * \param[in,out] buffer
+       * A reference to a buffer vector.
+       *
+       * \param[in] vector
+       * A dual vector whose entries are to be gathered.
+       *
+       * \param[in] alpha
+       * A scaling factor for the operation.
+       *
+       * \param[in] buffer_offset
+       * The offset within the buffer vector.
+       */
+      template<typename Tx_, typename Ix_, typename Ty_, typename Iy_, int BS_>
+      void gather_axpy_dual( LAFEM::DenseVector<Mem::Main, Tx_, Ix_>& buffer,
+                            const LAFEM::DenseVectorBlocked<Mem::Main, Ty_, Iy_, BS_>& vector,
+                            const Tx_ alpha = Tx_(1),
+                            const Index buffer_offset = Index(0)) const
+      {
+        this->gather_axpy_prim<Tx_, Ix_, Ty_, Iy_, BS_>(buffer, vector, alpha, buffer_offset);
+      }
+
+      /**
+       * \brief Performs a scatter-operation on a dual vector.
+       *
+       * \param[in,out] vector
+       * A reference to a dual vector.
+       *
+       * \param[in] buffer
+       * A reference to the buffer vector whose entries are to be scattered.
+       *
+       * \param[in] buffer_offset
+       * The offset within the buffer vector.
+       */
+      template<typename Tx_, typename Ix_, typename Ty_, typename Iy_, int BS_>
+      void scatter_dual( LAFEM::DenseVectorBlocked<Mem::Main, Tx_, Ix_, BS_>& vector,
+                        const LAFEM::DenseVector<Mem::Main, Ty_, Iy_>& buffer,
+                        const Index buffer_offset = Index(0)) const
+      {
+        this->scatter_prim<Tx_, Ix_, Ty_, Iy_, BS_>(vector, buffer, buffer_offset);
+      }
+
+      /**
+       * \brief Performs a scatter-axpy-operation on a dual vector.
+       *
+       * \param[in,out] vector
+       * A reference to a dual vector.
+       *
+       * \param[in] buffer
+       * A reference to the buffer vector whose entries are to be scattered.
+       *
+       * \param[in] alpha
+       * A scaling factor for the operation.
+       *
+       * \param[in] buffer_offset
+       * The offset within the buffer vector.
+       */
+      template<
+        typename Tx_,
+        typename Ix_,
+        typename Ty_,
+        typename Iy_>
+      void scatter_axpy_dual( LAFEM::DenseVector<Mem::Main, Tx_, Ix_>& vector,
+                             const LAFEM::DenseVector<Mem::Main, Ty_, Iy_>& buffer,
+                             const Tx_ alpha = Tx_(1),
+                             const Index buffer_offset = Index(0)) const
+      {
+        this->scatter_axpy_prim<Tx_, Ix_, Ty_, Iy_>(vector, buffer, alpha, buffer_offset);
+      }
+    }; // class VectorMirrorBlocked<...>
   } // namespace LAFEM
 } // namespace FEAST
 
