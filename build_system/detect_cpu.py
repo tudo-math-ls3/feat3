86aa155bf1a43bd36bf64411aef37ef5eb0806e4
Dirk Ribbrock <dirk.ribbrock@math.tu-dortmund.de>
2016-02-15 11:00:48 +0100

    Fixed int/string mixup
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@math.tu-dortmund.de>

--- build_system/detect_cpu.py
+++ build_system/detect_cpu.py
@@ -187,5 +187,5 @@ def detect_cpu():
 
   # TODO insert sparc support here once it has been implemented properly
 
-  print ("Warning: cputype unknown - vendor_id: " + vendor_id + ", cpu_family: " + cpu_family + ", model: " + model)
+  print ("Warning: cputype unknown - vendor_id: " + vendor_id + ", cpu_family: " + str(cpu_family) + ", model: " + str(model))
   return cputype
