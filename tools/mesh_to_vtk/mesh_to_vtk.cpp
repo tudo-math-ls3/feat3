1bd4b6e175780a85e479fc6cae75c7b423714da1
Jordi Paul <jordi.paul@math.tu-dortmund.de>
2016-05-09 16:38:55 +0200

    ENH: mesh2vtk can now handle seperate chart files
    It now uses a SimpleArgParser, so the arguments have to be given with
    switches.
    
    For meshes where the meshfile already contains all charts, nothing
    changes (i.e. venturi-pipe-...). Meshes where the charts are seperate
    (i.e. screws_...) can now be processed by specifying the chartfile on
    the command line.
    
    Signed-off-by: Jordi Paul <jordi.paul@math.tu-dortmund.de>

--- tools/mesh_to_vtk/mesh_to_vtk.cpp
+++ tools/mesh_to_vtk/mesh_to_vtk.cpp
@@ -4,9 +4,22 @@
 #include <kernel/geometry/mesh_node.hpp>
 #include <kernel/geometry/mesh_part.hpp>
 #include <kernel/geometry/mesh_file_reader.hpp>
+#include <kernel/util/simple_arg_parser.hpp>
+#include <kernel/util/runtime.hpp>
 
 using namespace FEAST;
 using namespace FEAST::Geometry;
+
+static void display_help()
+{
+  std::cout << "mesh2vtk: Reading meshes from files" << std::endl;
+  std::cout << "Mandatory arguments:" << std::endl;
+  std::cout << " --meshfile [path to mesh file]" << std::endl;
+  std::cout << "Optional arguments:" << std::endl;
+  std::cout << " --chartfile [path to chart file]" <<  std::endl;
+  std::cout << " --level [lvl_max lvl_min]" << std::endl;
+  std::cout << " --help: Displays this message" << std::endl;
+}
 
 String get_file_title(const String& filename)
 {
@@ -26,18 +39,26 @@ String get_file_title(const String& filename)
 }
 
 template<typename Mesh_>
-int run_xml(Geometry::MeshFileReader& reader, const String& filename, Index lvl_min, Index lvl_max)
+int run_xml(Geometry::MeshFileReader* mesh_reader, Geometry::MeshFileReader* chart_reader, const String& filename,
+Index lvl_min, Index lvl_max)
 {
   // create an empty atlas and a root mesh node
   Geometry::MeshAtlas<Mesh_>* atlas = new Geometry::MeshAtlas<Mesh_>();
   Geometry::RootMeshNode<Mesh_>* node = new Geometry::RootMeshNode<Mesh_>(nullptr, atlas);
+
+  ASSERT_(mesh_reader != nullptr);
 
   // try to parse the mesh file
 #ifndef DEBUG
   try
 #endif
   {
-    reader.parse(*node, *atlas);
+    // Parse the chart file first, if it was given
+    if(chart_reader != nullptr)
+      chart_reader->parse(*node, *atlas);
+
+    // Now parse the mesh file
+    mesh_reader->parse(*node, *atlas);
   }
 #ifndef DEBUG
   catch(std::exception& exc)
@@ -113,29 +134,6 @@ int run_xml(Geometry::MeshFileReader& reader, const String& filename, Index lvl_
 
 int main(int argc, char* argv[])
 {
-  if(argc < 2)
-  {
-    std::cout << "USAGE: " << argv[0] << " <meshfile> [<level-min>] [<level-max>]" << std::endl;
-    return 0;
-  }
-
-  // Input file name, required
-  FEAST::String filename(argv[1]);
-
-  // Refinement level
-  Index lvl_min(0);
-  if((argc > 2) && (!String(argv[2]).parse(lvl_min)))
-  {
-    std::cerr << "ERROR: Failed to parse '" << argv[2] << "' as refinement level!" << std::endl;
-    return 1;
-  }
-  Index lvl_max(lvl_min);
-  if((argc > 3) && (!String(argv[3]).parse(lvl_max)))
-  {
-    std::cerr << "ERROR: Failed to parse '" << argv[3] << "' as refinement level!" << std::endl;
-    return 1;
-  }
-
   // This is the list of all supported meshes that could appear in the mesh file
   typedef Geometry::ConformalMesh<Shape::Simplex<2>, 2, 2, Real> S2M2D;
   typedef Geometry::ConformalMesh<Shape::Simplex<2>, 3, 3, Real> S2M3D;
@@ -147,32 +145,108 @@ int main(int argc, char* argv[])
   typedef Geometry::ConformalMesh<Shape::Hypercube<2>, 3, 3, Real> H2M3D;
   typedef Geometry::ConformalMesh<Shape::Hypercube<3>, 3, 3, Real> H3M3D;
 
-  // create input file stream
-  std::ifstream ifs(filename, std::ios_base::in);
-  if(!ifs.is_open() || !ifs.good())
+  Runtime::initialise(argc, argv);
+
+  SimpleArgParser args(argc, argv);
+
+  args.support("meshfile");
+  args.support("chartfile");
+  args.support("level");
+
+  String chart_file_name("");
+  String mesh_file_name("");
+
+  int lvl_min(0);
+  int lvl_max(0);
+
+  Geometry::MeshFileReader* mesh_reader(nullptr);
+  Geometry::MeshFileReader* chart_reader(nullptr);
+
+  // check for unsupported options
+  auto unsupported = args.query_unsupported();
+  if( !unsupported.empty() || args.check("help") > -1)
   {
-    std::cerr << "ERROR: Failed to open '" << filename << "'" << std::endl;
+    // print all unsupported options to cerr
+    for(auto it = unsupported.begin(); it != unsupported.end(); ++it)
+      std::cerr << "ERROR: unsupported option '--" << (*it).second << "'" << std::endl;
+
+    display_help();
+
+    Runtime::abort();
     return 1;
   }
 
+  if(args.check("meshfile")!=1)
+  {
+    display_help();
+    throw InternalError(__func__,__FILE__,__LINE__, "You have to specify a mesh with --meshfile");
+  }
+
+  args.parse("meshfile", mesh_file_name);
+  std::ifstream ifs_mesh(mesh_file_name, std::ios_base::in);
+
+  if(!ifs_mesh.is_open() || !ifs_mesh.good())
+  {
+    std::cerr << "ERROR: Failed to open mesh file '" << mesh_file_name << "'" << std::endl;
+    return 1;
+  }
+  std::cout << "Reading mesh from " << mesh_file_name << std::endl;
+
   // create a reader and read the root markup
-  MeshFileReader reader(ifs);
-  reader.read_root_markup();
-
+  mesh_reader = new Geometry::MeshFileReader(ifs_mesh);
+  // create input file stream
+  mesh_reader->read_root_markup();
   // get mesh type
-  const String mtype = reader.get_meshtype_string();
+  const String mtype = mesh_reader->get_meshtype_string();
 
-  if(mtype == "conformal:hypercube:1:1") return run_xml<H1M1D>(reader, filename, lvl_min, lvl_max);
-  if(mtype == "conformal:hypercube:1:2") return run_xml<H1M2D>(reader, filename, lvl_min, lvl_max);
-  if(mtype == "conformal:hypercube:1:3") return run_xml<H1M3D>(reader, filename, lvl_min, lvl_max);
-  if(mtype == "conformal:hypercube:2:2") return run_xml<H2M2D>(reader, filename, lvl_min, lvl_max);
-  if(mtype == "conformal:hypercube:2:3") return run_xml<H2M3D>(reader, filename, lvl_min, lvl_max);
-  if(mtype == "conformal:hypercube:3:3") return run_xml<H3M3D>(reader, filename, lvl_min, lvl_max);
-  if(mtype == "conformal:simplex:2:2")   return run_xml<S2M2D>(reader, filename, lvl_min, lvl_max);
-  if(mtype == "conformal:simplex:2:3")   return run_xml<S2M3D>(reader, filename, lvl_min, lvl_max);
-  if(mtype == "conformal:simplex:3:3")   return run_xml<S3M3D>(reader, filename, lvl_min, lvl_max);
+  // Create a MeshFileReader for the optional chart file
+  std::ifstream* ifs_chart(nullptr);
+  if(args.check("chartfile")==1)
+  {
+    args.parse("chartfile", chart_file_name);
+    ifs_chart = new std::ifstream(chart_file_name, std::ios_base::in);
+    if(!ifs_chart->is_open() || !ifs_chart->good())
+    {
+      std::cerr << "ERROR: Failed to open chart file '" << chart_file_name << "'" << std::endl;
+      Runtime::abort();
+      return 1;
+    }
+    std::cout << "Reading chart file from " << chart_file_name << std::endl;
+    chart_reader = new Geometry::MeshFileReader(*ifs_chart);
+  }
 
-  // unsupported mesh type
-  std::cerr << "ERROR: invalid mesh type '" << mtype << "'" << std::endl;
-  return 1;
+  if(args.check("level") > 0)
+  {
+    args.parse("level", lvl_max, lvl_min);
+  }
+
+  int ret(1);
+  if(mtype == "conformal:hypercube:1:1")
+    ret = run_xml<H1M1D>(mesh_reader, chart_reader, mesh_file_name, lvl_min, lvl_max);
+  if(mtype == "conformal:hypercube:1:2")
+    ret = run_xml<H1M2D>(mesh_reader, chart_reader, mesh_file_name, lvl_min, lvl_max);
+  if(mtype == "conformal:hypercube:1:3")
+    ret = run_xml<H1M3D>(mesh_reader, chart_reader, mesh_file_name, lvl_min, lvl_max);
+  if(mtype == "conformal:hypercube:2:2")
+    ret = run_xml<H2M2D>(mesh_reader, chart_reader, mesh_file_name, lvl_min, lvl_max);
+  if(mtype == "conformal:hypercube:2:3")
+    ret = run_xml<H2M3D>(mesh_reader, chart_reader, mesh_file_name, lvl_min, lvl_max);
+  if(mtype == "conformal:hypercube:3:3")
+    ret = run_xml<H3M3D>(mesh_reader, chart_reader, mesh_file_name, lvl_min, lvl_max);
+  if(mtype == "conformal:simplex:2:2")
+    ret = run_xml<S2M2D>(mesh_reader, chart_reader, mesh_file_name, lvl_min, lvl_max);
+  if(mtype == "conformal:simplex:2:3")
+    ret = run_xml<S2M3D>(mesh_reader, chart_reader, mesh_file_name, lvl_min, lvl_max);
+  if(mtype == "conformal:simplex:3:3")
+    ret = run_xml<S3M3D>(mesh_reader, chart_reader, mesh_file_name, lvl_min, lvl_max);
+
+  // Clean up
+  delete ifs_chart;
+  delete mesh_reader;
+  delete chart_reader;
+
+  Runtime::finalise();
+
+  return ret;
+
 }
