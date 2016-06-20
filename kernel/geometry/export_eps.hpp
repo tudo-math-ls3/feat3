d600a125c9f0d0a6731fdc77d1f9a1167cc0b190
Peter Zajac <pzajac@math.tu-dortmund.de>
2016-06-20 13:16:15 +0200

    added mesh2eps tool
    Signed-off-by: Peter Zajac <pzajac@math.tu-dortmund.de>

---
+++ kernel/geometry/export_eps.hpp
@@ -0,0 +1,158 @@
+#pragma once
+#ifndef KERNEL_GEOMETRY_EXPORT_EPS_HPP
+#define KERNEL_GEOMETRY_EXPORT_EPS_HPP 1
+
+// includes, FEAT
+#include <kernel/geometry/conformal_mesh.hpp>
+#include <kernel/util/assertion.hpp>
+#include <kernel/util/exception.hpp>
+#include <kernel/util/math.hpp>
+
+// includes, system
+#include <iostream>
+#include <fstream>
+
+namespace FEAT
+{
+  namespace Geometry
+  {
+    /**
+     * \brief EPS exporter class
+     *
+     * This class implements an exporter for the Encapsulated Post-Script (EPS)
+     * file format, which can be used to generate mesh plots for LaTeX documents.
+     *
+     * \note
+     * This class can only export 2D meshes.
+     *
+     * \author Peter Zajac
+     */
+    class ExportEPS
+    {
+    public:
+      /**
+       * \brief Exports a mesh to an EPS file.
+       *
+       * \param[in,out] os
+       * The output stream to which to write to.
+       *
+       * \param[in] mesh
+       * The 2D conformal mesh that is to be exported.
+       *
+       * \param[in] width, height
+       * The maximum dimensions of the bounding box in millimeters.
+       *
+       * \param[in] stroke
+       * The stroke width in millimeters.
+       */
+      template<typename Shape_, int stride_, typename DataType_>
+      static void write(
+        std::ostream& os,
+        const ConformalMesh<Shape_, 2, stride_, DataType_>& mesh,
+        const double width = 100.0,
+        const double height = 100.0,
+        const double stroke = 0.1)
+      {
+        XASSERTM(width  >= 1.0, "invalid bounding box width");
+        XASSERTM(height >= 1.0, "invalid bounding box height");
+        XASSERTM(stroke > 0.01, "invalid stroke width");
+
+        // get vertices-at-edge
+        const auto& vtx = mesh.get_vertex_set();
+        const auto& idx = mesh.template get_index_set<1,0>();
+
+        // compute minimal and maximal x/y coords
+        DataType_ x_min, x_max, y_min, y_max;
+        x_min = x_max = vtx[0][0];
+        y_min = y_max = vtx[0][1];
+        for(Index i(1); i < vtx.get_num_vertices(); ++i)
+        {
+          const auto& v = vtx[i];
+          Math::minimax(v[0], x_min, x_max);
+          Math::minimax(v[1], y_min, y_max);
+        }
+
+        // compute x/y scale:
+        // Note: PostScript units are "points"
+        // 1 inch = 25.4 millimeters
+        // 1 inch = 72 points
+        // ==> 1 millimeter = 72 / 25.4 points
+        const double x_sc = (72.0 / 25.4) * width  / double(x_max - x_min);
+        const double y_sc = (72.0 / 25.4) * height / double(y_max - y_min);
+
+        // compute scaling factor
+        const double scale = Math::min(x_sc, y_sc);
+
+        // compute bounding box (rounding up)
+        const int box_x = int(scale * double(x_max - x_min)) + 1;
+        const int box_y = int(scale * double(y_max - y_min)) + 1;
+
+        // write EPS header
+        os << "%!!PS-Adobe-3.0 EPSF-3.0" << std::endl;
+
+        // write bounding box
+        os << "%%BoundingBox: 0 0 " << box_x << " " << box_y << std::endl;
+
+        // set stroke width
+        os << (72.0 * stroke / 25.4) << " setlinewidth" << std::endl;
+
+        // begin a new path
+        os << "newpath" << std::endl;
+
+        // write all edges
+        for(Index i(0); i < idx.get_num_entities(); ++i)
+        {
+          // get the vertices
+          const auto& v0 = vtx[idx[i][0]];
+          const auto& v1 = vtx[idx[i][1]];
+
+          // transform vertices
+          double v0x = (double(v0[0]) - x_min) * scale;
+          double v0y = (double(v0[1]) - y_min) * scale;
+          double v1x = (double(v1[0]) - x_min) * scale;
+          double v1y = (double(v1[1]) - y_min) * scale;
+
+          // write vertices
+          os << v0x << " " << v0y << " moveto" << std::endl;
+          os << v1x << " " << v1y << " lineto" << std::endl;
+        }
+
+        // draw path and show page
+        os << "stroke" << std::endl;
+        os << "showpage" << std::endl;
+      }
+
+      /**
+       * \brief Exports a mesh to an EPS file.
+       *
+       * \param[in] filename
+       * The name of the output file.
+       *
+       * \param[in] mesh
+       * The 2D conformal mesh that is to be exported.
+       *
+       * \param[in] width, height
+       * The maximum dimensions of the bounding box in millimeters.
+       *
+       * \param[in] stroke
+       * The stroke width in millimeters.
+       */
+      template<typename Shape_, int stride_, typename DataType_>
+      static void write(
+        const String& filename,
+        const ConformalMesh<Shape_, 2, stride_, DataType_>& mesh,
+        const double width = 100.0,
+        const double height = 100.0,
+        const double stroke = 0.1)
+      {
+        std::ofstream ofs(filename);
+        if(!ofs.is_open() || !ofs.good())
+          throw FileError(String("Failed to open output file: ") + filename);
+        write(ofs, mesh, width, height, stroke);
+        ofs.close();
+      }
+    };
+  } // namespace Geometry
+} // namespace FEAT
+
+#endif // KERNEL_GEOMETRY_EXPORT_EPS_HPP
---
+++ tools/mesh_to_eps/mesh_to_eps.cpp
@@ -0,0 +1,231 @@
+#include <kernel/geometry/conformal_factories.hpp>
+#include <kernel/geometry/export_eps.hpp>
+#include <kernel/geometry/mesh_atlas.hpp>
+#include <kernel/geometry/mesh_node.hpp>
+#include <kernel/geometry/mesh_part.hpp>
+#include <kernel/geometry/mesh_file_reader.hpp>
+#include <kernel/util/simple_arg_parser.hpp>
+#include <kernel/util/runtime.hpp>
+
+using namespace FEAT;
+using namespace FEAT::Geometry;
+
+static void display_help()
+{
+  std::cout << "mesh2eps: Converts a mesh from FEAT format to EPS format" << std::endl;
+  std::cout << std::endl;
+  std::cout << "Mandatory arguments:" << std::endl;
+  std::cout << " --meshfile [path to mesh file]" << std::endl;
+  std::cout << "Optional arguments:" << std::endl;
+  std::cout << " --chartfile [path to chart file]" <<  std::endl;
+  std::cout << " --level [lvl_max lvl_min]" << std::endl;
+  std::cout << " --box [width height]" << std::endl;
+  std::cout << " --stroke [width]" << std::endl;
+  std::cout << " --no-adapt" << std::endl;
+  std::cout << " --help: Displays this message" << std::endl;
+}
+
+String get_file_title(const String& filename)
+{
+  // find last slash
+  std::size_t p = filename.find_last_of("\\/");
+  if(p == filename.npos)
+    p = 0;
+  else
+    ++p;
+
+  // fine last dot
+  std::size_t q = filename.find_last_of(".");
+  if(q == filename.npos)
+    return filename.substr(p);
+  else
+    return filename.substr(p, q-p);
+}
+
+template<typename Mesh_>
+int run_xml(Geometry::MeshFileReader* mesh_reader, Geometry::MeshFileReader* chart_reader,
+  const String& filename, Index lvl_min, Index lvl_max, bool adapt, double box_x, double box_y, double stroke)
+{
+  // create an empty atlas and a root mesh node
+  Geometry::MeshAtlas<Mesh_>* atlas = new Geometry::MeshAtlas<Mesh_>();
+  Geometry::RootMeshNode<Mesh_>* node = new Geometry::RootMeshNode<Mesh_>(nullptr, atlas);
+
+  XASSERT(mesh_reader != nullptr);
+
+  // try to parse the mesh file
+#ifndef DEBUG
+  try
+#endif
+  {
+    // Parse the chart file first, if it was given
+    if(chart_reader != nullptr)
+      chart_reader->parse(*node, *atlas);
+
+    // Now parse the mesh file
+    mesh_reader->parse(*node, *atlas);
+  }
+#ifndef DEBUG
+  catch(std::exception& exc)
+  {
+    std::cerr << "ERROR: " << exc.what() << std::endl;
+    return 1;
+  }
+  catch(...)
+  {
+    std::cerr << "ERROR: unknown exception" << std::endl;
+    return 1;
+  }
+#endif
+
+  // adapt coarse mesh
+  if(adapt)
+    node->adapt();
+
+  // choose adapt mode
+  const AdaptMode adapt_mode = adapt ? AdaptMode::chart : AdaptMode::none;
+
+  // get all mesh part names
+  std::deque<String> part_names = node->get_mesh_part_names();
+
+  // refine
+  for(Index lvl(0); lvl <= lvl_max; ++lvl)
+  {
+    if(lvl > 0)
+    {
+      std::cout << "Refining up to level " << lvl << "..." << std::endl;
+      auto* old = node;
+      node = old->refine(adapt_mode);
+      delete old;
+    }
+
+    if(lvl < lvl_min)
+      continue;
+
+    FEAT::String epsname = get_file_title(filename) + "." + stringify(lvl) + ".eps";
+    std::cout << "Writing file '" << epsname << "'..." << std::endl;
+
+    Geometry::ExportEPS::write(epsname, *node->get_mesh(), box_x, box_y, stroke);
+  }
+
+  delete node;
+  delete atlas;
+
+  return 0;
+}
+
+int main(int argc, char* argv[])
+{
+  // This is the list of all supported meshes that could appear in the mesh file
+  typedef Geometry::ConformalMesh<Shape::Simplex<2>, 2, 2, Real> S2M2D;
+  typedef Geometry::ConformalMesh<Shape::Hypercube<2>, 2, 2, Real> H2M2D;
+
+  Runtime::initialise(argc, argv);
+
+  SimpleArgParser args(argc, argv);
+
+  // need help?
+  if(args.check("help") > -1)
+  {
+    display_help();
+    return Runtime::finalise();
+  }
+
+  args.support("meshfile");
+  args.support("chartfile");
+  args.support("level");
+  args.support("no-adapt");
+  args.support("box");
+  args.support("stroke");
+
+  String chart_file_name("");
+  String mesh_file_name("");
+
+  Index lvl_min(0);
+  Index lvl_max(0);
+
+  double box_x(100.0), box_y(100.0), stroke(0.1);
+
+  Geometry::MeshFileReader* mesh_reader(nullptr);
+  Geometry::MeshFileReader* chart_reader(nullptr);
+
+  // check for unsupported options
+  auto unsupported = args.query_unsupported();
+  if( !unsupported.empty() )
+  {
+    // print all unsupported options to cerr
+    for(auto it = unsupported.begin(); it != unsupported.end(); ++it)
+      std::cerr << "ERROR: unsupported option '--" << (*it).second << "'" << std::endl;
+
+    display_help();
+
+    return 1;
+  }
+
+  if(args.check("meshfile")!=1)
+  {
+    std::cerr << "ERROR: You have to specify a mesh with --meshfile" << std::endl;
+    display_help();
+    return 1;
+  }
+
+  args.parse("meshfile", mesh_file_name);
+  std::ifstream ifs_mesh(mesh_file_name, std::ios_base::in);
+
+  if(!ifs_mesh.is_open() || !ifs_mesh.good())
+  {
+    std::cerr << "ERROR: Failed to open mesh file '" << mesh_file_name << "'" << std::endl;
+    return 1;
+  }
+  std::cout << "Reading mesh from " << mesh_file_name << std::endl;
+
+  // create a reader and read the root markup
+  mesh_reader = new Geometry::MeshFileReader(ifs_mesh);
+  // create input file stream
+  mesh_reader->read_root_markup();
+  // get mesh type
+  const String mtype = mesh_reader->get_meshtype_string();
+
+  // Create a MeshFileReader for the optional chart file
+  std::ifstream* ifs_chart(nullptr);
+  if(args.check("chartfile")==1)
+  {
+    args.parse("chartfile", chart_file_name);
+    ifs_chart = new std::ifstream(chart_file_name, std::ios_base::in);
+    if(!ifs_chart->is_open() || !ifs_chart->good())
+    {
+      std::cerr << std::endl << "ERROR: Failed to open chart file '" << chart_file_name << "'" << std::endl;
+      return 1;
+    }
+    std::cout << "Reading chart file from " << chart_file_name << std::endl;
+    chart_reader = new Geometry::MeshFileReader(*ifs_chart);
+  }
+
+  if(args.check("level") > 0)
+  {
+    args.parse("level", lvl_max, lvl_min);
+  }
+
+  bool adapt = (args.check("no-adapt") < 0);
+
+  args.parse("box", box_x, box_y);
+  args.parse("stroke", stroke);
+
+  int ret(1);
+  if(mtype == "conformal:hypercube:2:2")
+    ret = run_xml<H2M2D>(mesh_reader, chart_reader, mesh_file_name, lvl_min, lvl_max, adapt, box_x, box_y, stroke);
+  if(mtype == "conformal:simplex:2:2")
+    ret = run_xml<S2M2D>(mesh_reader, chart_reader, mesh_file_name, lvl_min, lvl_max, adapt, box_x, box_y, stroke);
+
+  // Clean up
+  if(ifs_chart != nullptr)
+    delete ifs_chart;
+  if(mesh_reader != nullptr)
+    delete mesh_reader;
+  if(chart_reader != nullptr)
+    delete chart_reader;
+
+  Runtime::finalise();
+
+  return ret;
+
+}
