f0b33e9c97d9301dbaa4bd0ccf0c14f3f717713f
Jordi Paul <jordi.paul@math.tu-dortmund.de>
2016-09-22 10:37:28 +0200

    ENH: Added mpi_cout_ordered
    This can be used to get all MPI ranks to print one array to std::cout,
    ordered by ranks. The implementation is the most inefficient and most
    portable, as it gathers all information in one rank which then prints to
    std::cout. A construction with multiple MPI barriers only works for
    certain MPI implementations.
    
    As this implementation creates a communication bottleneck, it is
    intended only for debugging purposes.
    
    Signed-off-by: Jordi Paul <jordi.paul@math.tu-dortmund.de>

--- kernel/util/mpi_cout.hpp
+++ kernel/util/mpi_cout.hpp
@@ -52,6 +52,128 @@ namespace FEAT
       mpi_cout(s.pad_back(width, '.') + ": " + stringify(t) + "\n");
     }
 
+    /**
+     * \brief Writes an array, ordered by rank
+     *
+     * \tparam DT_ Data type, could be anything that can be sent through MPI
+     *
+     * \param[in] val
+     * The data array.
+     *
+     * \param[in] str
+     * String the prepend to the output.
+     *
+     * \param[in] size
+     * The number of entries of val to print.
+     *
+     * \warning To order the output, it is neccessary to send all information to one rank, which then prints
+     * everything. This is a slow communication bottleneck and should ONLY be used for debugging purposes.
+     *
+     * Sometimes, a series of barriers can be used to get the same effect without first gathering all data in one
+     * rank, but this is highly dependent on the MPI implementation. This is the most inefficient, but most portable
+     * variant.
+     *
+     */
+    template<typename DT_>
+    static inline void mpi_cout_ordered(const DT_* val, const String& str= "", Index size = Index(1))
+    {
+      Index* other_size(new Index[Util::Comm::size()]);
+
+#ifdef FEAT_HAVE_MPI
+      DT_* buffer;
+      Index max_size(0);
+
+      // Ignore all comm statuses
+      CommStatusIgnore st;
+      // This is the rank that receives everything
+      if(Util::Comm::rank() == Index(0))
+      {
+        // First, write out our own stuff
+        std::cout << Util::Comm::rank() << " " << str << ": ";
+
+        // Enclose within brackets if we write more than one value
+        if(size > Index(1))
+          std::cout << "[";
+
+        for(Index i(0); i < size; ++i)
+          std::cout << " " << val[i];
+
+        if(size > Index(1))
+          std::cout << " ]";
+
+        std::cout << std::endl;
+
+        // Get the sizes of what the other ranks want to send
+        for(Index r(1); r < Util::Comm::size(); ++r)
+        {
+          Util::Comm::recv(&(other_size[r]), Index(1), r, st);
+          if(other_size[r] > max_size)
+            max_size = other_size[r];
+        }
+
+        // Now that we know the size we can allocate the buffer for receiving
+        buffer = new DT_[max_size];
+
+        // Receive data from other ranks and print it
+        for(Index r(1); r < Util::Comm::size(); ++r)
+        {
+
+          Util::Comm::recv(buffer, other_size[r], r, st);
+
+          std::cout << r << " " << str << ": ";
+
+          // Enclose within brackets if we write more than one value
+          if(size > Index(1))
+            std::cout << "[";
+
+          for(Index i(0); i < other_size[r]; ++i)
+          {
+            std::cout << " " << buffer[i];
+          }
+
+          if(size > Index(1))
+            std::cout << " ]";
+
+          std::cout << std::endl;
+
+        }
+
+      }
+      // All other ranks
+      else
+      {
+        Comm::send(&size, Index(1), Index(0));
+
+        // Because MPI doesn't care for const, we copy our data to the buffer here and send the buffer
+        buffer = new DT_[size];
+        for(Index i(0); i < size; ++i)
+          buffer[i] = val[i];
+
+        Util::Comm::send(buffer, size, Index(0));
+
+      }
+      delete[] buffer;
+#else
+      // Write out our own stuff
+      std::cout << Util::Comm::rank() << " " << str << ": ";
+
+      // Enclose within brackets if we write more than one value
+      if(size > Index(1))
+        std::cout << "[";
+
+      for(Index i(0); i < size; ++i)
+        std::cout << " " << val[i];
+
+      if(size > Index(1))
+        std::cout << " ]";
+
+      std::cout << std::endl;
+#endif
+
+      delete[] other_size;
+
+    } // void mpi_cout_ordered
+
   } // namespace Util
 
 } // namespace FEAT
