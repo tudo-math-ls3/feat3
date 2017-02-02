f1d15559ac81c5f8e65cd4cbe95c901159041a9c
Jordi Paul <jordi.paul@math.tu-dortmund.de>
2017-02-02 08:07:21 +0100

    ENH: Added vector valued error computer
    Signed-off-by: Jordi Paul <jordi.paul@math.tu-dortmund.de>

--- kernel/assembly/error_computer.hpp
+++ kernel/assembly/error_computer.hpp
@@ -483,6 +483,493 @@ namespace FEAT
         return result;
       }
     }; // class ScalarErrorComputer
+
+    /// \cond internal
+    namespace Intern
+    {
+      template<bool h0_, typename AsmTraits_, typename AnaTraits_>
+      struct VecHelperH0
+      {
+        typedef typename AsmTraits_::DataType DataType;
+
+        template<typename FuncEval_, typename LocalVec_>
+        static DataType eval(
+          FuncEval_&,
+          const typename AsmTraits_::TrafoEvalData&,
+          const typename AsmTraits_::SpaceEvalData&,
+          const LocalVec_&,
+          const int
+          )
+        {
+          return DataType(0);
+        }
+      };
+
+      template<typename AsmTraits_, typename AnaTraits_>
+      struct VecHelperH0<true, AsmTraits_, AnaTraits_>
+      {
+        typedef typename AsmTraits_::DataType DataType;
+
+        template<typename FuncEval_, typename LocalVec_>
+        static DataType eval(
+          FuncEval_& func_eval,
+          const typename AsmTraits_::TrafoEvalData& trafo_data,
+          const typename AsmTraits_::SpaceEvalData& space_data,
+          const LocalVec_& lvad,
+          const int num_loc_dofs
+          )
+        {
+          // evaluate function value
+          typename AnaTraits_::ValueType value(DataType(0));
+          func_eval.value(value, trafo_data.img_point);
+
+          // test function loop
+          for(int i(0); i < num_loc_dofs; ++i)
+          {
+            // subtract basis function value
+            value -= lvad[i] * space_data.phi[i].value;
+            // continue with next trial function
+          }
+
+          // update result
+          return value.norm_euclid_sqr();
+        }
+      };
+
+      template<bool h1_, typename AsmTraits_, typename AnaTraits_>
+      struct VecHelperH1
+      {
+        typedef typename AsmTraits_::DataType DataType;
+
+        template<typename FuncEval_, typename LocalVec_>
+        static DataType eval(
+          FuncEval_&,
+          const typename AsmTraits_::TrafoEvalData&,
+          const typename AsmTraits_::SpaceEvalData&,
+          const LocalVec_&,
+          const int
+          )
+        {
+          return DataType(0);
+        }
+      };
+
+      template<typename AsmTraits_, typename AnaTraits_>
+      struct VecHelperH1<true, AsmTraits_, AnaTraits_>
+      {
+        typedef typename AsmTraits_::DataType DataType;
+
+        template<typename FuncEval_, typename LocalVec_>
+        static DataType eval(
+          FuncEval_& func_eval,
+          const typename AsmTraits_::TrafoEvalData& trafo_data,
+          const typename AsmTraits_::SpaceEvalData& space_data,
+          const LocalVec_& lvad,
+          const int num_loc_dofs
+          )
+        {
+          // evaluate function gradient
+          typename AnaTraits_::GradientType grad(DataType(0));
+          func_eval.gradient(grad, trafo_data.img_point);
+
+          // test function loop
+          for(int i(0); i < num_loc_dofs; ++i)
+          {
+            // subtract basis function gradient
+            for(int d(0); d < AnaTraits_::GradientType::n; ++d)
+            {
+              grad[d] -= space_data.phi[i].grad * lvad[i](d);
+            }
+            // continue with next trial function
+          }
+
+          // update result
+          return grad.norm_frobenius_sqr();
+        }
+      };
+
+      template<bool h2_, typename AsmTraits_, typename AnaTraits_>
+      struct VecHelperH2
+      {
+        typedef typename AsmTraits_::DataType DataType;
+
+        template<typename FuncEval_, typename LocalVec_>
+        static DataType eval(
+          FuncEval_&,
+          const typename AsmTraits_::TrafoEvalData&,
+          const typename AsmTraits_::SpaceEvalData&,
+          const LocalVec_&,
+          const int
+          )
+        {
+          return DataType(0);
+        }
+      };
+
+      //template<typename AsmTraits_, typename AnaTraits_>
+      //struct VecHelperH2<true, AsmTraits_, AnaTraits_>
+      //{
+      //  typedef typename AsmTraits_::DataType DataType;
+
+      //  template<typename FuncEval_, typename LocalVec_>
+      //  static DataType eval(
+      //    FuncEval_& func_eval,
+      //    const typename AsmTraits_::TrafoEvalData& trafo_data,
+      //    const typename AsmTraits_::SpaceEvalData& space_data,
+      //    const LocalVec_& lvad,
+      //    const int num_loc_dofs
+      //    )
+      //  {
+      //    // evaluate functor
+      //    typename AnaTraits_::HessianType value(DataType(0));
+      //    func_eval.hessian(value, trafo_data.img_point);
+
+      //    // test function loop
+      //    for(int i(0); i < num_loc_dofs; ++i)
+      //    {
+      //      // subtract basis function gradient
+      //      value -= lvad[i] * space_data.phi[i].hess;
+      //      // continue with next trial function
+      //    }
+
+      //    // update result
+      //    return value.norm_hessian_sqr();
+      //  }
+      //};
+    } // namespace Intern
+    /// \endcond
+
+    /**
+     * \brief Vector Error information structure
+     *
+     * This structure encapsulated the up to three error norms computed by the VectorErrorComputer
+     * class template.
+     */
+    template<typename DataType_>
+    struct VectorErrorInfo
+    {
+      /// Specifies whether the H0-error was computed.
+      bool have_h0;
+      /// Specifies whether the H1-error was computed.
+      bool have_h1;
+      /// Specifies whether the H2-error was computed.
+      bool have_h2;
+
+      /// The computed H0-error.
+      DataType_ norm_h0;
+      /// The computed H1-error.
+      DataType_ norm_h1;
+      /// The computed H2-error.
+      DataType_ norm_h2;
+
+      /// standard constructor
+      VectorErrorInfo() :
+        have_h0(false),
+        have_h1(false),
+        have_h2(false),
+        norm_h0(DataType_(0)),
+        norm_h1(DataType_(0)),
+        norm_h2(DataType_(0))
+      {
+      }
+
+      /// conversion constructor
+      template<typename DT2_>
+      VectorErrorInfo(const VectorErrorInfo<DT2_>& other) :
+        have_h0(other.have_h0),
+        have_h1(other.have_h1),
+        have_h2(other.have_h2),
+        norm_h0(DataType_(other.norm_h0)),
+        norm_h1(DataType_(other.norm_h1)),
+        norm_h2(DataType_(other.norm_h2))
+      {
+      }
+
+      /// conversion assignment operator
+      template<typename DT2_>
+      VectorErrorInfo& operator=(const VectorErrorInfo<DT2_>& other)
+      {
+        have_h0 = other.have_h0;
+        have_h1 = other.have_h1;
+        have_h2 = other.have_h2;
+        norm_h0 = DataType_(other.norm_h0);
+        norm_h1 = DataType_(other.norm_h1);
+        norm_h2 = DataType_(other.norm_h2);
+      }
+
+      /**
+       * \brief Synchronises the error information over a communicator
+       *
+       * This function sums up the error information of all patches in a
+       * parallel simulation to obtain the information for the global mesh.
+       *
+       * \param[in] comm
+       * The communication over which to synchronise.
+       */
+      void synchronise(const Dist::Comm& comm)
+      {
+        DataType_ verr[3] =
+        {
+          have_h0 ? Math::sqr(norm_h0) : DataType_(0),
+          have_h1 ? Math::sqr(norm_h1) : DataType_(0),
+          have_h2 ? Math::sqr(norm_h2) : DataType_(0)
+        };
+
+        comm.allreduce(verr, verr, std::size_t(3), Dist::op_sum);
+
+        if(have_h0) norm_h0 = Math::sqrt(verr[0]);
+        if(have_h1) norm_h1 = Math::sqrt(verr[1]);
+        if(have_h2) norm_h2 = Math::sqrt(verr[2]);
+      }
+
+      /**
+       * \brief Formats the error information as a string.
+       *
+       * \param[in] precision
+       * The precision for floating point values.
+       *
+       * \param[in] pad_size
+       * The leading string padding size. Should be >= 8 to achieve vertical alignment.
+       *
+       * \param[in] pad_char
+       * The leading string padding character.
+       *
+       * \returns
+       * A string containing the formatted error info.
+       */
+      String format_string(int precision = 0, std::size_t pad_size = 8u, char pad_char = '.') const
+      {
+        String s;
+        if(have_h0)
+          s += String("H0-Error").pad_back(pad_size, pad_char) + ": " + stringify_fp_sci(norm_h0, precision) + "\n";
+        if(have_h1)
+          s += String("H1-Error").pad_back(pad_size, pad_char) + ": " + stringify_fp_sci(norm_h1, precision) + "\n";
+        if(have_h2)
+          s += String("H2-Error").pad_back(pad_size, pad_char) + ": " + stringify_fp_sci(norm_h2, precision) + "\n";
+        return s;
+      }
+
+      /// prints the info to an output stream
+      friend std::ostream& operator<<(std::ostream& os, const VectorErrorInfo& sei)
+      {
+        // print errors
+        if(sei.have_h0)
+          os << "H0-Error: " << stringify_fp_sci(sei.norm_h0) << std::endl;
+        if(sei.have_h1)
+          os << "H1-Error: " << stringify_fp_sci(sei.norm_h1) << std::endl;
+        if(sei.have_h2)
+          os << "H2-Error: " << stringify_fp_sci(sei.norm_h2) << std::endl;
+        return os;
+      }
+    }; // struct VectorErrorInfo
+
+    /**
+     * \brief Vector H0/H1/H2-error computer class template
+     *
+     * This class template implements the computation of errors between a finite element
+     * function and an analytic reference function in the H0-norm as well as the H1- and
+     * H2-semi-norms, if desired.
+     *
+     * \tparam max_norm_
+     * Specifies the maximal H^k-semi-norm in which the error is to be measured:
+     * - max_norm_ = 0: compute only H0-norm of error
+     * - max_norm_ = 1: compute H0-norm and H1-semi-norm of error
+     * - max_norm_ = 2: compute H0-norm, H1- and H2-semi-norms of error
+     *
+     * \attention
+     * To compute the H1- and H2-semi-norms of the error, both the analytic reference function
+     * as well as the finite element space need to be able to compute gradients and hessians,
+     * respectively.
+     *
+     * \author Peter Zajac
+     */
+    template<int max_norm_ = 0>
+    class VectorErrorComputer
+    {
+      static_assert(max_norm_ >= 0, "invalid max_norm_ parameter");
+
+    private:
+      /// \cond internal
+      /**
+       * \brief Trafo configuration tag class
+       */
+      static constexpr TrafoTags trafo_config = TrafoTags::img_point | TrafoTags::jac_det;
+
+      /**
+       * \brief Space configuration tag class
+       */
+      static constexpr SpaceTags space_config =
+        ((max_norm_ >= 0) ? SpaceTags::value : SpaceTags::none) |
+        ((max_norm_ >= 1) ? SpaceTags::grad : SpaceTags::none) |
+        ((max_norm_ >= 2) ? SpaceTags::hess : SpaceTags::none);
+      /// \endcond
+
+    public:
+      /**
+       * \brief Computes the H0/H1/H2-error.
+       *
+       * This function computes the norm of the difference of a discrete Finite-Element function
+       * and an analytic function in the H0, H1 and H2 semi-norms, depending on the template argument
+       * of this class.
+       *
+       * \param[in] vector
+       * The coefficient vector of the FE function.
+       *
+       * \param[in] function
+       * An object implementing the AnalyticFunction interface representing the analytic function to be
+       * tested against.
+       *
+       * \param[in] space
+       * The Finite-Element space that the coefficient vector belongs to.
+       *
+       * \param[in] cubature_factory
+       * A reference to the cubature factory to be used for integration.
+       *
+       * \returns
+       * A VectorErrorInfo object which contains the H0-, H1- and H2-error (semi-)norms.
+       */
+      template<
+        typename Vector_,
+        typename Function_,
+        typename Space_,
+        typename CubatureFactory_>
+      static VectorErrorInfo<typename Vector_::DataType> compute(
+        const Vector_& vector,
+        const Function_& function,
+        const Space_& space,
+        const CubatureFactory_& cubature_factory)
+      {
+        // make sure the function has everything we need
+        static_assert(Function_::can_value, "function values are required for H0-Error");
+        static_assert(Function_::can_grad || (max_norm_ < 1), "function gradients are required for H1-Error");
+        static_assert(Function_::can_hess || (max_norm_ < 2), "function hessians are required for H2-Error");
+
+        // vector type
+        typedef Vector_ VectorType;
+        // analytic function type
+        typedef Function_ FunctionType;
+        // space type
+        typedef Space_ SpaceType;
+        /// The domain dim
+        static constexpr int dim = Function_::domain_dim;
+        /// We use the scalar assembly traits and just define our own LocalVectorData below
+        typedef AsmTraits1
+        <
+          typename Vector_::DataType,
+          SpaceType,
+          trafo_config,
+          space_config
+        > AsmTraits;
+        // data type
+        typedef typename AsmTraits::DataType DataType;
+
+        // create the cubature rule
+        typename AsmTraits::CubatureRuleType cubature_rule(Cubature::ctor_factory, cubature_factory);
+
+        // fetch the trafo
+        const typename AsmTraits::TrafoType& trafo = space.get_trafo();
+
+        // define our analytic evaluation traits
+        typedef Analytic::EvalTraits<DataType, Function_> AnalyticEvalTraits;
+
+        // create a function evaluator
+        typename FunctionType::template Evaluator<AnalyticEvalTraits> func_eval(function);
+
+        // create a trafo evaluator
+        typename AsmTraits::TrafoEvaluator trafo_eval(trafo);
+
+        // create a space evaluator and evaluation data
+        typename AsmTraits::SpaceEvaluator space_eval(space);
+
+        // create a dof-mapping
+        typename AsmTraits::DofMapping dof_mapping(space);
+
+        // create trafo evaluation data
+        typename AsmTraits::TrafoEvalData trafo_data;
+
+        // create space evaluation data
+        typename AsmTraits::SpaceEvalData space_data;
+
+        // create local vector data
+        typedef Tiny::Vector<DataType, dim> VectorValue;
+        typedef Tiny::Vector<VectorValue, AsmTraits::max_local_test_dofs> LocalVectorType;
+        LocalVectorType lvad;
+
+        // create matrix scatter-axpy
+        typename VectorType::GatherAxpy gather_axpy(vector);
+
+        // initialise result
+        VectorErrorInfo<DataType> result;
+        result.have_h0 = (max_norm_ >= 0);
+        result.have_h1 = (max_norm_ >= 1);
+        result.have_h2 = (max_norm_ >= 2);
+
+        // H0/H1/H2 helpers
+        typedef Intern::VecHelperH0<(max_norm_ >= 0), AsmTraits, AnalyticEvalTraits> VecH0;
+        typedef Intern::VecHelperH1<(max_norm_ >= 1), AsmTraits, AnalyticEvalTraits> VecH1;
+        typedef Intern::VecHelperH2<(max_norm_ >= 2), AsmTraits, AnalyticEvalTraits> VecH2;
+
+        // loop over all cells of the mesh
+        for(typename AsmTraits::CellIterator cell(trafo_eval.begin()); cell != trafo_eval.end(); ++cell)
+        {
+          // format local vector
+          lvad.format();
+
+          // initialise dof-mapping
+          dof_mapping.prepare(cell);
+
+          // incorporate local matrix
+          gather_axpy(lvad, dof_mapping);
+
+          // finish dof-mapping
+          dof_mapping.finish();
+
+          // prepare trafo evaluator
+          trafo_eval.prepare(cell);
+
+          // prepare space evaluator
+          space_eval.prepare(trafo_eval);
+
+          // fetch number of local dofs
+          int num_loc_dofs = space_eval.get_num_local_dofs();
+
+          // loop over all quadrature points and integrate
+          for(int k(0); k < cubature_rule.get_num_points(); ++k)
+          {
+            // compute trafo data
+            trafo_eval(trafo_data, cubature_rule.get_point(k));
+
+            // compute basis function data
+            space_eval(space_data, trafo_data);
+
+            // compute integration weight
+            DataType omega = trafo_data.jac_det * cubature_rule.get_weight(k);
+
+            // update results
+            result.norm_h0 += omega * VecH0::eval(func_eval, trafo_data, space_data, lvad, num_loc_dofs);
+            result.norm_h1 += omega * VecH1::eval(func_eval, trafo_data, space_data, lvad, num_loc_dofs);
+            result.norm_h2 += omega * VecH2::eval(func_eval, trafo_data, space_data, lvad, num_loc_dofs);
+
+            // continue with next cubature point
+          }
+
+          // finish evaluators
+          space_eval.finish();
+          trafo_eval.finish();
+
+          // continue with next cell
+        }
+
+        // take square-roots
+        result.norm_h0 = Math::sqrt(result.norm_h0);
+        result.norm_h1 = Math::sqrt(result.norm_h1);
+        result.norm_h2 = Math::sqrt(result.norm_h2);
+
+        // okay, that's it
+        return result;
+      }
+    }; // class VectorErrorComputer
+
   } // namespace Assembly
 } // namespace FEAT
 
