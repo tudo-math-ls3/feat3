63ad38e12193e12bfe588b30529ccd1012cf179f
Jordi Paul <jordi.paul@math.tu-dortmund.de>
2017-05-01 07:43:33 +0200

    (velo, pres) -> (trial, test) in GradOperatorAssembler
    Signed-off-by: Jordi Paul <jordi.paul@math.tu-dortmund.de>

--- kernel/assembly/grad_operator_assembler.hpp
+++ kernel/assembly/grad_operator_assembler.hpp
@@ -36,10 +36,10 @@ namespace FEAT
        * \tparam dim_
        * The world dimension, which is also the block size
        *
-       * \tparam SpaceVelo_
+       * \tparam SpaceTest_
        * The space for \f$ \phi \f$
        *
-       * \tparam SpacePres_
+       * \tparam SpaceTrial_
        * The space for \f$ \psi \f$
        *
        * \tparam CubatureFactory_
@@ -48,10 +48,10 @@ namespace FEAT
        * \param[out] matrix_g
        * The matrix we assemble into, gets overwritten
        *
-       * \param[in] space_velo
+       * \param[in] test_space
        * The space for \f$ \phi \f$
        *
-       * \param[in] space_pres
+       * \param[in] trial_space
        * The space for \f$ \psi \f$
        *
        * \param[in] cubature_factory
@@ -61,12 +61,12 @@ namespace FEAT
        * Scaling parameter \f$ \alpha \f$
        */
       template<typename DT_, typename IT_, int dim_,
-        typename SpaceVelo_, typename SpacePres_,
+        typename SpaceTest_, typename SpaceTrial_,
         typename CubatureFactory_>
       static void assemble(
         LAFEM::SparseMatrixBCSR<Mem::Main, DT_, IT_, dim_, 1>& matrix_g,
-        const SpaceVelo_& space_velo,
-        const SpacePres_& space_pres,
+        const SpaceTest_& test_space,
+        const SpaceTrial_& trial_space,
         const CubatureFactory_& cubature_factory,
         const DT_ scale = DT_(1)
         )
@@ -76,52 +76,52 @@ namespace FEAT
         static constexpr int dim = dim_;
 
         // ensure that the matrices have the correct dimensions
-        static_assert(SpaceVelo_::shape_dim == dim_, "invalid matrix block sizes");
+        static_assert(SpaceTest_::shape_dim == dim_, "invalid matrix block sizes");
 
-        // make sure that velocity and pressure have the same trafo
-        if((&space_velo.get_trafo()) != (&space_pres.get_trafo()))
-          throw InternalError("Velocity and Pressure spaces must be defined on the same trafo!");
+        // make sure that trial and test spaces have the same trafo
+        if((&test_space.get_trafo()) != (&trial_space.get_trafo()))
+          throw InternalError("Trial and test spaces must be defined on the same trafo!");
 
         typedef LAFEM::SparseMatrixBCSR<Mem::Main, DataType, IndexType, dim, 1> MatrixG;
 
         // assembly traits
         typedef AsmTraits2<
           DataType,
-          SpaceVelo_,
-          SpacePres_,
+          SpaceTest_,
+          SpaceTrial_,
           TrafoTags::jac_det,
           SpaceTags::value,
           SpaceTags::grad> AsmTraits;
 
         // fetch the trafo
-        const typename AsmTraits::TrafoType& trafo = space_velo.get_trafo();
+        const typename AsmTraits::TrafoType& trafo = test_space.get_trafo();
 
         // create a trafo evaluator
         typename AsmTraits::TrafoEvaluator trafo_eval(trafo);
 
         // create space evaluators
-        typename AsmTraits::TestEvaluator velo_eval(space_velo);
-        typename AsmTraits::TrialEvaluator pres_eval(space_pres);
+        typename AsmTraits::TestEvaluator test_eval(test_space);
+        typename AsmTraits::TrialEvaluator trial_eval(trial_space);
 
         // create dof-mappings
-        typename AsmTraits::TestDofMapping velo_dof_mapping(space_velo);
-        typename AsmTraits::TrialDofMapping pres_dof_mapping(space_pres);
+        typename AsmTraits::TestDofMapping test_dof_mapping(test_space);
+        typename AsmTraits::TrialDofMapping trial_dof_mapping(trial_space);
 
         // create trafo evaluation data
         typename AsmTraits::TrafoEvalData trafo_data;
 
         // create space evaluation data
-        typename AsmTraits::TestEvalData velo_data;
-        typename AsmTraits::TrialEvalData pres_data;
+        typename AsmTraits::TestEvalData test_data;
+        typename AsmTraits::TrialEvalData trial_data;
 
         // maximum number of dofs
-        static constexpr int max_velo_dofs = AsmTraits::max_local_test_dofs;
-        static constexpr int max_pres_dofs = AsmTraits::max_local_trial_dofs;
+        static constexpr int max_test_dofs = AsmTraits::max_local_test_dofs;
+        static constexpr int max_trial_dofs = AsmTraits::max_local_trial_dofs;
 
         // create local matrix data
         //typename AsmTraits::LocalMatrixType lmd;
         typedef Tiny::Matrix<DataType, dim, 1> GEntryType;
-        Tiny::Matrix<GEntryType, max_velo_dofs, max_pres_dofs> local_matrix;
+        Tiny::Matrix<GEntryType, max_test_dofs, max_trial_dofs> local_matrix;
 
         // create cubature rule
         typename AsmTraits::CubatureRuleType cubature_rule(Cubature::ctor_factory, cubature_factory);
@@ -129,7 +129,7 @@ namespace FEAT
         // first of all, check whether we need to assemble the matrix structures
         if(matrix_g.empty())
         {
-          Adjacency::Graph graph_g(SymbolicAssembler::assemble_graph_std2(space_velo, space_pres));
+          Adjacency::Graph graph_g(SymbolicAssembler::assemble_graph_std2(test_space, trial_space));
           matrix_g = MatrixG(graph_g);
         }
         matrix_g.format();
@@ -144,12 +144,12 @@ namespace FEAT
           trafo_eval.prepare(cell);
 
           // prepare space evaluators
-          velo_eval.prepare(trafo_eval);
-          pres_eval.prepare(trafo_eval);
+          test_eval.prepare(trafo_eval);
+          trial_eval.prepare(trafo_eval);
 
           // fetch number of local dofs
-          const int num_loc_velo_dofs = velo_eval.get_num_local_dofs();
-          const int num_loc_pres_dofs = pres_eval.get_num_local_dofs();
+          const int num_loc_test_dofs = test_eval.get_num_local_dofs();
+          const int num_loc_trial_dofs = trial_eval.get_num_local_dofs();
 
           // format local matrix and vector
           local_matrix.format();
@@ -161,21 +161,21 @@ namespace FEAT
             trafo_eval(trafo_data, cubature_rule.get_point(pt));
 
             // compute basis function data
-            velo_eval(velo_data, trafo_data);
-            pres_eval(pres_data, trafo_data);
+            test_eval(test_data, trafo_data);
+            trial_eval(trial_data, trafo_data);
 
             // trial function loop
-            for(int i(0); i < num_loc_velo_dofs; ++i)
+            for(int i(0); i < num_loc_test_dofs; ++i)
             {
               // test function loop
-              for(int j(0); j < num_loc_pres_dofs; ++j)
+              for(int j(0); j < num_loc_trial_dofs; ++j)
               {
-                const DataType pv = trafo_data.jac_det * cubature_rule.get_weight(pt) * velo_data.phi[i].value;
+                const DataType pv = trafo_data.jac_det * cubature_rule.get_weight(pt) * test_data.phi[i].value;
 
                 // dimension loop
                 for(int k(0); k < dim; ++k)
                 {
-                  local_matrix[i][j][k][0] += pres_data.phi[j].grad[k] * pv;
+                  local_matrix[i][j][k][0] += trial_data.phi[j].grad[k] * pv;
                 }
 
                 // continue with next trial function
@@ -186,20 +186,20 @@ namespace FEAT
           }
 
           // finish evaluators
-          pres_eval.finish();
-          velo_eval.finish();
+          trial_eval.finish();
+          test_eval.finish();
           trafo_eval.finish();
 
           // initialise dof-mappings
-          velo_dof_mapping.prepare(cell);
-          pres_dof_mapping.prepare(cell);
+          test_dof_mapping.prepare(cell);
+          trial_dof_mapping.prepare(cell);
 
           // scatter into matrix
-          scatter_g(local_matrix, velo_dof_mapping, pres_dof_mapping, scale);
+          scatter_g(local_matrix, test_dof_mapping, trial_dof_mapping, scale);
 
           // finish dof mapping
-          pres_dof_mapping.finish();
-          velo_dof_mapping.finish();
+          trial_dof_mapping.finish();
+          test_dof_mapping.finish();
 
           // continue with next cell
         }
@@ -217,10 +217,10 @@ namespace FEAT
        * \tparam dim_
        * The world dimension, which is also the block size
        *
-       * \tparam SpaceVelo_
+       * \tparam SpaceTest_
        * The space for \f$ \phi \f$
        *
-       * \tparam SpacePres_
+       * \tparam SpaceTrial_
        * The space for \f$ \psi \f$
        *
        * \tparam CubatureFactory_
@@ -232,10 +232,10 @@ namespace FEAT
        * \param[in] vec_in
        * The vector we apply the bilinear form to
        *
-       * \param[in] space_velo
+       * \param[in] test_space
        * The space for \f$ \phi \f$
        *
-       * \param[in] space_pres
+       * \param[in] trial_space
        * The space for \f$ \psi \f$
        *
        * \param[in] cubature_factory
@@ -245,13 +245,13 @@ namespace FEAT
        * Scaling parameter \f$ \alpha \f$
        */
       template<typename DT_, typename IT_, int dim_,
-        typename SpaceVelo_, typename SpacePres_,
+        typename SpaceTest_, typename SpaceTrial_,
         typename CubatureFactory_>
       static void assemble(
         LAFEM::DenseVectorBlocked<Mem::Main, DT_, IT_, dim_>& vec_asm,
         const LAFEM::DenseVector<Mem::Main, DT_, IT_>& vec_in,
-        const SpaceVelo_& space_velo,
-        const SpacePres_& space_pres,
+        const SpaceTest_& test_space,
+        const SpaceTrial_& trial_space,
         const CubatureFactory_& cubature_factory,
         const DT_ scale = DT_(1)
         )
@@ -261,11 +261,11 @@ namespace FEAT
         static constexpr int dim = dim_;
 
         // ensure that the matrices have the correct dimensions
-        static_assert(SpaceVelo_::shape_dim == dim, "invalid matrix block sizes");
+        static_assert(SpaceTest_::shape_dim == dim, "invalid matrix block sizes");
 
-        // make sure that velocity and pressure have the same trafo
-        if((&space_velo.get_trafo()) != (&space_pres.get_trafo()))
-          throw InternalError("Velocity and Pressure spaces must be defined on the same trafo!");
+        // make sure that trial and test spaces have the same trafo
+        if((&test_space.get_trafo()) != (&trial_space.get_trafo()))
+          throw InternalError("Trial and test spaces must be defined on the same trafo!");
 
         typedef LAFEM::DenseVectorBlocked<Mem::Main, DataType, IndexType, dim> VectorAsm;
         typedef LAFEM::DenseVector<Mem::Main, DataType, IndexType> VectorIn;
@@ -273,47 +273,47 @@ namespace FEAT
         // assembly traits
         typedef AsmTraits2<
           DataType,
-          SpaceVelo_,
-          SpacePres_,
+          SpaceTest_,
+          SpaceTrial_,
           TrafoTags::jac_det,
-          SpaceTags::grad,
-          SpaceTags::value> AsmTraits;
+          SpaceTags::value,
+          SpaceTags::grad> AsmTraits;
 
         // fetch the trafo
-        const typename AsmTraits::TrafoType& trafo = space_velo.get_trafo();
+        const typename AsmTraits::TrafoType& trafo = test_space.get_trafo();
 
         // create a trafo evaluator
         typename AsmTraits::TrafoEvaluator trafo_eval(trafo);
 
         // create space evaluators
-        typename AsmTraits::TestEvaluator velo_eval(space_velo);
-        typename AsmTraits::TrialEvaluator pres_eval(space_pres);
+        typename AsmTraits::TestEvaluator test_eval(test_space);
+        typename AsmTraits::TrialEvaluator trial_eval(trial_space);
 
         // create dof-mappings
-        typename AsmTraits::TestDofMapping velo_dof_mapping(space_velo);
-        typename AsmTraits::TrialDofMapping pres_dof_mapping(space_pres);
+        typename AsmTraits::TestDofMapping test_dof_mapping(test_space);
+        typename AsmTraits::TrialDofMapping trial_dof_mapping(trial_space);
 
         // create trafo evaluation data
         typename AsmTraits::TrafoEvalData trafo_data;
 
         // create space evaluation data
-        typename AsmTraits::TestEvalData velo_data;
-        typename AsmTraits::TrialEvalData pres_data;
+        typename AsmTraits::TestEvalData test_data;
+        typename AsmTraits::TrialEvalData trial_data;
 
         // maximum number of dofs
-        static constexpr int max_velo_dofs = AsmTraits::max_local_test_dofs;
-        static constexpr int max_pres_dofs = AsmTraits::max_local_trial_dofs;
+        static constexpr int max_test_dofs = AsmTraits::max_local_test_dofs;
+        static constexpr int max_trial_dofs = AsmTraits::max_local_trial_dofs;
 
         // create local vector data
         typedef Tiny::Vector<DataType, dim> VectorValue;
-        typedef Tiny::Vector<VectorValue, max_velo_dofs> LocalVectorType;
+        typedef Tiny::Vector<VectorValue, max_test_dofs> LocalVectorType;
         LocalVectorType local_vector;
 
-        typedef Tiny::Vector<DataType, max_pres_dofs> LocalVecInType;
+        typedef Tiny::Vector<DataType, max_trial_dofs> LocalVecInType;
         LocalVecInType local_vec_in_dofs;
 
-        // our local velocity value
-        Tiny::Vector<DataType, dim> loc_grad_p;
+        // our local trial function gradient
+        Tiny::Vector<DataType, dim> loc_grad_trial;
 
         // create cubature rule
         typename AsmTraits::CubatureRuleType cubature_rule(Cubature::ctor_factory, cubature_factory);
@@ -330,19 +330,19 @@ namespace FEAT
           trafo_eval.prepare(cell);
 
           // prepare space evaluators
-          velo_eval.prepare(trafo_eval);
-          pres_eval.prepare(trafo_eval);
+          test_eval.prepare(trafo_eval);
+          trial_eval.prepare(trafo_eval);
 
-          // Initialise pressure DoF mapping
-          pres_dof_mapping.prepare(cell);
+          // Initialise trial DoF mapping
+          trial_dof_mapping.prepare(cell);
 
           // Gather our local input DoF
           local_vec_in_dofs.format();
-          gather_vec_in(local_vec_in_dofs, pres_dof_mapping);
+          gather_vec_in(local_vec_in_dofs, trial_dof_mapping);
 
           // fetch number of local dofs
-          const int num_loc_velo_dofs = velo_eval.get_num_local_dofs();
-          const int num_loc_pres_dofs = pres_eval.get_num_local_dofs();
+          const int num_loc_test_dofs = test_eval.get_num_local_dofs();
+          const int num_loc_trial_dofs = trial_eval.get_num_local_dofs();
 
           // format local matrix and vector
           local_vector.format();
@@ -354,49 +354,49 @@ namespace FEAT
             trafo_eval(trafo_data, cubature_rule.get_point(pt));
 
             // compute basis function data
-            velo_eval(velo_data, trafo_data);
-            pres_eval(pres_data, trafo_data);
+            test_eval(test_data, trafo_data);
+            trial_eval(trial_data, trafo_data);
 
-            // evaluate pressure gradient
-            loc_grad_p.format();
-            for(int i(0); i < num_loc_pres_dofs; ++i)
+            // evaluate trial function gradient
+            loc_grad_trial.format();
+            for(int i(0); i < num_loc_trial_dofs; ++i)
             {
               for(int k(0); k < dim; ++k)
               {
-                loc_grad_p[k] += local_vec_in_dofs[i] * pres_data.phi[i].grad[k];
+                loc_grad_trial[k] += local_vec_in_dofs[i] * trial_data.phi[i].grad[k];
               }
             }
 
             // test function loop
-            for(int i(0); i < num_loc_velo_dofs; ++i)
+            for(int i(0); i < num_loc_test_dofs; ++i)
             {
-              const DataType v(trafo_data.jac_det * cubature_rule.get_weight(pt) * velo_data.phi[i].value);
+              const DataType v(trafo_data.jac_det * cubature_rule.get_weight(pt) * test_data.phi[i].value);
 
               // dimension loop
               for(int k(0); k < dim; ++k)
               {
-                //local_matrix[i][j][k][0] += velo_data.phi[i].grad[k] * pv;
+                //local_matrix[i][j][k][0] += test_data.phi[i].grad[k] * pv;
                 // update vector
-                local_vector[i][k] += v * loc_grad_p[k];
+                local_vector[i][k] += v * loc_grad_trial[k];
               }
             } // continue with next test function
 
           }// continue with next cubature point
 
           // finish evaluators
-          pres_eval.finish();
-          velo_eval.finish();
+          trial_eval.finish();
+          test_eval.finish();
           trafo_eval.finish();
 
-          // Initialise velocity DoF mapping
-          velo_dof_mapping.prepare(cell);
+          // Initialise test DoF mapping
+          test_dof_mapping.prepare(cell);
 
           // scatter into vector
-          scatter_vector(local_vector, velo_dof_mapping, scale);
+          scatter_vector(local_vector, test_dof_mapping, scale);
 
           // Finish all DoF mappings
-          pres_dof_mapping.finish();
-          velo_dof_mapping.finish();
+          trial_dof_mapping.finish();
+          test_dof_mapping.finish();
 
         } // continue with next cell
       }
