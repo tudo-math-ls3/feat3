0dadac80b2c55657e8930106ef3f58c3d108889b
Peter Zajac <pzajac@math.tu-dortmund.de>
2018-10-18 13:48:48 +0200

    moved debug tools to new 'area51' directory
    This new 'area51' directory acts as a collection of all types of applications
    and tools, which have been written to act as a development and testing ground
    during the development and implementation phase of some complex new feature
    and which may come in handy if that particular feature has to be enhanced,
    reverified, bugfixed or somehow else modified in future.
    
    Up to now, this type of application/tool was stored in the 'tools' directory,
    usually prefixed by 'dbg_', which now all have been moved to the 'area51'.
    
    This commit also adds two new debug apps, which have been used in the recent
    implementation of the directed mesh width computation (dbg_mesh_width) and
    the streamline diffusion (dbg_streamdiff).
    
    Signed-off-by: Peter Zajac <pzajac@math.tu-dortmund.de>

---
+++ area51/dbg_mesh_width.cpp
@@ -0,0 +1,209 @@
+#include <kernel/geometry/common_factories.hpp>
+#include <kernel/geometry/export_vtk.hpp>
+#include <kernel/geometry/mesh_atlas.hpp>
+#include <kernel/geometry/mesh_node.hpp>
+#include <kernel/geometry/mesh_part.hpp>
+#include <kernel/geometry/mesh_file_reader.hpp>
+#include <kernel/trafo/standard/mapping.hpp>
+#include <kernel/util/simple_arg_parser.hpp>
+#include <kernel/util/runtime.hpp>
+
+using namespace FEAT;
+using namespace FEAT::Geometry;
+
+String get_file_title(const String& filename)
+{
+  // find last slash
+  std::size_t p = filename.find_last_of("\\/");
+  if(p == filename.npos)
+    p = 0;
+  else
+    ++p;
+
+  // fine last dot
+  std::size_t q = filename.find_last_of(".");
+  if(q == filename.npos)
+    return filename.substr(p);
+  else
+    return filename.substr(p, q-p);
+}
+
+template<typename Mesh_>
+int run_xml(SimpleArgParser& args, Geometry::MeshFileReader& mesh_reader, const String& filename)
+{
+  static constexpr int world_dim = Mesh_::world_dim;
+
+  Tiny::Vector<double, 3> ray_3(0.0);
+  ray_3[0] = 1.0;
+
+  // parse levels
+  Index lvl_min(0);
+  Index lvl_max(0);
+  args.parse("level", lvl_max, lvl_min);
+  args.parse("ray", ray_3[0], ray_3[1], ray_3[2]);
+
+  Tiny::Vector<double, world_dim> ray(ray_3.template size_cast<world_dim>());
+  ray.normalise();
+
+  std::cout << "Ray = [ " << stringify(ray[0]);
+  for(int i(1); i < world_dim; ++i)
+    std::cout << " , " << stringify(ray[i]);
+  std::cout << " ]" << std::endl;
+
+  // create an empty atlas and a root mesh node
+  Geometry::MeshAtlas<Mesh_>* atlas = new Geometry::MeshAtlas<Mesh_>();
+  Geometry::RootMeshNode<Mesh_>* node = new Geometry::RootMeshNode<Mesh_>(nullptr, atlas);
+
+  // try to parse the mesh file
+#ifndef DEBUG
+  try
+#endif
+  {
+    std::cout << "Parsing mesh files..." << std::endl;
+    // Now parse the mesh file
+    mesh_reader.parse(*node, *atlas, nullptr);
+  }
+#ifndef DEBUG
+  catch(std::exception& exc)
+  {
+    std::cerr << "ERROR: " << exc.what() << std::endl;
+    return 1;
+  }
+  catch(...)
+  {
+    std::cerr << "ERROR: unknown exception" << std::endl;
+    return 1;
+  }
+#endif
+
+  // adapt coarse mesh
+  node->adapt();
+
+
+  // refine
+  for(Index lvl(0); lvl <= lvl_max; ++lvl)
+  {
+    if(lvl > 0)
+    {
+      std::cout << "Refining up to level " << lvl << "..." << std::endl;
+      auto* old = node;
+      node = old->refine();
+      delete old;
+    }
+
+    if(lvl < lvl_min)
+      continue;
+
+    // get our mesh
+    Mesh_& mesh = *node->get_mesh();
+
+    // create a trafo for our mesh
+    Trafo::Standard::Mapping<Mesh_> trafo(mesh);
+    typename decltype(trafo)::template Evaluator<>::Type trafo_eval(trafo);
+
+    // Create a VTK exporter for our mesh
+    FEAT::String vtkname = filename + "." + stringify(lvl);
+    std::cout << "Writing file '" << vtkname << ".vtu'..." << std::endl;
+    Geometry::ExportVTK<Mesh_> exporter(mesh);
+
+    std::vector<double> volume(mesh.get_num_elements(), 0.0);
+    std::vector<double> width(mesh.get_num_elements(), 0.0);
+
+    for(Index i(0); i < mesh.get_num_elements(); ++i)
+    {
+      trafo_eval.prepare(i);
+      volume[i] = trafo_eval.volume();
+      //volume[i] = Math::pow(trafo_eval.volume(), 1.0 / double(world_dim));
+      width[i] = trafo_eval.width_directed(ray);
+      trafo_eval.finish();
+    }
+
+    exporter.add_cell_scalar("volume", volume.data());
+    exporter.add_cell_scalar("width", width.data());
+
+    exporter.write(vtkname);
+  }
+
+  delete node;
+  delete atlas;
+
+  return 0;
+}
+
+int run(int argc, char* argv[])
+{
+  // This is the list of all supported meshes that could appear in the mesh file
+  typedef Geometry::ConformalMesh<Shape::Simplex<2>, 2, Real> S2M2D;
+  typedef Geometry::ConformalMesh<Shape::Simplex<3>, 3, Real> S3M3D;
+  typedef Geometry::ConformalMesh<Shape::Hypercube<2>, 2, Real> H2M2D;
+  typedef Geometry::ConformalMesh<Shape::Hypercube<3>, 3, Real> H3M3D;
+
+  SimpleArgParser args(argc, argv);
+
+  // need help?
+  if((argc < 2) || (args.check("help") > -1))
+  {
+    return 0;
+  }
+
+  args.support("mesh");
+  args.support("vtk");
+  args.support("level");
+  args.support("ray");
+
+  // check for unsupported options
+  auto unsupported = args.query_unsupported();
+  if( !unsupported.empty() )
+  {
+    // print all unsupported options to cerr
+    for(auto it = unsupported.begin(); it != unsupported.end(); ++it)
+      std::cerr << "ERROR: unsupported option '--" << (*it).second << "'" << std::endl;
+
+    return 1;
+  }
+
+  int num_mesh_files = args.check("mesh");
+  if(num_mesh_files < 1)
+  {
+    std::cerr << "ERROR: You have to specify at least one meshfile with --mesh <files...>" << std::endl;
+    return 1;
+  }
+
+  // get our filename deque
+  auto mpars = args.query("mesh");
+  XASSERT(mpars != nullptr);
+  const std::deque<String>& filenames = mpars->second;
+
+  // our VTK output filename
+  String vtk_name = get_file_title(filenames.back());
+  args.parse("vtk", vtk_name);
+
+  // create an empty mesh file reader
+  Geometry::MeshFileReader mesh_reader;
+  mesh_reader.add_mesh_files(filenames);
+
+  // read root markup
+  mesh_reader.read_root_markup();
+
+  // get mesh type
+  const String mtype = mesh_reader.get_meshtype_string();
+
+  std::cout << "Mesh Type: " << mtype << std::endl;
+
+  if(mtype == "conformal:hypercube:2:2") return run_xml<H2M2D>(args, mesh_reader, vtk_name);
+  if(mtype == "conformal:hypercube:3:3") return run_xml<H3M3D>(args, mesh_reader, vtk_name);
+  if(mtype == "conformal:simplex:2:2") return run_xml<S2M2D>(args, mesh_reader, vtk_name);
+  if(mtype == "conformal:simplex:3:3") return run_xml<S3M3D>(args, mesh_reader, vtk_name);
+
+  std::cout << "ERROR: unsupported mesh type!" << std::endl;
+
+  return 1;
+}
+
+int main(int argc, char* argv[])
+{
+  Runtime::initialise(argc, argv);
+  int ret = run(argc, argv);
+  Runtime::finalise();
+  return ret;
+}
---
+++ area51/dbg_streamdiff.cpp
@@ -0,0 +1,329 @@
+#include <kernel/util/string.hpp>
+#include <kernel/util/runtime.hpp>
+#include <kernel/util/simple_arg_parser.hpp>
+#include <kernel/geometry/boundary_factory.hpp>
+#include <kernel/geometry/conformal_mesh.hpp>
+#include <kernel/geometry/common_factories.hpp>
+#include <kernel/geometry/export_vtk.hpp>
+#include <kernel/geometry/mesh_part.hpp>
+#include <kernel/trafo/standard/mapping.hpp>
+#include <kernel/space/lagrange1/element.hpp>
+#include <kernel/space/lagrange2/element.hpp>
+#include <kernel/cubature/dynamic_factory.hpp>
+#include <kernel/analytic/common.hpp>
+#include <kernel/assembly/symbolic_assembler.hpp>
+#include <kernel/assembly/common_operators.hpp>
+#include <kernel/assembly/common_functionals.hpp>
+#include <kernel/assembly/bilinear_operator_assembler.hpp>
+#include <kernel/assembly/linear_functional_assembler.hpp>
+#include <kernel/assembly/unit_filter_assembler.hpp>
+#include <kernel/assembly/error_computer.hpp>
+#include <kernel/assembly/discrete_projector.hpp>
+#include <kernel/assembly/interpolator.hpp>
+#include <kernel/assembly/burgers_assembler.hpp>
+#include <kernel/lafem/dense_vector.hpp>
+#include <kernel/lafem/dense_vector_blocked.hpp>
+#include <kernel/lafem/sparse_matrix_csr.hpp>
+#include <kernel/lafem/unit_filter.hpp>
+#include <kernel/solver/umfpack.hpp>
+
+namespace Andicore
+{
+  using namespace FEAT;
+
+  //typedef Shape::Hypercube<1> ShapeType;  // 1D
+  //typedef Shape::Triangle ShapeType;      // 2D, same as Shape::Simplex<2>
+  typedef Shape::Quadrilateral ShapeType;   // 2D, same as Shape::Hypercube<2>
+  //typedef Shape::Tetrahedron ShapeType;   // 3D, same as Shape::Simplex<3>
+  //typedef Shape::Hexahedron ShapeType;    // 3D, same as Shape::Hypercube<3>
+
+  static constexpr int dim = ShapeType::dimension;
+
+  typedef Geometry::ConformalMesh<ShapeType> MeshType;
+  typedef Geometry::MeshPart<MeshType> MeshPartType;
+  typedef Trafo::Standard::Mapping<MeshType> TrafoType;
+  typedef Space::Lagrange1::Element<TrafoType> SpaceType;
+  //typedef Space::Lagrange2::Element<TrafoType> SpaceType;
+
+  typedef Mem::Main MemType;
+  typedef double DataType;
+  typedef Index IndexType;
+
+  typedef LAFEM::DenseVector<MemType, DataType, IndexType> VectorType;
+  typedef LAFEM::DenseVectorBlocked<MemType, DataType, IndexType, dim> BlockedVectorType;
+  typedef LAFEM::SparseMatrixCSR<MemType, DataType, IndexType> MatrixType;
+  typedef LAFEM::UnitFilter<MemType, DataType, IndexType> FilterType;
+
+
+  template<typename Mesh_>
+  void disturb_mesh(Mesh_& mesh, double dsh)
+  {
+    auto& vtx = mesh.get_vertex_set();
+
+    Random rng;
+
+    for(Index i(0); i < vtx.get_num_vertices(); ++i)
+    {
+      // generate rotation matrix
+      double t(0);
+      rng >> t;
+      t *= 2.0 * Math::pi<double>();
+      if((vtx[i][0] > 0.001) && (vtx[i][0] < 0.999))
+        vtx[i][0] += dsh * Math::cos(t);
+      if((vtx[i][1] > 0.001) && (vtx[i][1] < 0.999))
+        vtx[i][1] += dsh * Math::sin(t);
+    }
+  }
+
+  class ConvectFunction :
+    public Analytic::Function
+  {
+  public:
+    static constexpr int domain_dim = 2;
+    typedef Analytic::Image::Vector<2> ImageType;
+
+    static constexpr bool can_value = true;
+    static constexpr bool can_grad = false;
+    static constexpr bool can_hess = false;
+
+    template<typename EvalTraits_>
+    class Evaluator :
+      public Analytic::Function::Evaluator<EvalTraits_>
+    {
+    public:
+      typedef typename EvalTraits_::DataType DataType;
+      typedef typename EvalTraits_::PointType PointType;
+      typedef typename EvalTraits_::ValueType ValueType;
+      typedef typename EvalTraits_::HessianType HessianType;
+
+    public:
+      const DataType _pi2;
+
+      explicit Evaluator(const ConvectFunction&) :
+        _pi2(2.0*Math::pi<DataType>())
+      {
+      }
+
+      ValueType value(const PointType& p)
+      {
+        ValueType conv;
+        conv[0] =  (1.0 - Math::cos(_pi2*p[0])) * Math::sin(_pi2*p[1]);
+        conv[1] = -(1.0 - Math::cos(_pi2*p[1])) * Math::sin(_pi2*p[0]);
+        return conv;
+      }
+    };
+  };
+
+  template<typename SolFunction_>
+  class AndicoreFunctional :
+    public Assembly::LinearFunctional
+  {
+  public:
+    const DataType _nu, _beta, _theta;
+    const SolFunction_& _sol_func;
+    explicit AndicoreFunctional( const SolFunction_& sol_func, const DataType nu, const DataType beta, const DataType theta) :
+      _nu(nu),
+      _beta(beta),
+      _theta(theta),
+      _sol_func(sol_func)
+    {
+    }
+
+    static constexpr TrafoTags trafo_config = TrafoTags::img_point;
+    static constexpr SpaceTags test_config = SpaceTags::value;
+
+    template<typename AsmTraits_>
+    class Evaluator :
+      public Assembly::LinearFunctional::Evaluator<AsmTraits_>
+    {
+    public:
+      typedef typename AsmTraits_::TrafoData TrafoData;
+      typedef typename AsmTraits_::TestBasisData TestBasisData;
+      typedef typename AsmTraits_::TrafoEvaluator TrafoEvaluator;
+      typedef typename AsmTraits_::DataType DataType;
+      typedef Analytic::EvalTraits<DataType, SolFunction_> AnalyticEvalTraits;
+      typedef typename AnalyticEvalTraits::ValueType ValueType;
+      typedef typename SolFunction_::template Evaluator<AnalyticEvalTraits> SolEvaluator;
+
+    protected:
+      const DataType _pi2, _nu, _beta, _theta;
+      SolEvaluator _sol_eval;
+      ValueType _force_value;
+
+    public:
+      explicit Evaluator(const AndicoreFunctional& functional) :
+        _pi2(2.0*Math::pi<DataType>()),
+        _nu(functional._nu),
+        _beta(functional._beta),
+        _theta(functional._theta),
+        _sol_eval(functional._sol_func),
+        _force_value(DataType(0))
+      {
+      }
+
+      void set_point(const TrafoData& tau)
+      {
+        typename AnalyticEvalTraits::ValueType    value = _sol_eval.value(tau.img_point);
+        typename AnalyticEvalTraits::GradientType grad  = _sol_eval.gradient(tau.img_point);
+        typename AnalyticEvalTraits::HessianType  hess  = _sol_eval.hessian(tau.img_point);
+
+        // evaluate convection
+        typename AnalyticEvalTraits::GradientType conv;
+        conv[0] =  (1.0 - Math::cos(_pi2*tau.img_point[0])) * Math::sin(_pi2*tau.img_point[1]);
+        conv[1] = -(1.0 - Math::cos(_pi2*tau.img_point[1])) * Math::sin(_pi2*tau.img_point[0]);
+
+        _force_value = -_nu * hess.trace() + _beta * dot(conv, grad) + _theta*value;
+      }
+
+      ValueType operator()(const TestBasisData& psi) const
+      {
+        return _force_value * psi.value;
+      }
+    }; // class AndicoreFunctional<...>::Evaluator<...>
+  }; // class AndicoreFunctional<...>
+
+
+  void main(int argc, char** argv)
+  {
+    SimpleArgParser args(argc, argv);
+
+    args.support("level");
+    args.support("nu");
+    args.support("beta");
+    args.support("theta");
+    args.support("upsam");
+
+    DataType nu = 1E-5;   // diffusion
+    DataType beta = 1.0;  // convection
+    DataType theta = 0.0; // reaction
+    DataType upsam = 0.0; // stabilisation
+    Index level = 5;
+
+    args.parse("level", level);
+    args.parse("nu", nu);
+    args.parse("beta", beta);
+    args.parse("theta", theta);
+    args.parse("upsam", upsam);
+
+    std::cout << String("Level").pad_back(20, '.') << ": " << level << std::endl;
+    std::cout << String("nu").pad_back(20, '.') << ": " << nu << std::endl;
+    std::cout << String("beta").pad_back(20, '.') << ": " << beta << std::endl;
+    std::cout << String("theta").pad_back(20, '.') << ": " << theta << std::endl;
+    std::cout << String("upsam").pad_back(20, '.') << ": " << upsam << std::endl;
+
+    // ********************************************************************************************
+
+    Cubature::DynamicFactory cubature(String("auto-degree:") + stringify(2*(SpaceType::local_degree)+1));
+
+    Geometry::RefinedUnitCubeFactory<MeshType> mesh_factory(level);
+    MeshType mesh(mesh_factory);
+
+    // disturb mesh
+    disturb_mesh(mesh, 0.15 / double(1 << level));
+
+    Geometry::BoundaryFactory<MeshType> boundary_factory(mesh);
+    MeshPartType boundary(boundary_factory);
+
+    TrafoType trafo(mesh);
+    SpaceType space(trafo);
+
+
+
+    ConvectFunction conv_function;
+    Analytic::Common::SineBubbleFunction<ShapeType::dimension> sol_function;
+    AndicoreFunctional<decltype(sol_function)> rhs_functional(sol_function, nu, beta, theta);
+
+
+    MatrixType matrix;
+
+    Assembly::SymbolicAssembler::assemble_matrix_std1(matrix, space);
+
+    VectorType vec_sol_1 = matrix.create_vector_r();
+    VectorType vec_sol_2 = matrix.create_vector_r();
+    VectorType vec_rhs = matrix.create_vector_l();
+
+    BlockedVectorType vec_conv(matrix.rows());
+    Assembly::Interpolator::project(vec_conv, conv_function, space);
+
+    vec_sol_1.format();
+    vec_sol_2.format();
+    vec_rhs.format();
+
+    Assembly::LinearFunctionalAssembler::assemble_vector(vec_rhs, rhs_functional, space, cubature);
+
+    FilterType filter;
+    Assembly::UnitFilterAssembler<MeshType> unit_asm;
+    unit_asm.add_mesh_part(boundary);
+    unit_asm.assemble(filter, space);
+
+    filter.filter_rhs(vec_rhs);
+    filter.filter_sol(vec_sol_1);
+    filter.filter_sol(vec_sol_2);
+
+
+    Assembly::BurgersAssembler<DataType, IndexType, 2> burgers;
+    burgers.nu = nu;
+    burgers.beta = beta;
+    burgers.theta = theta;
+
+    matrix.format();
+    burgers.assemble_scalar_matrix(matrix, vec_conv, space, cubature);
+    filter.filter_mat(matrix);
+
+#ifdef FEAT_HAVE_UMFPACK
+
+    auto solver = Solver::new_umfpack(matrix);
+
+    solver->init();
+    Solver::solve(*solver, vec_sol_1, vec_rhs, matrix, filter);
+    solver->done_numeric();
+
+    burgers.sd_delta = upsam;
+    burgers.sd_nu = nu;
+    burgers.set_sd_v_norm(vec_conv);
+    matrix.format();
+    burgers.assemble_scalar_matrix(matrix, vec_conv, space, cubature);
+    filter.filter_mat(matrix);
+
+    solver->init_numeric();
+    Solver::solve(*solver, vec_sol_2, vec_rhs, matrix, filter);
+
+    solver->done();
+
+    Assembly::ScalarErrorInfo<DataType> errors_1 = Assembly::ScalarErrorComputer<1>::
+      compute(vec_sol_1, sol_function, space, cubature);
+    Assembly::ScalarErrorInfo<DataType> errors_2 = Assembly::ScalarErrorComputer<1>::
+      compute(vec_sol_2, sol_function, space, cubature);
+
+    std::cout << "Unstabilised:" << std::endl;
+    std::cout << errors_1 << std::endl;
+
+    std::cout << "Streamline Diffusion:" << std::endl;
+    std::cout << errors_2 << std::endl;
+
+    String vtk_name(String("./andicore-stabil-lvl") + stringify(level));
+
+    std::cout << "Writing VTK file '" << vtk_name << ".vtu'..." << std::endl;
+
+    Geometry::ExportVTK<MeshType> exporter(mesh);
+    exporter.add_vertex_vector("conv", vec_conv);
+    exporter.add_vertex_scalar("sol_1", vec_sol_1.elements());
+    exporter.add_vertex_scalar("sol_2", vec_sol_2.elements());
+    exporter.add_vertex_scalar("rhs", vec_rhs.elements());
+    exporter.write(vtk_name);
+
+    std::cout << "Finished!" << std::endl;
+
+#else // no UMFPACK
+    std::cout << "You need to compile with UMFPACK support." << std::endl;
+#endif // FEAT_HAVE_UMFPACK
+  }
+} // namespace Andicore
+
+// Here's our main function
+int main(int argc, char* argv[])
+{
+  FEAT::Runtime::initialise(argc, argv);
+  Andicore::main(argc, argv);
+  return FEAT::Runtime::finalise();
+}
