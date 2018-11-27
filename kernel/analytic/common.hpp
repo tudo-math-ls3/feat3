a773559af19db2b49e1a9bf5544860cfde48187b
Dirk Ribbrock <dirk.ribbrock@math.tu-dortmund.de>
2018-11-27 16:33:44 +0100

    taming goldstein price function
    Signed-off-by: Dirk Ribbrock <dirk.ribbrock@math.tu-dortmund.de>

--- kernel/analytic/common.hpp
+++ kernel/analytic/common.hpp
@@ -1671,7 +1671,7 @@ namespace FEAT
 
       /// \cond internal
       /**
-       * \brief Goldstein\/Price function
+       * \brief Goldstein-Price function
        *
        * \tparam DataType_
        * Floating point precision
@@ -1740,7 +1740,7 @@ namespace FEAT
       /// \endcond
 
       /**
-       * \brief Goldstein\/Price function
+       * \brief Goldstein-Price function
        *
        * \tparam DataType_
        * Floating point precision
