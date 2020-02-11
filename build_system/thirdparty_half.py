a4e1506e8fe1ca19bffd56c7a548ed0ecbbd32e2
Dirk Ribbrock <dirk.ribbrock@math.tu-dortmund.de>
2020-02-11 11:22:21 +0100

    Update half thirdparty lib to 2.1.0

--- build_system/thirdparty_half.py
+++ build_system/thirdparty_half.py
@@ -12,8 +12,8 @@ class HALF(ThirdpartyPackage):
   def __init__(self,trunk_dirname):
     self.names = ["half"]
     self.dirname = "half"
-    self.filename = "half-1.12.0.zip"
-    self.url = "http://downloads.sourceforge.net/project/half/half/1.12.0/half-1.12.0.zip?r=http%3A%2F%2Fhalf.sourceforge.net%2Findex.html&ts=1455282582&use_mirror=netcologne"
+    self.filename = "half-2.1.0.zip"
+    self.url = "http://downloads.sourceforge.net/project/half/half/2.1.0/half-2.1.0.zip?r=http%3A%2F%2Fhalf.sourceforge.net%2Findex.html&ts=1581415216&use_mirror=netcologne"
     self.cmake_flags = " -DFEAT_HAVE_HALFMATH:BOOL=ON"
     self.trunk_dirname = trunk_dirname
     self.target_dirname = trunk_dirname+os.sep+self.dirname
