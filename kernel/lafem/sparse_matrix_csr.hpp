570c7cf42db8111daf1b9a0d5edcbc2441ecc334
Peter Zajac <peter.zajac@math.tu-dortmund.de>
2020-10-30 12:37:23 +0100

    implemented SparseMatrixCSR::add_mat_mat_product()
    Well, actually just copy-&-paste'd from add_double_mat_product()
    and deleted a few lines...
    
    Signed-off-by: Peter Zajac <peter.zajac@math.tu-dortmund.de>

--- kernel/lafem/sparse_matrix_csr.hpp
+++ kernel/lafem/sparse_matrix_csr.hpp
@@ -2271,6 +2271,117 @@ namespace FEAT
           }
         }
       }
+
+      /**
+       * \brief Adds a matrix-matrix product onto this matrix
+       *
+       * This function performs the following computation:
+       * \f[ X \leftarrow X + \alpha D\cdot B\f]
+       *
+       * where
+       * - \e X denotes this m-by-n matrix
+       * - \e D denotes a m-by-l matrix
+       * - \e B denotes a l-by-n matrix
+       *
+       * \attention
+       * This function assumes that the output matrix already contains the
+       * required sparsity pattern. This function will throw an exception
+       * if the sparsity pattern of the output matrix is incomplete unless
+       * \p allow_incomplete is set to \c true.
+       *
+       * \note
+       * This function currently only supports data in main memory.
+       *
+       * \param[in] d, b
+       * The left and right multiplicand matrices
+       *
+       * \param[in] alpha
+       * The scaling factor for the product
+       *
+       * \param[in] allow_incomplete
+       * Specifies whether the output matrix structure is allowed to be incomplete.
+       * If set to \c false, this function will throw an exception on incompleteness,
+       * otherwise the missing entries are ignored (dropped).
+       */
+      void add_mat_mat_product(
+        const LAFEM::SparseMatrixCSR<Mem::Main, DT_, IT_>& d,
+        const LAFEM::SparseMatrixCSR<Mem::Main, DT_, IT_>& b,
+        const DT_ alpha = DT_(1),
+        const bool allow_incomplete = false)
+      {
+        // validate matrix dimensions
+        XASSERT(this->rows() == d.rows());
+        XASSERT(d.columns() == b.rows());
+        XASSERT(b.columns() == this->columns());
+
+        // fetch matrix arrays:
+        DT_* data_x = this->val();
+        const DT_* data_d = d.val();
+        const DT_* data_b = b.val();
+        const IT_* row_ptr_x = this->row_ptr();
+        const IT_* col_idx_x = this->col_ind();
+        const IT_* row_ptr_d = d.row_ptr();
+        const IT_* col_idx_d = d.col_ind();
+        const IT_* row_ptr_b = b.row_ptr();
+        const IT_* col_idx_b = b.col_ind();
+
+        // loop over all rows of D and X, resp.
+        for(IT_ i(0); i < IT_(this->rows()); ++i)
+        {
+          // loop over all non-zeros D_ik in row i of D
+          for(IT_ ik(row_ptr_d[i]); ik  < row_ptr_d[i+1]; ++ik)
+          {
+            // get column index k
+            const IT_ k = col_idx_d[ik];
+
+            // pre-compute factor (alpha * D_ik)
+            const DT_ omega = alpha *  data_d[ik];
+
+            // loop over all non-zeros B_kj in row j of B and
+            // loop over all non-zeros X_ij in row i of X and
+            // perform a "sparse axpy" of B_l onto X_i, i.e.:
+            //   X_i. += (alpha * D_ik) * B_k.
+            IT_ ij = row_ptr_x[i];
+            IT_ kj = row_ptr_b[k];
+            while(kj < row_ptr_b[k+1])
+            {
+              if(ij >= row_ptr_x[i+1])
+              {
+                // we have reached the end of row X_i, but there is at least
+                // one entry in row B_l left, so the pattern of X is incomplete
+                // We let the caller decide whether this is a valid case or not:
+                if(allow_incomplete)
+                  break; // continue with next row
+                else
+                  XABORTM("Incomplete output matrix structure");
+              }
+              else if(col_idx_x[ij] == col_idx_b[kj])
+              {
+                // okay: B_kj contributes to X_ij
+                data_x[ij] += omega * data_b[kj];
+                ++ij;
+                ++kj;
+              }
+              else if(col_idx_x[ij] < col_idx_b[kj])
+              {
+                // entry X_ij exists, but B_kj is missing:
+                // this is a perfectly valid case, so continue with the next non-zero of X_i
+                ++ij;
+              }
+              else //if(col_idx_x[ij] > col_idx_b[kj])
+              {
+                // If we come out here, then the sparsity pattern of X is incomplete:
+                // B_kj is meant to be added onto X_ij, but the entry X_ij is missing
+                // We let the caller decide whether this is a valid case or not:
+                if(allow_incomplete)
+                  ++kj;
+                else
+                  XABORTM("Incomplete output matrix structure");
+              }
+            }
+          }
+        }
+      }
       ///@}
 
       /// \copydoc lump_rows()
