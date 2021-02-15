5f03593770f6c90ed5409fbc77474862ef89909e
Peter Zajac <peter.zajac@math.tu-dortmund.de>
2021-02-15 11:08:06 +0100

    added '2d_tria_rect.py' mesh generator Python script
    This script is based on the existing '2d_quad_rect.py' script and
    can be used to generate structured 2D triangular meshes of the form:
    
    +-----+-----+-----+-----+
    |  ./'|  ./'|  ./'|  ./'|
    |./'  |./'  |./'  |./'  |
    +-----+-----+-----+-----+
    |  ./'|  ./'|  ./'|  ./'|
    |./'  |./'  |./'  |./'  |
    +-----+-----+-----+-----+
    
    Signed-off-by: Peter Zajac <peter.zajac@math.tu-dortmund.de>

--- tools/mesh_gen/2d_quad_ring.py
+++ tools/mesh_gen/2d_quad_ring.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # FEAT3: Finite Element Analysis Toolbox, Version 3
-# Copyright (C) 2010 - 2020 by Stefan Turek & the FEAT group
+# Copyright (C) 2010 - 2021 by Stefan Turek & the FEAT group
 # FEAT3 is released under the GNU General Public License version 3,
 # see the file 'copyright.txt' in the top level directory for details.
 ################################################################################
@@ -10,7 +10,7 @@
 # bnd:i     The inner boundary ring meshpart
 # bnd:o     The outer boundary ring meshpart
 #
-# USAGE: 2d_ring_quad.py cx cy r0 r1 m n filename
+# USAGE: 2d_quad_ring.py cx cy r0 r1 m n filename
 #
 # Options:
 # cx cy     The coordinates of the ring barycentre
