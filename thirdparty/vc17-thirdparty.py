8d39436fa956cc30d7f2c575d5d9e92462b430a7
Peter Zajac <pzajac@math.tu-dortmund.de>
2023-03-24 10:42:20 +0100

    added patch to fix HYPRE 32 vs 64 bit hashing issues
    See: https://github.com/hypre-space/hypre/issues/870
    
    Signed-off-by: Peter Zajac <pzajac@math.tu-dortmund.de>

--- thirdparty/vc17-thirdparty.py
+++ thirdparty/vc17-thirdparty.py
@@ -623,6 +623,11 @@ class ThirdPartyHypre(ThirdPartyPackage):
     self.patch_file(os.path.join("src", "matrix_matrix", "HYPRE_ConvertPETScMatrixToDistributedMatrix.c"), [
       [15, '#include <gmalloc.h>', '/*#include <gmalloc.h>*/']
     ])
+    # patch 32 vs 64 bit int issues in utilities
+    self.patch_file(os.path.join("src", "utilities", "_hypre_utilities.h"), [
+      [2171, 'hypre_ulongint h64 = HYPRE_XXH_PRIME64_5 + sizeof(input);', '  hypre_ulonglongint h64 = HYPRE_XXH_PRIME64_5 + sizeof(input);'],
+      [2173, 'hypre_ulongint k1 = input;', '  hypre_ulonglongint k1 = input;']
+    ])
 
   def ready(self):
     return os.path.isfile(os.path.join("..", "lib", "hypre-seq-" + self.version + ".vc17-opt-x64.lib"))
