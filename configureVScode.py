2f6400a87561ac9501722475b11501f10fb4418b
Peter Zajac <peter.zajac@math.tu-dortmund.de>
2023-04-13 13:21:56 +0200

    updated year in copyright header
    Signed-off-by: Peter Zajac <peter.zajac@math.tu-dortmund.de>

--- configureVScode.py
+++ configureVScode.py
@@ -1,3 +1,8 @@
+# FEAT3: Finite Element Analysis Toolbox, Version 3
+# Copyright (C) 2010 - 2023 by Stefan Turek & the FEAT group
+# FEAT3 is released under the GNU General Public License version 3,
+# see the file 'copyright.txt' in the top level directory for details.
+
 import os
 import sys
 import shutil
