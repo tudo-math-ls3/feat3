8ad8d7b968e5ab50d12ad87edf3c0e3c0e06def8
Dirk Ribbrock <dirk.ribbrock@math.tu-dortmund.de>
2024-04-08 12:14:38 +0200

    fix2: ftrapuv is not known by icpx

--- build_system/configure_icx.py
+++ build_system/configure_icx.py
@@ -22,7 +22,8 @@ def configure_icx(cpu, buildid, compiler, system_host_compiler, restrict_errors)
     cxxflags += " -gcc-name=" + system_host_compiler
 
   if "debug" in buildid or "noop" in buildid:
-    cxxflags += "  -O0 -debug all -ftrapv"
+    cxxflags += "  -O0 -debug all"
+    #TODO missing ftrapuv, not know by icpx: Initializes stack local variables to an unusual value to aid error detection.
 
   elif "opt" in buildid or "fast" in buildid:
     if "lto" in buildid :
