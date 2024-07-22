537a4486b4b09c9e6af977ce946fe47c49a13feb
Peter Zajac <pzajac@math.tu-dortmund.de>
2024-07-22 15:47:39 +0200

    temporarily disabled opt mode for Intel oneAPI DPC++ compilers
    There seems to be a serious optimization bug in the new Intel
    C++ compilers, since it consistently produces incorrect results
    related to the computation of our jacobian determinants. which
    are used throughout the FEM kernels. Adding compiler hacks for
    this problem does not make any sense, so for now, opt-mode is
    disabled for ICPX and we'll see how Intel reacts to our bug
    report, see:
    
    https://community.intel.com/t5/Intel-oneAPI-DPC-C-Compiler/Optimization-compiler-bug-in-Intel-DPC-compiler-2024-and-older/m-p/1617029#
    
    Signed-off-by: Peter Zajac <pzajac@math.tu-dortmund.de>

--- build_system/configure_icx.py
+++ build_system/configure_icx.py
@@ -13,6 +13,17 @@ def configure_icx(cpu, buildid, compiler, system_host_compiler, restrict_errors)
   minor2 = int(version.split('.')[2])
   print ("Detected icx version: " + str(major) + " " + str(minor) + " " + str(minor2))
 
+  # There seems to be a serious optimization bug in the new Intel C++ compilers, since
+  # it consistently produces incorrect results related to the computation of our
+  # jacobian determinants. which are used throughout the FEM kernels. Adding compiler
+  # hacks for this problem does not make any sense, so for now, opt-mode is disabled
+  # for ICPX and we'll see how Intel reacts to our bug report, see:
+  if "opt" in buildid:
+    print("\nERROR: Intel ICPX compiler cannot be used in opt-mode due to a compiler optimization bug! See the bug report here:")
+    print("https://community.intel.com/t5/Intel-oneAPI-DPC-C-Compiler/Optimization-compiler-bug-in-Intel-DPC-compiler-2024-and-older/m-p/1617029#M3971")
+    print("\nPlease configure in debug mode or use another compiler for now!\n")
+    sys.exit(1);
+
   cxxflags = "-std=c++17 -g -Wall -Wextra -Wcheck -Wdeprecated -Wnon-virtual-dtor -Wpointer-arith -Wreturn-type -Wshadow -Wshorten-64-to-32 -Wuninitialized"
 
   if restrict_errors:
