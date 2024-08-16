68481308edfa2f09905dfdb93f917f2696bafa32
Peter Zajac <peter.zajac@math.tu-dortmund.de>
2024-08-16 13:26:37 +0200

    re-enable opt mode for Intel OneAPI C/C++ compiler
    And set the floating point model to 'precise', because icx won't properly
    check for infs or nans otherwise.
    
    Signed-off-by: Peter Zajac <peter.zajac@math.tu-dortmund.de>

--- build_system/configure_icx.py
+++ build_system/configure_icx.py
@@ -1,5 +1,5 @@
 # FEAT3: Finite Element Analysis Toolbox, Version 3
-# Copyright (C) 2010 - 2023 by Stefan Turek & the FEAT group
+# Copyright (C) 2010 - 2024 by Stefan Turek & the FEAT group
 # FEAT3 is released under the GNU General Public License version 3,
 # see the file 'copyright.txt' in the top level directory for details.
 import platform
@@ -14,18 +14,7 @@ def configure_icx(cpu, buildid, compiler, system_host_compiler, restrict_errors)
   minor2 = int(version.split('.')[2])
   print ("Detected icx version: " + str(major) + " " + str(minor) + " " + str(minor2))
 
-  # There seems to be a serious optimization bug in the new Intel C++ compilers, since
-  # it consistently produces incorrect results related to the computation of our
-  # jacobian determinants. which are used throughout the FEM kernels. Adding compiler
-  # hacks for this problem does not make any sense, so for now, opt-mode is disabled
-  # for ICPX and we'll see how Intel reacts to our bug report, see:
-  if "opt" in buildid:
-    print("\nERROR: Intel ICPX compiler cannot be used in opt-mode due to a compiler optimization bug! See the bug report here:")
-    print("https://community.intel.com/t5/Intel-oneAPI-DPC-C-Compiler/Optimization-compiler-bug-in-Intel-DPC-compiler-2024-and-older/m-p/1617029#M3971")
-    print("\nPlease configure in debug mode or use another compiler for now!\n")
-    sys.exit(1);
-
-  cxxflags = "-std=c++17 -g -Wall -Wextra -Wcheck -Wdeprecated -Wnon-virtual-dtor -Wpointer-arith -Wreturn-type -Wshadow -Wshorten-64-to-32 -Wuninitialized"
+  cxxflags = "-std=c++17 -g -Wall -Wextra -Wdeprecated -Wnon-virtual-dtor -Wpointer-arith -Wreturn-type -Wshadow -Wshorten-64-to-32 -Wuninitialized"
 
   if restrict_errors:
     cxxflags += " -diag-error-limit1"
@@ -34,15 +23,14 @@ def configure_icx(cpu, buildid, compiler, system_host_compiler, restrict_errors)
     cxxflags += " -gcc-name=" + system_host_compiler
 
   if "debug" in buildid or "nopt" in buildid:
-    cxxflags += "  -O0 -debug all"
+    cxxflags += "  -O0 -debug all -fp-model=strict"
     #TODO missing ftrapuv, not know by icpx: Initializes stack local variables to an unusual value to aid error detection.
 
   elif "opt" in buildid:
+    cxxflags += " -O3 -fp-model=precise"
+
     if "lto" in buildid :
       cxxflags += " -ipo "
-
-    if "opt" in buildid:
-      cxxflags += " -O3"
 
     if cpu == "unknown":
       # generate code for every simd unit, existing so far
