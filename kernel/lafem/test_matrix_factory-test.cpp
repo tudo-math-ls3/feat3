4d823d1fc0b7cf6bf4df964171030dfe95faba34
Pia Ritter <pia.ritter@math.tu-dortmund.de>
2024-10-04 12:03:35 +0200

    Created test matrix factory for CSR Matrix

--- kernel/lafem/test_matrix_factory-test.cpp
+++ kernel/lafem/test_matrix_factory-test.cpp
@@ -24,6 +24,12 @@ public:
   virtual void run() const override
   {
     test_generic_csr();
+    test_symmetric_struct_csr();
+    test_symmetric_csr();
+    test_non_negative_csr();
+    test_non_empty_diag_csr();
+    test_non_zero_diag_csr();
+    test_diagonal_dominant_csr();
   }
 
   void test_generic_csr() const
@@ -79,6 +85,250 @@ public:
     for(Index i(0); i < nnze; ++i)
     {
       TEST_CHECK_IN_RANGE(val[i], DT_(-1), DT_(1));
+    }
+  }
+
+  void test_symmetric_struct_csr() const
+  {
+    SparseMatrixCSR<DT_, IT_> matrix;
+    TestMatrixFactory factory;
+    factory.seed_rng_by_timer();
+    factory.create(matrix,  6u, 6u, 15u, TestMatrixFlags::symmetric_struct);
+
+    TEST_CHECK_EQUAL(matrix.rows(), 6u);
+    TEST_CHECK_EQUAL(matrix.columns(), 6u);
+    TEST_CHECK_EQUAL(matrix.used_elements(), 15u);
+    check_structure_symmetric(matrix);
+    check_values_generic(matrix);
+  }
+
+  void check_structure_symmetric(const SparseMatrixCSR<DT_, IT_>& matrix) const
+  {
+    const Index nrows = matrix.rows();
+    const Index ncols = matrix.columns();
+    const IT_* row_ptr = matrix.row_ptr();
+    const IT_* col_idx = matrix.col_ind();
+    bool symmetric_entry = false;
+
+    TEST_CHECK(nrows == ncols);
+
+    // Check that the matrix has a symmetric structure
+    for(Index i = 0; i < nrows; ++i)
+    {
+      for(IT_ j = row_ptr[i]; j < row_ptr[i+1]; ++j)
+      {
+        IT_ col_j = col_idx[j];  // Column index of the entry
+
+        // Ensure that for every entry at (i, col_j) there is an entry at (col_j, i)
+        symmetric_entry = false;
+
+        for(IT_ k = row_ptr[col_j]; k < row_ptr[col_j + 1]; ++k)
+        {
+          if(col_idx[k] == i)
+          {
+            symmetric_entry = true;
+            break;
+          }
+        }
+        TEST_CHECK(symmetric_entry);
+      }
+    }
+  }
+
+  void test_symmetric_csr() const
+  {
+    SparseMatrixCSR<DT_, IT_> matrix;
+    TestMatrixFactory factory;
+    factory.seed_rng_by_timer();
+    factory.create(matrix, 6u, 6u, 15u, TestMatrixFlags::symmetric);
+
+    TEST_CHECK_EQUAL(matrix.rows(), 6u);
+    TEST_CHECK_EQUAL(matrix.columns(), 6u);
+    TEST_CHECK_EQUAL(matrix.used_elements(), 15u);
+    check_symmetric(matrix);
+    check_values_generic(matrix);
+  }
+
+  void check_symmetric(const SparseMatrixCSR<DT_, IT_>& matrix) const
+  {
+    const Index nrows = matrix.rows();
+    const Index ncols = matrix.columns();
+    const IT_* row_ptr = matrix.row_ptr();
+    const IT_* col_idx = matrix.col_ind();
+    const DT_* values = matrix.val();
+    bool symmetric_entry = false;
+
+    TEST_CHECK(nrows == ncols);
+
+    // Check that the matrix is symmetric
+    for(Index i = 0; i < nrows; ++i)
+    {
+      for(IT_ j = row_ptr[i]; j < row_ptr[i+1]; ++j)
+      {
+        IT_ col_j = col_idx[j];  // Column index of the entry
+
+        // Ensure that the entry at (i, col_j) = (col_j, i)
+        symmetric_entry = false;
+
+        for(IT_ k = row_ptr[col_j]; k < row_ptr[col_j + 1]; ++k)
+        {
+          if(col_idx[k] == i)
+          {
+            // Ensure that the values are equal for symmetry
+            TEST_CHECK(values[j] == values[k]);
+            symmetric_entry = true;
+            break;
+          }
+        }
+        TEST_CHECK(symmetric_entry);
+      }
+    }
+  }
+  void test_non_negative_csr() const
+  {
+    SparseMatrixCSR<DT_, IT_> matrix;
+    TestMatrixFactory factory;
+    factory.seed_rng_by_timer();
+    factory.create(matrix,  4u, 6u, 15u, TestMatrixFlags::non_negative);
+
+    TEST_CHECK_EQUAL(matrix.rows(), 4u);
+    TEST_CHECK_EQUAL(matrix.columns(), 6u);
+    TEST_CHECK_EQUAL(matrix.used_elements(), 15u);
+    check_structure_generic(matrix);
+    check_values_non_negative(matrix);
+    }
+
+  void check_values_non_negative(const SparseMatrixCSR<DT_, IT_>& matrix) const
+  {
+    const Index nnze = matrix.used_elements();
+    const DT_* val = matrix.val();
+    for(Index i(0); i < nnze; ++i)
+    {
+      TEST_CHECK_IN_RANGE(val[i], DT_(0), DT_(1));
+    }
+  }
+
+  void test_non_empty_diag_csr() const
+  {
+    SparseMatrixCSR<DT_, IT_> matrix;
+    TestMatrixFactory factory;
+    factory.seed_rng_by_timer();
+    factory.create(matrix,  6u, 6u, 15u, TestMatrixFlags::non_empty_diag);
+
+    TEST_CHECK_EQUAL(matrix.rows(), 6u);
+    TEST_CHECK_EQUAL(matrix.columns(), 6u);
+    TEST_CHECK_EQUAL(matrix.used_elements(), 15u);
+    check_structure_non_empty_diag(matrix);
+    check_values_generic(matrix);
+  }
+
+  void check_structure_non_empty_diag(const SparseMatrixCSR<DT_, IT_>& matrix) const
+  {
+    const Index nrows = matrix.rows();
+    const Index ncols = matrix.columns();
+    const IT_* row_ptr = matrix.row_ptr();
+    const IT_* col_idx = matrix.col_ind();
+    bool diagonal_entry = false;
+
+    TEST_CHECK(nrows == ncols);
+
+    for (Index i = 0; i < nrows; ++i)
+    {
+      diagonal_entry = false;
+
+      for (IT_ j = row_ptr[i]; j < row_ptr[i+1]; ++j)
+      {
+        if (col_idx[j] == i) // Check if the column index matches the row index
+        {
+          diagonal_entry = true;
+          break;
+        }
+      }
+
+      TEST_CHECK(diagonal_entry);
+    }
+  }
+
+  void test_non_zero_diag_csr() const
+  {
+    SparseMatrixCSR<DT_, IT_> matrix;
+    TestMatrixFactory factory;
+    factory.seed_rng_by_timer();
+    factory.create(matrix,  6u, 6u, 15u, TestMatrixFlags::non_zero_diag);
+
+    TEST_CHECK_EQUAL(matrix.rows(), 6u);
+    TEST_CHECK_EQUAL(matrix.columns(), 6u);
+    TEST_CHECK_EQUAL(matrix.used_elements(), 15u);
+    check_structure_non_empty_diag(matrix);
+    check_values_non_zero_diag(matrix);
+  }
+
+  void check_values_non_zero_diag(const SparseMatrixCSR<DT_, IT_>& matrix) const
+  {
+    const Index nrows = matrix.rows();
+    const Index ncols = matrix.columns();
+    const IT_* row_ptr = matrix.row_ptr();
+    const IT_* col_idx = matrix.col_ind();
+    const DT_* values = matrix.val();
+
+    TEST_CHECK(nrows == ncols);
+
+    for (Index i = 0; i < nrows; ++i)
+    {
+      for (IT_ j = row_ptr[i]; j < row_ptr[i+1]; ++j)
+      {
+        if (col_idx[j] == i) // Check if the column index matches the row index
+        {
+          TEST_CHECK_NOT_EQUAL(values[j], DT_(0));
+          break;
+        }
+      }
+    }
+  }
+
+  void test_diagonal_dominant_csr() const
+  {
+    SparseMatrixCSR<DT_, IT_> matrix;
+    TestMatrixFactory factory;
+    factory.seed_rng_by_timer();
+    factory.create(matrix,  6u, 6u, 15u, TestMatrixFlags::diagonal_dominant);
+
+    TEST_CHECK_EQUAL(matrix.rows(), 6u);
+    TEST_CHECK_EQUAL(matrix.columns(), 6u);
+    TEST_CHECK_EQUAL(matrix.used_elements(), 15u);
+    check_structure_non_empty_diag(matrix);
+    check_values_diagonal_dominant(matrix);
+  }
+
+  void check_values_diagonal_dominant(const SparseMatrixCSR<DT_, IT_>& matrix) const
+  {
+    const Index nrows = matrix.rows();
+    const Index ncols = matrix.columns();
+    const IT_* row_ptr = matrix.row_ptr();
+    const IT_* col_idx = matrix.col_ind();
+    const DT_* values = matrix.val();
+
+    TEST_CHECK(nrows == ncols);
+
+    for(Index i = 0; i < nrows; ++i)
+    {
+      DT_ off_diagonal = 0;
+      for(IT_ j = row_ptr[i]; j < row_ptr[i + 1]; ++j)
+      {
+        if(col_idx[j] != i) // Skip the diagonal element
+        {
+          off_diagonal += std::abs(values[j]);
+        }
+      }
+
+      for(IT_ j = row_ptr[i]; j < row_ptr[i + 1]; ++j)
+      {
+        if(col_idx[j] == i) // Check if this is the diagonal element
+        {
+          TEST_CHECK(values[j] > off_diagonal);
+          break;
+        }
+      }
     }
   }
 }; // class TestMatrixFactoryTest
--- kernel/lafem/test_matrix_factory.hpp
+++ kernel/lafem/test_matrix_factory.hpp
@@ -77,6 +77,51 @@ namespace FEAT
       {
         _create_generic_struct(matrix, nrows, ncols, nnze);
         _fill_generic_values(matrix);
+
+        if(flags * (TestMatrixFlags::non_negative & TestMatrixFlags::diagonal_dominant))
+        {
+          _create_non_empty_diag_struct(matrix, nrows, ncols, nnze);
+          _fill_non_negative_values(matrix);
+          _make_diagonal_dominant(matrix);
+        }
+        else if(flags * (TestMatrixFlags::non_negative & TestMatrixFlags::symmetric))
+        {
+          _create_symmetric_struct(matrix, nrows, ncols, nnze);
+          _fill_non_negative_values(matrix);
+          _make_symmetric_values(matrix);
+        }
+        else if(flags * TestMatrixFlags::symmetric_struct)
+        {
+          _create_symmetric_struct(matrix, nrows, ncols, nnze);
+          _fill_generic_values(matrix);
+        }
+        else if(flags * TestMatrixFlags::symmetric)
+        {
+          _create_symmetric_struct(matrix, nrows, ncols, nnze);
+          _fill_generic_values(matrix);
+          _make_symmetric_values(matrix);
+        }
+        else if(flags * TestMatrixFlags::non_negative)
+        {
+          _fill_non_negative_values(matrix);
+        }
+        else if(flags * TestMatrixFlags::non_empty_diag)
+        {
+          _create_non_empty_diag_struct(matrix, nrows, ncols, nnze);
+          _fill_generic_values(matrix);
+        }
+        else if(flags * TestMatrixFlags::non_zero_diag)
+        {
+          _create_non_empty_diag_struct(matrix, nrows, ncols, nnze);
+          _fill_generic_values(matrix);
+          _make_non_zero_diag(matrix);
+        }
+        else if(flags * TestMatrixFlags::diagonal_dominant)
+        {
+          _create_non_empty_diag_struct(matrix, nrows, ncols, nnze);
+          _fill_generic_values(matrix);
+          _make_diagonal_dominant(matrix);
+        }
       }
 
     protected:
@@ -98,6 +143,56 @@ namespace FEAT
       }
 
       template<typename DT_, typename IT_>
+      void _create_symmetric_struct(SparseMatrixCSR<DT_, IT_>& matrix, Index nrows, Index ncols, Index nnze)
+      {
+        XASSERT(nrows > 0u);
+        XASSERT(ncols > 0u);
+        XASSERT(nrows == ncols);
+        XASSERT(nnze <= nrows * ncols);
+        Adjacency::DynamicGraph dygraph(nrows, ncols);
+        Index nm = nrows * ncols - Index(1);
+        while(dygraph.get_num_indices() < nnze)
+        {
+          Index k = rng(Index(0), nm);
+          if(dygraph.get_num_indices() == nnze - 1)
+          {
+            dygraph.insert(k / ncols, k / ncols);
+          }
+          else
+          {
+            dygraph.insert(k / ncols, k % ncols);
+            dygraph.insert(k % ncols, k / ncols);
+          }
+        }
+        Adjacency::Graph graph(Adjacency::RenderType::as_is, dygraph);
+        matrix = SparseMatrixCSR<DT_, IT_>(graph);
+      }
+
+      template<typename DT_, typename IT_>
+      void _create_non_empty_diag_struct(SparseMatrixCSR<DT_, IT_>& matrix, Index nrows, Index ncols, Index nnze)
+      {
+        XASSERT(nrows > 0u);
+        XASSERT(ncols > 0u);
+        XASSERT(nrows == ncols);
+        XASSERT(nnze <= nrows * ncols);
+        XASSERT(nnze >= ncols);
+        Adjacency::DynamicGraph dygraph(nrows, ncols);
+        Index nm = nrows * ncols - Index(1);
+        for(Index i(0); i < ncols; ++i)
+        {
+          dygraph.insert(i, i);
+        }
+
+        while(dygraph.get_num_indices() < nnze)
+        {
+          Index k = rng(Index(0), nm);
+          dygraph.insert(k / ncols, k % ncols);
+        }
+        Adjacency::Graph graph(Adjacency::RenderType::as_is, dygraph);
+        matrix = SparseMatrixCSR<DT_, IT_>(graph);
+      }
+
+      template<typename DT_, typename IT_>
       void _fill_generic_values(SparseMatrixCSR<DT_, IT_>& matrix)
       {
         DT_* values = matrix.val();
@@ -105,6 +200,94 @@ namespace FEAT
         for(Index i(0); i < nnze; ++i)
           values[i] = rng(-DT_(1), DT_(1));
       }
+
+      template<typename DT_, typename IT_>
+      void _fill_non_negative_values(SparseMatrixCSR<DT_, IT_>& matrix)
+      {
+        DT_* values = matrix.val();
+        Index nnze = matrix.used_elements();
+        for(Index i(0); i < nnze; ++i)
+          values[i] = rng(DT_(0), DT_(1));
+      }
+
+      template<typename DT_, typename IT_>
+      void _make_symmetric_values(SparseMatrixCSR<DT_, IT_>& matrix)
+      {
+        DT_* values = matrix.val();
+        Index nnze = matrix.used_elements();
+        IT_ * col_idx = matrix.col_ind();
+        IT_ * row_ptr = matrix.row_ptr();
+        for (Index i(0); i < nnze; ++i) {
+          Index row_i = std::upper_bound(row_ptr, row_ptr + matrix.rows(), i) - row_ptr - 1;
+          Index col_i = col_idx[i];
+
+          DT_ value = values[i];
+
+          Index start = row_ptr[col_i];
+          Index end = row_ptr[col_i + 1];
+
+          for (Index j = start; j < end; ++j) {
+            if (col_idx[j] == row_i) {
+              values[j] = value;
+              break;
+            }
+          }
+        }
+      }
+      template<typename DT_, typename IT_>
+      void _make_non_zero_diag(SparseMatrixCSR<DT_, IT_>& matrix)
+      {
+        DT_* values = matrix.val();
+        IT_* col_idx = matrix.col_ind();
+        IT_* row_ptr = matrix.row_ptr();
+        const Index nrows = matrix.rows();
+        bool diagonal_entry = false;
+
+        for(Index i = 0; i < nrows; ++i)
+        {
+          diagonal_entry = false;
+          for(IT_ j = row_ptr[i]; j < row_ptr[i+1]; ++j)
+          {
+            if(col_idx[j] == i)  // Check if this is a diagonal element
+            {
+              values[j] = rng(DT_(0.1), DT_(1));
+              diagonal_entry = true;
+              break;
+            }
+          }
+        }
+      }
+
+      template<typename DT_, typename IT_>
+      void _make_diagonal_dominant(SparseMatrixCSR<DT_, IT_>& matrix)
+      {
+        DT_* values = matrix.val();
+        IT_* col_idx = matrix.col_ind();
+        IT_* row_ptr = matrix.row_ptr();
+        const Index nrows = matrix.rows();
+
+        for(Index i = 0; i < nrows; ++i)
+        {
+          DT_ off_diagonal = 0;
+          for(IT_ j = row_ptr[i]; j < row_ptr[i + 1]; ++j)
+          {
+            if(col_idx[j] != i) // Skip the diagonal element
+            {
+              off_diagonal += std::abs(values[j]);
+            }
+          }
+
+          for(IT_ j = row_ptr[i]; j < row_ptr[i + 1]; ++j)
+          {
+            if(col_idx[j] == i) // Check if this is the diagonal element
+            {
+              values[j] = off_diagonal + rng(DT_(0.1), DT_(1)); // Set diagonal value and ensure it's greater than the sum of off-diagonal elements
+              break;
+            }
+          }
+        }
+      }
+
     }; // class TestMatrixFactory
   } // namespace LAFEM
 } // namespace FEAT
