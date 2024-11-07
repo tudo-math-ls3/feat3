31a86b00f35fee0c8bbbc12b9c4f76fa8491ac12
Peter Zajac <peter.zajac@math.tu-dortmund.de>
2024-11-07 11:23:04 +0100

    adjust copyright notice to include only year 2010
    The first commit in the FEAT3 repository was pushed in 2010, so we decided that
    the copyright notice of each source/header file should state this year in its
    copyright notice, since it is a part of the project. We do not include the
    latest year that the corresponding file was modified anymore, because this does
    not seem to be that useful in continuous development cycle without classical
    release versions anymore.
    
    Signed-off-by: Peter Zajac <peter.zajac@math.tu-dortmund.de>

--- kernel/util/cuda_math.cuh
+++ kernel/util/cuda_math.cuh
@@ -1,5 +1,9 @@
-#ifndef KERNEL_UTIL_CUDA_MATH_CUH
-#define KERNEL_UTIL_CUDA_MATH_CUH 1
+// FEAT3: Finite Element Analysis Toolbox, Version 3
+// Copyright (C) 2010 by Stefan Turek & the FEAT group
+// FEAT3 is released under the GNU General Public License version 3,
+// see the file 'copyright.txt' in the top level directory for details.
+
+#pragma once
 
 #ifndef __CUDACC__
 static_assert(false, "Tyring to include a cuh header in a non nvcc compile unit!")
@@ -866,9 +870,5 @@ namespace FEAT
     {
       return (cuda_get_align<DT_>() < cuda_get_align<IT_>()) ? cuda_get_align<DT_>() : cuda_get_align<IT_>();
     }
-
-
-
   }
 }
-#endif // KERNEL_UTIL_CUDA_MATH_CUH
