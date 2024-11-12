c589583009a9df70554ecd6e925cdc15cb72a4fc
Maximilian Esser <maximilian.esser@math.tu-dortmund.de>
2024-11-12 12:47:12 +0100

    Added copyright header
    Signed-off-by: Maximilian Esser <maximilian.esser@math.tu-dortmund.de>

--- kernel/voxel_assembly/arch/burgers_assembler.cpp
+++ kernel/voxel_assembly/arch/burgers_assembler.cpp
@@ -1,3 +1,8 @@
+// FEAT3: Finite Element Analysis Toolbox, Version 3
+// Copyright (C) 2010 by Stefan Turek & the FEAT group
+// FEAT3 is released under the GNU General Public License version 3,
+// see the file 'copyright.txt' in the top level directory for details.
+
 #include <kernel/voxel_assembly/burgers_assembler.hpp>
 #include <kernel/lafem/matrix_gather_scatter_helper.hpp>
 #include <kernel/lafem/vector_gather_scatter_helper.hpp>
