467eb4a06d8ed78cf65b4191e46582a3551da1d1
Maximilian Esser <maximilian.esser@math.tu-dortmund.de>
2025-08-11 13:50:26 +0200

    Fixed CGAL and cleanup of configure script
    Signed-off-by: Maximilian Esser <maximilian.esser@math.tu-dortmund.de>

--- build_system/cmake/cgal-extra.cmake
+++ build_system/cmake/cgal-extra.cmake
@@ -4,7 +4,8 @@ if(NOT TARGET CGAL::CGAL)
 
   add_library(feat-cgal-extern INTERFACE)
   target_include_directories(feat-cgal-extern INTERFACE "${cgal_SOURCE_DIR}/include")
-  target_link_libraries(feat-cgal-extern INTERFACE Boost::boost)
+  # target_link_libraries(feat-cgal-extern INTERFACE Boost::boost)
+  target_link_libraries(feat-cgal-extern INTERFACE Boost::graph Boost::heap Boost::logic)
 
   add_library(CGAL::CGAL ALIAS feat-cgal-extern)
 endif()
