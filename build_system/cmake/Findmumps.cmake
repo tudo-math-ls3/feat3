a3d3a751bb4a0221e5096f14753bce837cd7d691
Markus Mügge <mmuegge@mathematik.tu-dortmund.de>
2026-01-07 14:42:31 +0100

    Correctly import MUMPS dependencies
    The MUMPS Find-Module only included libdmumps.a. This new version also
    includes the following so-far missing dependencies:
    - libmumps_common.a
    - libpord.a
    - Scalapack
    - MKL (or any BLAS in non-MKL builds)
    - Fortran MPI
    
    For debugging purposes this commit also adds a "--debug-find-package"
    flag to the configure script, which causes CMake to print detailed
    information about all paths it considered for any find_path or
    find_library calls.

--- build_system/cmake/Findmumps.cmake
+++ build_system/cmake/Findmumps.cmake
@@ -3,9 +3,12 @@
 include(FindPackageHandleStandardArgs)
 
 find_library(mumps_LIBRARY NAMES dmumps)
+find_library(mumps_common_LIBRARY NAMES mumps_common)
+find_library(pord_LIBRARY NAMES pord)
+find_library(scalapack_LIBRARY NAMES scalapack)
 find_path(mumps_INCLUDE_DIR NAMES dmumps_c.h dmumps_root.h dmumps_struc.h)
 
-find_package_handle_standard_args(mumps REQUIRED_VARS mumps_LIBRARY mumps_INCLUDE_DIR)
+find_package_handle_standard_args(mumps REQUIRED_VARS mumps_LIBRARY mumps_common_LIBRARY pord_LIBRARY scalapack_LIBRARY mumps_INCLUDE_DIR)
 
 if(mumps_FOUND)
   mark_as_advanced(mumps_LIBRARY)
@@ -13,7 +16,35 @@ if(mumps_FOUND)
 endif()
 
 if(mumps_FOUND AND NOT TARGET mumps::mumps)
+  add_library(mumps::scalapack UNKNOWN IMPORTED GLOBAL)
+  set_target_properties(mumps::scalapack PROPERTIES IMPORTED_LOCATION ${scalapack_LIBRARY})
+
+  add_library(mumps::common UNKNOWN IMPORTED GLOBAL)
+  set_target_properties(mumps::common PROPERTIES IMPORTED_LOCATION ${mumps_common_LIBRARY})
+
+  add_library(mumps::pord UNKNOWN IMPORTED GLOBAL)
+  set_target_properties(mumps::pord PROPERTIES IMPORTED_LOCATION ${pord_LIBRARY})
+
   add_library(mumps::mumps UNKNOWN IMPORTED GLOBAL)
   set_target_properties(mumps::mumps PROPERTIES IMPORTED_LOCATION ${mumps_LIBRARY})
   target_include_directories(mumps::mumps INTERFACE ${mumps_INCLUDE_DIR})
+
+  target_link_libraries(mumps::mumps INTERFACE mumps::common mumps::scalapack gfortran)
+  target_link_libraries(mumps::common INTERFACE mumps::pord)
+
+  if(FEAT_HAVE_MKL)
+    find_package(MKL CONFIG REQUIRED)
+    target_link_libraries(mumps::mumps INTERFACE MKL::MKL)
+  else()
+    find_package(BLAS)
+    target_link_libraries(mumps::mumps INTERFACE BLAS::BLAS)
+  endif()
+
+  if(FEAT_HAVE_MPI)
+    # find_package(MPI COMPONENTS Fortran) depends on a working fortran compiler
+    # We thus enable the language locally to avoid having to link against MPI manually
+    enable_language(Fortran)
+    find_package(MPI REQUIRED COMPONENTS Fortran)
+    target_link_libraries(mumps::mumps INTERFACE MPI::MPI_Fortran)
+  endif()
 endif()
